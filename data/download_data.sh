2c51ade9185c67989e9cfbaea29662481f8479ab
Abhishek Das <das.abhshk@gmail.com>
2019-10-06 03:36:09 -0400

    Readme update

---
+++ data/download_data.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+mkdir -p data
+
+# CO adsorption experiments from Zack / Jun.
+wget https://www.dropbox.com/s/ee0hfoc480zzlgb/ulissigroup_co.zip
+unzip ulissigroup_co.zip
+rm ulissigroup_co.zip
