82c2fa898ffdade4af7b9fa80b9c1092286119dc
Abhishek Das <das.abhshk@gmail.com>
2019-10-06 04:08:37 -0400

    Adds download link to QM9

--- data/download_data.sh
+++ data/download_data.sh
@@ -6,3 +6,8 @@ mkdir -p data
 wget https://www.dropbox.com/s/ee0hfoc480zzlgb/ulissigroup_co.zip
 unzip ulissigroup_co.zip
 rm ulissigroup_co.zip
+
+# QM9
+wget https://www.dropbox.com/s/vneosc922p8bbn8/qm9.zip
+unzip qm9.zip
+rm qm9.zip
