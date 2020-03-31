2c551c687e988985ae6dfc6dd8e8b65fe3848e42
Kevin Tran <ktrain9891@gmail.com>
2020-03-31 16:35:38 -0400

    Script to pull bulk relaxations from GASdb

---
+++ data/open-catalyst-dataset/open_catalyst_dataset/bulks.py
@@ -0,0 +1,23 @@
+'''
+This submodule contains the scripts that the Ulissi group used to pull the
+relaxed bulk structures from our database.
+'''
+
+__author__ = 'Kevin Tran'
+__email__ = 'ktran@andrew.cmu.edu'
+
+from tqdm import tqdm
+import ase.db
+from gaspy.gasdb import get_mongo_collection
+from gaspy.mongo import make_atoms_from_doc
+
+
+def dump_bulks(output_file_name='bulks.db'):
+    with get_mongo_collection('atoms') as collection:
+        docs = list(tqdm(collection.find({'fwname.calculation_type': 'unit cell optimization'}),
+                         desc='pulling from FireWorks'))
+
+    db = ase.db.connect(output_file_name)
+    for doc in tqdm(docs, desc='writing to database'):
+        atoms = make_atoms_from_doc(doc)
+        _ = db.write(atoms, mpid=doc['fwname']['mpid'])
