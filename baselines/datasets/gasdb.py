9ebcd26fa6b89f7f18067e95e88211835b573938
Abhishek Das <das.abhshk@gmail.com>
2020-04-06 17:23:54 -0400

    Don't override BaseDataset's `__len__` method; messes up indexing otherwise

--- baselines/datasets/gasdb.py
+++ baselines/datasets/gasdb.py
@@ -39,12 +39,9 @@ class Gasdb(BaseDataset):
 
         try:
             self.data, self.slices = torch.load(self.processed_file_names[0])
-            print("### Loaded preprocessed data from:  {}".format(self.preprocessed_file_names))
+            print("### Loaded preprocessed data from:  {}".format(self.processed_file_names))
         except FileNotFoundError:
             self.process()
-
-    def __len__(self):
-        return self.ase_db.count()
 
     @property
     def raw_file_names(self):
