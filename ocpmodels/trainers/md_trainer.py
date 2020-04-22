85b3ca634712a2af6a92ef5ddaf8df5af52b364b
Abhishek Das <das.abhshk@gmail.com>
2020-04-22 13:16:15 -0400

    Adds work-in-progress training script for forces

---
+++ ocpmodels/trainers/md_trainer.py
@@ -0,0 +1,68 @@
+import datetime
+import os
+import warnings
+
+import torch
+import yaml
+
+from ocpmodels.common.registry import registry
+from ocpmodels.datasets import *
+from ocpmodels.trainers.base_trainer import BaseTrainer
+
+
+@registry.register_trainer("md")
+class MDTrainer(BaseTrainer):
+    def __init__(
+        self,
+        task,
+        model,
+        dataset,
+        optimizer,
+        identifier,
+        run_dir=None,
+        is_debug=False,
+        is_vis=False,
+        print_every=100,
+        seed=None,
+        logger="wandb",
+    ):
+
+        if run_dir is None:
+            run_dir = os.getcwd()
+
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+        if identifier:
+            timestamp += "-{}".format(identifier)
+
+        self.config = {
+            "task": task,
+            "dataset": dataset,
+            "model": model.pop("name"),
+            "model_attributes": model,
+            "optim": optimizer,
+            "logger": logger,
+            "cmd": {
+                "identifier": identifier,
+                "print_every": print_every,
+                "seed": seed,
+                "timestamp": timestamp,
+                "checkpoint_dir": os.path.join(
+                    run_dir, "checkpoints", timestamp
+                ),
+                "results_dir": os.path.join(run_dir, "results", timestamp),
+                "logs_dir": os.path.join(run_dir, "logs", timestamp),
+            },
+        }
+
+        os.makedirs(self.config["cmd"]["checkpoint_dir"])
+        os.makedirs(self.config["cmd"]["results_dir"])
+        os.makedirs(self.config["cmd"]["logs_dir"])
+
+        self.is_debug = is_debug
+        self.is_vis = is_vis
+        self.device = torch.device(
+            "cuda" if torch.cuda.is_available() else "cpu"
+        )
+
+        self.load()
+        print(yaml.dump(self.config, default_flow_style=False))
