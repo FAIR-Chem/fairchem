fd177f5fcafee9c7719222b719cd8c6fc2c737ea
Kevin Tran <ktrain9891@gmail.com>
2020-04-29 10:51:24 -0400

    Add some more credits for authorship

--- src/fairchem/data/oc/ocdata/adsorptions.py
+++ src/fairchem/data/oc/ocdata/adsorptions.py
@@ -6,8 +6,8 @@ Note that some of these scripts were taken from
 [GASpy](https://github.com/ulissigroup/GASpy) with permission of author.
 '''
 
-__authors__ = ['Kevin Tran, Aini Palizhati']
-__emails__ = ['ktran@andrew.cmu.edu', 'apalizha@andrew.cmu.edu']
+__authors__ = ['Kevin Tran, Aini Palizhati', 'Siddharth Goyal', 'Zachary Ulissi']
+__email__ = ['ktran@andrew.cmu.edu']
 
 import math
 from collections import defaultdict
