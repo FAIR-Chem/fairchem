660cce0f10e9e94b1ea376fd03e93d394f38706f
Kevin Tran <ktrain9891@gmail.com>
2020-05-15 09:09:31 -0400

    Parametrize kernel and covariance matrix for GP

--- ocpmodels/models/gps.py
+++ ocpmodels/models/gps.py
@@ -4,14 +4,18 @@ from ocpmodels.common.registry import registry
 
 @registry.register_model("exact_gp")
 class ExactGP(gpytorch.models.ExactGP):
-    """ Taken from GPyTorch tutorial documentation """
+    """ Modified from GPyTorch tutorial documentation """
 
-    def __init__(self, train_x, train_y, likelihood, device, n_devices):
+    def __init__(self, train_x, train_y, likelihood, device, n_devices,
+                 kernel=None, cov_matrix=None):
+        if kernel is None:
+            kernel = gpytorch.kernels.MaternKernel()
+        if cov_matrix is None:
+            self.cov_matrix = gpytorch.distributions.MultivariateNormal
+
         super(ExactGP, self).__init__(train_x, train_y, likelihood)
         self.mean_module = gpytorch.means.ConstantMean()
-        base_covar_module = gpytorch.kernels.ScaleKernel(
-            gpytorch.kernels.MaternKernel()
-        )
+        base_covar_module = gpytorch.kernels.ScaleKernel(kernel)
 
         self.covar_module = gpytorch.kernels.MultiDeviceKernel(
             base_covar_module, device_ids=range(n_devices), device=device
@@ -20,4 +24,4 @@ class ExactGP(gpytorch.models.ExactGP):
     def forward(self, x):
         mean_x = self.mean_module(x)
         covar_x = self.covar_module(x)
-        return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
+        return self.cov_matrix(mean_x, covar_x)
