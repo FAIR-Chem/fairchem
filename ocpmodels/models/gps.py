ffdf58bea50ee42e2b24b829b537f740d8a28062
Kevin Tran <ktrain9891@gmail.com>
2020-06-03 12:30:20 -0400

    Parameterize kernel and covariance matrix for GP (#10)
    * Parametrize kernel and covariance matrix for GP
    
    * Propagate parameterized GP through GPyTorchTrainer
    
    * Call ExactGP.forward method directly
    
    When trying to load a previous state and rerunning, for some reason we
    were getting argument errors (e.g., passing 3 positional arguments and
    expected 2) when calling the GP model, which implicitly called the
    `forward` method. This somehow got fixed by calling the `forward` method
    explicitly.
    
    * Add init file to modules to fix importing
    
    * Check for empty ase.db objects
    
    * Pass checkpoint sizes correctly for GPyTorch
    
    * More consistent GP state loading for CFGP
    
    * Fix bug with default cov matrix in CFGP
    
    * Clarify and correct GPpyTorch training procedure
    
    Former-commit-id: 0a50d3b55d5731051b49128e9e722a9fb31c8479

--- ocpmodels/models/gps.py
+++ ocpmodels/models/gps.py
@@ -4,20 +4,23 @@ from ocpmodels.common.registry import registry
 
 @registry.register_model("exact_gp")
 class ExactGP(gpytorch.models.ExactGP):
-    """ Taken from GPyTorch tutorial documentation """
+    """ Modified from GPyTorch tutorial documentation """
 
-    def __init__(self, train_x, train_y, likelihood, device, n_devices):
+    def __init__(self, MeanKernel, CovKernel, OutputDist,
+                 likelihood, train_x, train_y,
+                 output_device, n_devices,
+                 ):
+        self.OutputDist = OutputDist
+
         super(ExactGP, self).__init__(train_x, train_y, likelihood)
-        self.mean_module = gpytorch.means.ConstantMean()
-        base_covar_module = gpytorch.kernels.ScaleKernel(
-            gpytorch.kernels.MaternKernel()
-        )
+        self.mean_module = MeanKernel()
+        base_covar_module = gpytorch.kernels.ScaleKernel(CovKernel())
 
         self.covar_module = gpytorch.kernels.MultiDeviceKernel(
-            base_covar_module, device_ids=range(n_devices), device=device
+            base_covar_module, device_ids=range(n_devices), output_device=output_device
         )
 
     def forward(self, x):
         mean_x = self.mean_module(x)
         covar_x = self.covar_module(x)
-        return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
+        return self.OutputDist(mean_x, covar_x)
