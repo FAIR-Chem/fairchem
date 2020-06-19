3c1b462b971d6d31ef08e10eb8ac70567dee2fb5
Muhammed Shuaibi <45150244+mshuaibii@users.noreply.github.com>
2020-06-19 16:55:46 -0500

    ML-relaxation evaluation (#22)
    * Renamed "MDTrainer" to "ForcesTrainer" and "COCuMD" dataset to "TrajectoryDataset". Adding ML-relaxation evaluation metric.
    
    * ase_calc load error check
    
    * removed hard-coded evaluation settings, updated task config
    
    * DEBUG: initial parallel scheme
    
    * removed parallel evaluator for time being, direct ml results to "results_dir", clean up forces_trainer
    
    * fix directory to be consistent with other examples
    
    * pre-commit hook
    
    Co-authored-by: Abhishek Das <das.abhshk@gmail.com>

--- ocpmodels/trainers/base_trainer.py
+++ ocpmodels/trainers/base_trainer.py
@@ -214,7 +214,7 @@ class BaseTrainer:
             "xie_grossman_mat_proj",
         ]:
             bond_feat_dim = self.train_loader.dataset[0].edge_attr.shape[-1]
-        elif self.config["task"]["dataset"] in ["gasdb", "md"]:
+        elif self.config["task"]["dataset"] in ["gasdb", "trajectory"]:
             bond_feat_dim = self.config["model_attributes"].get(
                 "num_gaussians", 50
             )
