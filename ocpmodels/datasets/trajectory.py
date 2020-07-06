83a00d8a5414a02d495ded37094e29210a5e65ae
Muhammed Shuaibi <mshuaibi@andrew.cmu.edu>
2020-07-06 12:01:34 -0700

    Update TrajectoryDataset to use new processing
    Former-commit-id: 7cd056cedbe2b0319e8b4a769e4b6680327ff826

--- ocpmodels/datasets/trajectory.py
+++ ocpmodels/datasets/trajectory.py
@@ -13,6 +13,7 @@ from ocpmodels.common.utils import collate
 from ocpmodels.datasets import BaseDataset
 from ocpmodels.datasets.elemental_embeddings import EMBEDDINGS
 from ocpmodels.datasets.gasdb import AtomicFeatureGenerator, GaussianDistance
+from ocpmodels.preprocessing import AtomsToGraphs
 
 
 @registry.register_dataset("trajectory")
@@ -59,56 +60,26 @@ class TrajectoryDataset(BaseDataset):
             )
         )
         traj = Trajectory(self.raw_file_names[0])
-        feature_generator = TrajectoryFeatureGenerator(traj)
-
-        # Hacky approach to bypass prediction time where energy and forces
-        # are not available
-        try:
-            forces = [i.get_forces(apply_constraint=False) for i in traj]
-            p_energies = [
-                i.get_potential_energy(apply_constraint=False) for i in traj
-            ]
-        except Exception:
-            # TODO: cleaner way to bypass the requirement for energies, forces.
-            # fooling it for now
-            forces = [[0] for i in traj]
-            p_energies = [0 for i in traj]
+        a2g = AtomsToGraphs(
+            max_neigh=self.config.get("max_neigh", 12),
+            radius=self.config.get("radius", 6),
+            dummy_distance=self.config.get("radius", 6) + 1,
+            dummy_index=-1,
+            r_energy=True,
+            r_forces=True,
+            r_distances=False,
+        )
 
         data_list = []
-        zipped_data = zip(feature_generator, forces, p_energies)
 
-        for (
-            (embedding, distance, index, positions, atomic_numbers),
-            force,
-            p_energy,
-        ) in tqdm(
-            zipped_data,
+        for atoms in tqdm(
+            traj,
             desc="preprocessing atomic features",
             total=len(traj),
             unit="structure",
         ):
-            edge_index = [[], []]
-            edge_attr = torch.FloatTensor(
-                index.shape[0] * index.shape[1], distance.shape[-1]
-            )
-            for j in range(index.shape[0]):
-                for k in range(index.shape[1]):
-                    edge_index[0].append(j)
-                    edge_index[1].append(index[j, k])
-                    edge_attr[j * index.shape[1] + k] = distance[j, k].clone()
-            edge_index = torch.LongTensor(edge_index)
-            data_list.append(
-                Data(
-                    x=embedding,
-                    edge_index=edge_index,
-                    edge_attr=edge_attr,
-                    y=p_energy,
-                    pos=positions,
-                    atomic_numbers=atomic_numbers,
-                    force=torch.tensor(force),
-                    natoms=torch.tensor([positions.shape[0]]),
-                )
-            )
+            data_list.append(a2g.convert(atoms))
+
         self.data, self.slices = collate(data_list)
         torch.save((self.data, self.slices), self.processed_file_names[0])
 
@@ -146,55 +117,3 @@ class TrajectoryDataset(BaseDataset):
             test_loader = DataLoader(test_dataset, batch_size=batch_size)
 
         return train_loader, val_loader, test_loader
-
-
-class TrajectoryFeatureGenerator(AtomicFeatureGenerator):
-    """
-    Iterator meant to generate the features of the atoms objects within a trajectory
-
-    Parameters
-    ----------
-
-    traj: instance of `ase.io.trajectory.Trajectory`
-        MD trajactory of `ase.Atoms` objects
-    max_num_nbr: int
-        The maximum number of neighbors while constructing the crystal graph
-    radius: float
-        The cutoff radius for searching neighbors
-    dmin: float
-        The minimum distance for constructing GaussianDistance
-    step: float
-        The step size for constructing GaussianDistance
-
-    Returns
-    -------
-
-    embeddings: torch.Tensor shape (n_i, atom_fea_len)
-    gaussian_distances: torch.Tensor shape (n_i, M, nbr_fea_len)
-    all_indices: torch.LongTensor shape (n_i, M)
-    """
-
-    def __init__(
-        self, traj, max_num_nbr=12, radius=6, dmin=0, step=0.2, start=0
-    ):
-        self.traj = traj
-        super(TrajectoryFeatureGenerator, self).__init__(
-            None, max_num_nbr, radius, dmin, step, start
-        )
-
-    def __len__(self):
-        return len(self.traj)
-
-    def __next__(self):
-        try:
-            item = self.__getitem__(self.num)
-            self.num += 1
-            return item
-
-        except IndexError:
-            assert self.num == self.__len__()
-            raise StopIteration
-
-    def __getitem__(self, index):
-        atoms = self.traj[index]
-        return self.extract_atom_features(atoms)
