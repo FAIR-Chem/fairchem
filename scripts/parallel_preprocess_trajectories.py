05724eca0f28786bc482789cf1979c128a1c40db
Abhishek Das <das.abhshk@gmail.com>
2020-07-13 16:16:55 -0700

    Gets number of images from args.size, saves sampled ids in txt, plays well with tqdm

--- scripts/parallel_preprocess_trajectories.py
+++ scripts/parallel_preprocess_trajectories.py
@@ -1,5 +1,6 @@
 """
-Reads a folder of Trajectory files and saves an LMDB with extracted features.
+Reads a txt file with paths to ASE trajectories and
+creates LMDB files with extracted graph features.
 """
 
 import argparse
@@ -10,50 +11,67 @@ import random
 
 import ase.io
 import lmdb
-import numpy as np
 import torch
-from ase.io.trajectory import Trajectory
 from tqdm import tqdm
 
 from ocpmodels.preprocessing import AtomsToGraphs
 
 
-def read_trajectory_and_extract_features(a2g, traj_path):
-    traj = ase.io.read(traj_path, ":")
-    return a2g.convert_all(traj, disable_tqdm=True)
-
-
-def construct_lmdb(inputs):
-    idx, paths = inputs
-    os.makedirs(os.path.join(args.out_path, f"db_{idx}"), exist_ok=True)
-    map_size = 1099511627776 * 2
+def write_images_to_lmdb(mp_arg):
+    a2g, db_path, traj_paths, sampled_unique_ids, idx = mp_arg
     db = lmdb.open(
-        os.path.join(args.out_path, f"db_{idx}", f"data_{idx}.lmdb"),
-        map_size=map_size,
+        db_path,
+        map_size=1099511627776 * 2,
         subdir=False,
         meminit=False,
         map_async=True,
     )
 
-    # Extract features.
-    i = 0
-    for path in paths:
-        try:
-            dl = read_trajectory_and_extract_features(a2g, path)
-            for do in dl:
-                # filter out images with excessivley large forces, if applicable
-                if torch.max(torch.abs(do.force)).item() <= args.filter:
-                    txn = db.begin(write=True)
-                    txn.put(
-                        f"{i}".encode("ascii"), pickle.dumps(do, protocol=-1)
-                    )
-                    txn.commit()
-                    i += 1
-        except Exception:
-            continue
+    try:
+        traj_idx = random.randrange(0, len(traj_paths))
+        dl, sampled_unique_ids = read_trajectory_and_extract_features(
+            a2g, traj_paths[traj_idx], sampled_unique_ids
+        )
+        for do in dl:
+            # filter out images with excessivley large forces, if applicable
+            if (
+                torch.max(torch.abs(do.force)).item()
+                <= args.force_filter_threshold
+            ):
+                txn = db.begin(write=True)
+                txn.put(
+                    f"{idx}".encode("ascii"), pickle.dumps(do, protocol=-1)
+                )
+                txn.commit()
+                idx += 1
+    except Exception:
+        pass
 
     db.sync()
     db.close()
+
+    return sampled_unique_ids, idx
+
+
+def read_trajectory_and_extract_features(a2g, traj_path, sampled_unique_ids):
+    traj = ase.io.read(traj_path, ":")
+    traj_len = len(traj)
+    # 0.1 heuristic should work fine as long as args.size is greater than
+    # 20 * num_trajectories (each trajectory has 200 images on average).
+    sample_count = int(0.1 * traj_len)
+    sample_idx = random.sample(range(traj_len), sample_count)
+    images = []
+    for idx in sample_idx:
+        uid = os.path.join(traj_path, f" {idx}\n")
+        if uid not in sampled_unique_ids:
+            sampled_unique_ids.append(uid)
+            images.append(traj[idx])
+    return a2g.convert_all(images, disable_tqdm=True), sampled_unique_ids
+
+
+def chunk_list(lst, num_splits):
+    n = max(1, len(lst) // num_splits)
+    return [lst[i : i + n] for i in range(0, len(lst), n)]
 
 
 if __name__ == "__main__":
@@ -72,17 +90,24 @@ if __name__ == "__main__":
         help="Directory to save extracted features. Will create if doesn't exist",
     )
     parser.add_argument(
-        "--filter",
+        "--num-workers",
+        type=int,
+        default=1,
+        required=True,
+        help="No. of feature-extracting processes or no. of dataset chunks",
+    )
+    parser.add_argument(
+        "--size",
+        type=int,
+        default=None,
+        required=True,
+        help="No. of images in the dataset",
+    )
+    parser.add_argument(
+        "--force-filter-threshold",
         type=float,
         default=1.0e9,
         help="Max force to filter out, default: no filter",
-    )
-
-    parser.add_argument(
-        "--cpus",
-        type=int,
-        default=50,
-        help="Number of cpus to parallelize across",
     )
 
     args = parser.parse_args()
@@ -108,10 +133,42 @@ if __name__ == "__main__":
         r_distances=False,
     )
 
-    raw_traj_splits = np.array_split(np.array(raw_traj_files), args.cpus)
-    _inputs = []
-    for idx, i in enumerate(range(1, args.cpus + 1)):
-        _inputs.append((i, raw_traj_splits[idx]))
+    # Create output directory if it doesn't exist.
+    os.makedirs(os.path.join(args.out_path), exist_ok=True)
 
-    pool = mp.Pool(args.cpus)
-    list(tqdm(pool.imap(construct_lmdb, _inputs)))
+    # Initialize lmdb paths
+    db_paths = [
+        os.path.join(args.out_path, "data.%03d.lmdb" % i)
+        for i in range(args.num_workers)
+    ]
+
+    # Chunk the trajectories into args.num_workers splits
+    chunked_traj_files = chunk_list(raw_traj_files, args.num_workers)
+
+    # Extract features
+    sampled_unique_ids, idx = [[]] * args.num_workers, [0] * args.num_workers
+    pbar = tqdm(total=args.size)
+    pool = mp.Pool(args.num_workers)
+    while sum(idx) <= args.size:
+        mp_args = [
+            (
+                a2g,
+                db_paths[i],
+                chunked_traj_files[i],
+                sampled_unique_ids[i],
+                idx[i],
+            )
+            for i in range(args.num_workers)
+        ]
+        op = list(zip(*pool.imap(write_images_to_lmdb, mp_args)))
+        sampled_unique_ids, idx = list(op[0]), list(op[1])
+        pbar.update(sum(idx) - pbar.last_print_n)
+
+    # Log sampled image, trajectory trace
+    for i in range(args.num_workers):
+        ids_log = open(
+            os.path.join(args.out_path, "data_log.%03d.txt" % i), "w"
+        )
+        ids_log.writelines(sampled_unique_ids[i])
+
+    pbar.close()
