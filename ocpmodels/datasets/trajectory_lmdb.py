fa9e9349bf999a8d057b3e3fd59e2db6b4d0fe8e
Muhammed Shuaibi <45150244+mshuaibii@users.noreply.github.com>
2020-07-14 17:37:25 -0500

    Parallel preprocessing from ASE to LMDB (#25)
    * parallel preprocessing
    
    * Gets number of images from args.size, saves sampled ids in txt, plays well with tqdm
    
    * Typo
    
    * Updates dataloader to read from directory of LMDBs
    
    * adds the indices of the fixed atoms to the preprocessing script
    
    * added fixed atoms flag to parallel script
    
    * binary vector instead of explicity indices
    
    Co-authored-by: Abhishek Das <das.abhshk@gmail.com>

--- ocpmodels/datasets/trajectory_lmdb.py
+++ ocpmodels/datasets/trajectory_lmdb.py
@@ -1,8 +1,10 @@
+import glob
 import json
 import os
 import pickle
 
 import lmdb
+import numpy as np
 from torch.utils.data import Dataset
 from torch_geometric.data import Batch
 
@@ -15,34 +17,55 @@ class TrajectoryLmdbDataset(Dataset):
         super(TrajectoryLmdbDataset, self).__init__()
 
         self.config = config
-        env = lmdb.open(
-            self.config["src"],
-            subdir=False,
-            readonly=True,
-            lock=False,
-            readahead=False,
-            map_size=1099511627776 * 2,
-        )
-        self.db_txn = env.begin()
-        self._keys = [
-            f"{i}".encode("ascii") for i in range(env.stat()["entries"])
-        ]
 
-        self.inds = []
+        self.db_paths = glob.glob(self.config["src"] + "*lmdb")
+        envs = [
+            lmdb.open(
+                self.db_paths[i],
+                subdir=False,
+                readonly=True,
+                lock=False,
+                readahead=False,
+                map_size=1099511627776 * 2,
+            )
+            for i in range(len(self.db_paths))
+        ]
+        self.db_txn = [envs[i].begin() for i in range(len(self.db_paths))]
+
+        self._keys = [
+            [f"{j}".encode("ascii") for j in range(envs[i].stat()["entries"])]
+            for i in range(len(self.db_paths))
+        ]
+        self._keylens = [len(k) for k in self._keys]
+        self._keylen_cumulative = np.cumsum(self._keylens).tolist()
+
         self.transform = transform
 
     def __len__(self):
-        return len(self._keys)
+        return sum(self._keylens)
 
     def __getitem__(self, idx):
-        datapoint_pickled = self.db_txn.get(self._keys[idx])
+        # Figure out which db this should be indexed from.
+        db_idx = 0
+        for i in range(len(self._keylen_cumulative)):
+            if self._keylen_cumulative[i] > idx:
+                db_idx = i
+                break
+
+        # Extract index of element within that db.
+        el_idx = idx
+        if db_idx != 0:
+            el_idx = idx - self._keylen_cumulative[db_idx - 1]
+        assert el_idx >= 0
+
+        # Return features.
+        datapoint_pickled = self.db_txn[db_idx].get(self._keys[db_idx][el_idx])
         data_object = pickle.loads(datapoint_pickled)
         data_object = (
             data_object
             if self.transform is None
             else self.transform(data_object)
         )
-        self.inds.append(idx)
         return data_object
 
 
