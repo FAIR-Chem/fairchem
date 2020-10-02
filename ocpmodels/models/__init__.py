fb7cbe2ee016f3979c3cb69f44783941118c4c07
Abhishek Das <das.abhshk@gmail.com>
2020-10-02 01:15:02 -0700

    Implements DimeNet++ (#77)
    * Implements DimeNet++
    
    * Updated config
    
    Former-commit-id: f311887658ec6ee61c79466223a2542656a85edd

--- ocpmodels/models/__init__.py
+++ ocpmodels/models/__init__.py
@@ -3,6 +3,7 @@ __all__ = [
     "CGCNNGu",
     "CNN3D_LOCAL",
     "DimeNet",
+    "DimeNetPlusPlus",
     "DOGSS",
     "ExactGP",
     "SchNet",
@@ -13,6 +14,7 @@ from .cgcnn import CGCNN
 from .cgcnn_gu import CGCNNGu
 from .cnn3d_local import CNN3D_LOCAL
 from .dimenet import DimeNetWrap as DimeNet
+from .dimenet_plus_plus import DimeNetPlusPlusWrap as DimeNetPlusPlus
 from .dogss import DOGSS
 from .gps import ExactGP
 from .schnet import SchNetWrap as SchNet
