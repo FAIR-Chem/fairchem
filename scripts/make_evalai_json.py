c338dad0eecf31755c8a4f6a46eb870f694727dc
Muhammed Shuaibi <mshuaibi@andrew.cmu.edu>
2020-10-11 20:14:10 +0000

    makes split names consistent with paper
    Former-commit-id: ca2bd8d48ccda7f1de3fb30c0373f84be8f767f8

--- scripts/make_evalai_json.py
+++ scripts/make_evalai_json.py
@@ -5,7 +5,7 @@ import os
 def main(paths, filename, data_split="val"):
     submission_file = {}
 
-    for idx, split in enumerate(["id", "ood_ads", "ood_bulk", "ood_ads_bulk"]):
+    for idx, split in enumerate(["id", "ood_ads", "ood_cat", "ood_both"]):
         key = "_".join([data_split, split])
         submission_file[key] = json.load(open(os.path.join(paths[idx]), "r"))
 
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     """
     Create a submission file for evalAI. Ensure that for the task you are
     submitting for you have generated results files on each of the 4 splits -
-    id, ood_ads, ood_bulk, ood_ads_bulk.
+    id, ood_ads, ood_cat, ood_both.
 
     Results file can be obtained as follows for the various tasks:
 
@@ -31,9 +31,9 @@ if __name__ == "__main__":
 
     id_path = "/path/to/id/results_file"
     ood_ads_path = "/path/to/ood_ads/results_file"
-    ood_bulk_path = "/path/to/ood_bulk/results_file"
-    ood_ads_bulk_path = "/path/to/ood_ads_bulk/results_file"
+    ood_cat_path = "/path/to/ood_cat/results_file"
+    ood_both_path = "/path/to/ood_both/results_file"
 
-    paths = [id_path, ood_ads_path, ood_bulk_path, ood_ads_bulk_path]
+    paths = [id_path, ood_ads_path, ood_cat_path, ood_both_path]
 
     main(paths, filename="TASKNAME_evalai_submission.json", data_split="val")
