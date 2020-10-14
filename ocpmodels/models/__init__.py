6d9ff5e8352b911091ed87df279f3b00863625c2
anuroopsriram <anuroops@fb.com>
2020-10-13 17:29:13 -0700

    Removed dimenet++
    Former-commit-id: c7d8e255f83ad7d2b9d524ea929b20c3e4933776

--- ocpmodels/models/__init__.py
+++ ocpmodels/models/__init__.py
@@ -8,11 +8,9 @@ LICENSE file in the root directory of this source tree.
 __all__ = [
     "CGCNN",
     "DimeNet",
-    "DimeNetPlusPlus",
     "SchNet",
 ]
 
 from .cgcnn import CGCNN
 from .dimenet import DimeNetWrap as DimeNet
-from .dimenet_plus_plus import DimeNetPlusPlusWrap as DimeNetPlusPlus
 from .schnet import SchNetWrap as SchNet
