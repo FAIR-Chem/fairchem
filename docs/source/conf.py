be09f68fb48c32d092c094e998f4f68780f147ad
Muhammed Shuaibi <mshuaibi@andrew.cmu.edu>
2020-10-14 04:18:26 +0000

    apply black to conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -64,5 +64,4 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
-master_doc = 'index'
-
+master_doc = "index"
