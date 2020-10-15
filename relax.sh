d03e43a8b156efe1e1fa087d2992e32d54632bb8
anuroopsriram <anuroops@fb.com>
2020-10-14 18:04:48 -0700

    Parallel relaxations
    Former-commit-id: de7bb0cfbb9a0671462a14eda9197c3bcc9a3497

--- relax.sh
+++ relax.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 
-#source /private/home/anuroops/anaconda3/bin/activate
-#conda activate ocp-models
-
-
 /private/home/anuroops/anaconda3/envs/ocp-models/bin/python -u main.py --mode run-relaxations --config-yml configs/relax/dimenet_is.yml \
                  --distributed --num-gpus 8 --checkpoint checkpoint.pt --num-runs 40 \
                  --slurm-partition priority --submit
