cb444bf704b8cd0ebd67eee51b9fd82c167a55b6
anuroopsriram <anuroops@fb.com>
2020-12-18 14:51:42 -0800

    Minor cleanup

--- ocpmodels/trainers/forces_trainer.py
+++ ocpmodels/trainers/forces_trainer.py
@@ -605,9 +605,6 @@ class ForcesTrainer(BaseTrainer):
         for i, batch in tqdm(
             enumerate(self.relax_loader), total=len(self.relax_loader)
         ):
-            if i == 20:
-                break
-
             relaxed_batch = ml_relax(
                 batch=batch,
                 model=self,
