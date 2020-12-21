957bdff3adda95674d9e88f17fa327ff801b9fb1
Muhammed Shuaibi <45150244+mshuaibii@users.noreply.github.com>
2020-12-21 13:31:39 -0600

    LMDB optimizations (#154)
    * lmdb optimizations
    
    * close lmdb after training

--- ocpmodels/datasets/single_point_lmdb.py
+++ ocpmodels/datasets/single_point_lmdb.py
@@ -35,29 +35,25 @@ class SinglePointLmdbDataset(Dataset):
             self.db_path
         )
 
-        env = self.connect_db(self.db_path)
+        self.env = self.connect_db(self.db_path)
 
         self._keys = [
-            f"{j}".encode("ascii") for j in range(env.stat()["entries"])
+            f"{j}".encode("ascii") for j in range(self.env.stat()["entries"])
         ]
         self.transform = transform
-
-        env.close()
 
     def __len__(self):
         return len(self._keys)
 
     def __getitem__(self, idx):
         # Return features.
-        env = self.connect_db(self.db_path)
-        datapoint_pickled = env.begin().get(self._keys[idx])
+        datapoint_pickled = self.env.begin().get(self._keys[idx])
         data_object = pickle.loads(datapoint_pickled)
         data_object = (
             data_object
             if self.transform is None
             else self.transform(data_object)
         )
-        env.close()
 
         return data_object
 
@@ -68,6 +64,10 @@ class SinglePointLmdbDataset(Dataset):
             readonly=True,
             lock=False,
             readahead=False,
-            map_size=1099511627776 * 2,
+            meminit=False,
+            max_readers=1,
         )
         return env
+
+    def close_db(self):
+        self.env.close()
