c781727baba33d3afdc03c58b55311aeddd65c16
Janice Lan <janlan@fb.com>
2021-01-25 18:21:30 -0800

    Refactor code for sampling and enumerating structures (#23)
    Adds a script to sample structures with refactored classes and the following functionality:
    * can read from precomputed surface enumeration
    * can enumerate all surfaces and configs for a list of bulks and one adsorbate
    * can enumerate all surfaces and configs for a file containing (bulk, adsorbate) pairs
    * can sample one random structure
    
    Co-authored-by: Siddharth Goyal <sidgoyal@fb.com>
    Former-commit-id: fe017957a848fca2635a7e1340ed46074bf1b0b6

---
+++ data/open-catalyst-dataset/ocdata/adsorbates.py
@@ -0,0 +1,37 @@
+
+import numpy as np
+import pickle
+
+
+class Adsorbate():
+    '''
+    This class handles all things with the adsorbate.
+    Selects one (either specified or random), and stores info as an object
+    '''
+    def __init__(self, adsorbate_database, specified_index=None):
+        self.choose_adsorbate_pkl(adsorbate_database, specified_index)
+
+    def choose_adsorbate_pkl(self, adsorbate_database, specified_index=None):
+        '''
+        Chooses an adsorbate from our pkl based inverted index at random.
+
+        Args:
+            adsorbate_database   A string pointing to the a pkl file that contains
+                                 an inverted index over different adsorbates.
+        Sets:
+            atoms                    `ase.Atoms` object of the adsorbate
+            smiles                   SMILES-formatted representation of the adsorbate
+            bond_indices             list of integers indicating the indices of the atoms in
+                                     the adsorbate that are meant to be bonded to the surface
+            adsorbate_sampling_str   Enum string specifying the sample, [index]/[total]
+        '''
+        with open(adsorbate_database, 'rb') as f:
+            inv_index = pickle.load(f)
+
+        if specified_index is not None:
+            element = specified_index
+        else:
+            element = np.random.choice(len(inv_index))
+
+        self.adsorbate_sampling_str = str(element) + "/" + str(len(inv_index))
+        self.atoms, self.smiles, self.bond_indices = inv_index[element]
