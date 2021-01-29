975dc4ec022ca69cc062664b9d0cfc046df837e8
anuroopsriram <anuroops@fb.com>
2021-01-29 00:00:15 -0800

    Commit
    Former-commit-id: bbf6fee49f4ce850578fd69ddbc6da448ff5ef83

--- get_results.py
+++ get_results.py
@@ -4,7 +4,8 @@ from typing import List
 import pandas as pd
 
 
-COLS = list("params forces_mae forces_cos energy_mae energy_force_within_threshold time".split())
+# COLS = list("params forces_mae forces_cos energy_mae energy_force_within_threshold time".split())
+COLS = list("params forces_mae forces_cos energy_mae energy_force_within_threshold time gpu_mem".split())
 
 
 def main(fls: List[Path], feats: List[str]):
@@ -12,18 +13,32 @@ def main(fls: List[Path], feats: List[str]):
 
     for fl in fls:
         text = fl.read_text().splitlines()
-        config = {
-            feat: next(ln.strip().split()[-1] for ln in text if feat in ln.strip())
-            for feat in feats
-        }
+        # config = {
+        #     feat: next(ln.strip().split()[-1] for ln in text if feat in ln.strip())
+        #     for feat in feats
+        # }
+        config = {}
+        for feat in feats:
+            vals = [ln.strip().split()[-1] for ln in text if feat in ln.strip()]
+            config[feat] = 1 if not vals else vals[0]
+
         param_line = [ln for ln in text if "parameters" in ln][0]
         params = param_line.strip().split()[-2]
         config["fl"] = fl
         config["params"] = params
+        config["name"] = fl.parent.name.split("_")[0]
+
+        memlines = [ln for ln in text if "[MemStats]" in ln]
+        if len(memlines) > 0:
+            memln = memlines[-1]
+            mem = memln.split()[5].strip()
+            mem = int(mem.strip(",").strip("G"))
+            config["gpu_mem"] = mem
 
         try:
+            metrics = text[-3].strip()
             # metrics = text[-3].strip().split(" ", 1)[1]
-            metrics = [ln for ln in text if "Validation:" in ln][-1].strip().split(" ", 1)[1]
+            # metrics = [ln for ln in text if "Validation:" in ln][-1].strip().split(" ", 1)[1]
             metrics = dict(x.split(":") for x in metrics.split(","))
             metrics = {k.strip(): v for k, v in metrics.items()}
             config.update(metrics)
@@ -36,11 +51,12 @@ def main(fls: List[Path], feats: List[str]):
 
             results.append(config)
         except:
-            pass
+            raise
     
     results = pd.DataFrame(results)
     results = results[feats + COLS]
     # print(results.to_string(index=None).replace("   ", "\t"))
+    results.sort_values(by=feats)
     print(results.to_csv(index=False).replace(" ", "").replace(",", "\t"))
     
     # for fl in fls:
@@ -75,9 +91,23 @@ if __name__ == "__main__":
     # fls = list(Path("exp/dpp_sweep/dpp_2M/sweep_dpp6/slurm/logs/").glob("dpp_*/*_0_log.out"))
     # feats = ["num_spherical", "num_radial"]
 
-    fls = list(Path("exp/dpp_sweep/dpp_2M/sweep_dpp7/slurm/logs/").glob("dpp_*/*_0_log.out"))
-    feats = ["cutoff", "max_num_neighbors"]
+    # fls = list(Path("exp/dpp_sweep/dpp_2M/sweep_dpp7/slurm/logs/").glob("dpp_*/*_0_log.out"))
+    # feats = ["cutoff", "max_num_neighbors"]
 
+    # fls = list(Path("exp/paralleldpp/pardpp.sweeps/sweep1/slurm/logs/").glob("*/*_0_log.out"))
+    # feats = ["gpus_per_task", "num_blocks", "batch_size"]
+
+    # fls = list(Path("exp/paralleldpp/pardpp.sweeps/sweep2/slurm/logs/").glob("*/*_0_log.out"))
+    # feats = ["gpus_per_task", "hidden_channels", "out_emb_channels", "batch_size"]
+
+    # fls = list(Path("exp/paralleldpp/pardpp.sweeps/sweep3/slurm/logs/").glob("*/*_0_log.out"))
+    # feats = ["gpus_per_task", "int_emb_size", "batch_size"]
+
+    # fls = list(Path("exp/paralleldpp/pardpp.sweeps/sweep4/slurm/logs/").glob("*/*_0_log.out"))
+    # feats = ["gpus_per_task", "num_after_skip", "num_output_layers", "batch_size"]
+
+    fls = list(Path("exp/paralleldpp/pardpp.sweeps/sweep5/slurm/logs/").glob("*/*_0_log.out"))
+    feats = ["gpus_per_task", "cutoff", "num_neighbors", "batch_size"]
 
     main(fls, feats)
 
