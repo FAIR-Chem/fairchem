c45ce5d604a7214e887baf94de5ebc63327917b6
Janice Lan <janlan@fb.com>
2021-02-19 14:12:42 -0800

    Add scripts to precompute structures (#24)
    Former-commit-id: c62df880f05edd9cdcc5fdf2af9b0dc7e735f55f

--- src/fairchem/data/oc/ocdata/base_atoms/pkls/__init__.py
+++ src/fairchem/data/oc/ocdata/base_atoms/pkls/__init__.py
@@ -2,4 +2,6 @@ import os
 
 
 BULK_PKL = os.path.join(__path__[0], 'bulks.pkl')
+MAY12_BULK_PKL = os.path.join(__path__[0], 'bulks_may12.pkl')
 ADSORBATE_PKL = os.path.join(__path__[0], 'adsorbates.pkl')
+MAY12_SURFACE_ENUM_PKL = os.path.join(__path__[0], 'for_surface_enumeration_bulk_may12.pkl')
