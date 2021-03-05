2d4abe76ccdda8d85209419c344032b3ca0f8af6
Muhammed Shuaibi <mshuaibi@andrew.cmu.edu>
2021-03-05 16:09:33 +0000

    no grad calls

--- ocpmodels/trainers/pipeline_parallel_trainer.py
+++ ocpmodels/trainers/pipeline_parallel_trainer.py
@@ -249,6 +249,7 @@ class PipelineParallelTrainer(BaseTrainer):
             self.logger.log_plots(plots)
 
     # Takes in a new data source and generates predictions on it.
+    @torch.no_grad()
     def predict(
         self, data_loader, per_image=True, results_file=None, disable_tqdm=True
     ):
