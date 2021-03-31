64034b1903f5b4e90f1e1f9b4c7b02a0f050cae7
Janice Lan <janlan@fb.com>
2021-03-30 17:24:44 -0700

    Documentation for refactored input generation (#32)
    * catch errors in is_config_reasonable, make output dir structure nested
    * docstrings for classes and methods
    * some updates to readme, more updates will be in another PR

--- src/fairchem/data/oc/ocdata/adsorbates.py
+++ src/fairchem/data/oc/ocdata/adsorbates.py
@@ -7,7 +7,19 @@ class Adsorbate():
     '''
     This class handles all things with the adsorbate.
     Selects one (either specified or random), and stores info as an object
+
+    Attributes
+    ----------
+    atoms : Atoms
+        actual atoms of the adsorbate
+    smiles : str
+        SMILES representation of the adsorbate
+    bond_indices : list
+        indices of the atoms meant to be bonded to the surface
+    adsorbate_sampling_str : str
+        string capturing the adsorbate index and total possible adsorbates
     '''
+
     def __init__(self, adsorbate_database, specified_index=None):
         self.choose_adsorbate_pkl(adsorbate_database, specified_index)
 
@@ -16,8 +28,9 @@ class Adsorbate():
         Chooses an adsorbate from our pkl based inverted index at random.
 
         Args:
-            adsorbate_database   A string pointing to the a pkl file that contains
-                                 an inverted index over different adsorbates.
+            adsorbate_database: A string pointing to the a pkl file that contains
+                                an inverted index over different adsorbates.
+            specified_index: adsorbate index to choose instead of choosing a random one
         Sets:
             atoms                    `ase.Atoms` object of the adsorbate
             smiles                   SMILES-formatted representation of the adsorbate
