98efd2e346cdb86201d2b8d344a69f7813f7b8e9
Janice Lan <janlan@fb.com>
2021-04-29 21:55:26 -0700

    Update readme (#34)
    Add more details to readme, including links to db files, workflow diagram, and specifics for OC20.

--- src/fairchem/data/oc/setup.py
+++ src/fairchem/data/oc/setup.py
@@ -1,24 +1,15 @@
-from setuptools import setup, find_packages
-from distutils.command.install import INSTALL_SCHEMES
+"""
+Copyright (c) Facebook, Inc. and its affiliates.
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+"""
 
-
-for scheme in INSTALL_SCHEMES.values():
-    scheme['data'] = scheme['purelib']
+from setuptools import find_packages, setup
 
 setup(name='ocdata',
       version='0.0.1',
       description='Module for generating random catalyst adsorption configurations',
       url='http://github.com/Open-Catalyst-Project/Open-Catalyst-Dataset',
-      author='Pari Palizhati, Kevin Tran, Javi Heras Domingo, Zack Ulissi, Siddharth Goyal',
-      author_email='zulissi@andrew.cmu.edu',
-      license='GPL',
-      platforms=[],
       packages=find_packages(),
-      scripts=[],
-      data_files=[('ocdata/base_atoms/ase_dbs', ['ocdata/base_atoms/ase_dbs/bulks.db',
-                                                 'ocdata/base_atoms/ase_dbs/adsorbates.db']),
-                  ('ocdata/base_atoms/pkls', ['ocdata/base_atoms/pkls/bulks.pkl',
-                                              'ocdata/base_atoms/pkls/adsorbates.pkl'])],
       include_package_data=True,
-      install_requires=['pymatgen==2020.4.2', 'ase>=3.19.1', 'catkit @ git+https://github.com/SUNCAT-Center/CatKit.git#egg=catkit'],
-      long_description='''Module for generating random catalyst adsorption configurations for high-throughput dataset generation.''',)
+)
