5fc99688fd595d46166e58b2c73efece089ee19e
Aini Palizhati <apalizha@andrew.cmu.edu>
2021-08-05 08:18:27 -0700

    code clean up, add additional explanations to code
    Former-commit-id: ae161e6ebea80397dc58fa1c1aef031d88c081a8

--- src/fairchem/data/oc/test/flag_anomaly.py
+++ src/fairchem/data/oc/test/flag_anomaly.py
@@ -14,58 +14,7 @@ from pymatgen.core.structure import SiteCollection
 from pymatgen.core.surface import SlabGenerator
 from pymatgen.analysis.local_env import VoronoiNN
 
-def get_connectivity(atoms, multiple=1):
-    """
-    Generate the connectivity of an atoms obj.
-    Args:
-                atoms      An `ase.Atoms` object
-    Returns:
-                matrix     The connectivity matrix of the atoms object.
-    """
-    cutoff = natural_cutoffs(atoms, mult=multiple)
-    neighborList = neighborlist.NeighborList(cutoff, self_interaction=False, bothways=True)
-    neighborList.update(atoms)
-    matrix = neighborlist.get_connectivity_matrix(neighborList.nl).toarray()
-    return matrix
-
-def find_max_movement(init_atoms, final_atoms):
-    '''
-    Given ase.Atoms objects, find the furthest distance that any single atom in
-    a set of atoms traveled (in Angstroms)
-    Args:
-            init_atoms      `ase.Atoms` of the structure in its initial state
-            final_atoms     `ase.Atoms` of the structure in its final state
-    Returns:
-            max_movement    A float indicating the further movement of any single atom
-                            before and after relaxation (in Angstroms)
-    '''
-    # Calculate the distances for each atom
-    distances = final_atoms.positions - init_atoms.positions
-
-    # Reduce the distances in case atoms wrapped around (the minimum image convention)
-    _, movements = find_mic(distances, final_atoms.cell, final_atoms.pbc)
-    max_movement = max(movements)
-    return max_movement
-
-def is_adsorbate_placed_correct(adslab, atoms_tag):
-    """
-    Make sure all adsorbate atoms are connected after placement.
-    False means there is at least one isolated adsorbate atom.
-    Args:
-            adslab             `ase.Atoms` of the structure in its initial state
-            atoms_tag (list)    0=bulk, 1=surface, 2=adsorbate
-    Returns:
-            boolean    If there is any stand alone adsorbate atoms after placement,
-                       return False.
-
-    """
-    adsorbate_idx = [idx for idx, tag in enumerate(self.atoms_tag) if tag==2]
-    connectivity = get_connectivity(self.init_atoms[adsorbate_idx])
-    if np.any(np.sum(connectivity, axis=0)==0):
-        return False
-    return True
-
-class DetectAnomaly:
+class DetectTrajAnomaly:
     def __init__(self, init_atoms, final_atoms, atoms_tag):
         """
         flag anomalies based on initial and final stucture of a relaxation.
@@ -78,37 +27,33 @@ class DetectAnomaly:
         self.final_atoms = final_atoms
         self.atoms_tag = atoms_tag
 
-    def _is_adsorbate_dissociated(self):
+    def is_adsorbate_dissociated(self):
         """
-        True if the adsorbate is dissociated
+        True if the adsorbate is dissociated.
         """
         # test to see if adsorbate is dissociated from surface
         adsorbate_idx = [idx for idx, tag in enumerate(self.atoms_tag) if tag==2]
 
         # if adsorbate is dissociated, the connectivity matrix would change
-        initial_connectivity = get_connectivity(self.init_atoms[adsorbate_idx])
-        final_connectivity = get_connectivity(self.final_atoms[adsorbate_idx])
-        if np.array_equal(initial_connectivity, final_connectivity):
-            return False
-        return True
+        initial_connectivity = self._get_connectivity(self.init_atoms[adsorbate_idx])
+        final_connectivity = self._get_connectivity(self.final_atoms[adsorbate_idx])
+        return np.array_equal(initial_connectivity, final_connectivity) is False
 
-    def _is_surface_reconstructed(self, slab_movement_thres=3):
+    def is_surface_reconstructed(self, slab_movement_thres=3):
         """
         if any slab atoms moved more than X Angstrom, consider possible reconstruction.
-        A larger X means user is more conversative of what's considered reconstructed.
+        A larger X means the user is more conversative of what's considered reconstructed.
         """
         slab_idx = [idx for idx, tag in enumerate(self.atoms_tag) if tag!=2]
         slab_init = self.init_atoms[slab_idx]
         slab_final = self.final_atoms[slab_idx]
-        max_slab_movement = find_max_movement(slab_init, slab_final)
-        if max_slab_movement >= slab_movement_thres: # A
-            return True
-        return False
+        max_slab_movement = self._find_max_movement(slab_init, slab_final)
+        return max_slab_movement >= slab_movement_thres
 
-    def _is_adsorbate_desorbed(self, neighbor_thres=3):
+    def is_adsorbate_desorbed(self, neighbor_thres=3):
         """
         if the adsorbate binding atoms have no connection with slab atoms,
-        consider it desorbed.
+        consider it desorbed (returns True).
         Args:
             neighbor_thres    Given an atom, threshold (angstorm) for getting connecting
                               neighbor atoms, 3 is a fairly reasonable number.
@@ -124,3 +69,36 @@ class DetectAnomaly:
             if len(surface_neighbors) != 0:
                 return False
         return True
+
+    def _find_max_movement(self, init_atoms, final_atoms):
+        '''
+        Given ase.Atoms objects, find the furthest distance that any single atom in
+        a set of atoms traveled (in Angstroms)
+        Args:
+                init_atoms      `ase.Atoms` of the structure in its initial state
+                final_atoms     `ase.Atoms` of the structure in its final state
+        Returns:
+                max_movement    A float indicating the further movement of any single atom
+                                before and after relaxation (in Angstroms)
+        '''
+        # Calculate the distances for each atom
+        distances = final_atoms.positions - init_atoms.positions
+
+        # Reduce the distances in case atoms wrapped around (the minimum image convention)
+        _, movements = find_mic(distances, final_atoms.cell, final_atoms.pbc)
+        max_movement = max(movements)
+        return max_movement
+
+    def _get_connectivity(self, atoms):
+        """
+        Generate the connectivity of an atoms obj.
+        Args:
+                    atoms      An `ase.Atoms` object
+        Returns:
+                    matrix     The connectivity matrix of the atoms object.
+        """
+        cutoff = natural_cutoffs(atoms)
+        neighborList = neighborlist.NeighborList(cutoff, self_interaction=False, bothways=True)
+        neighborList.update(atoms)
+        matrix = neighborlist.get_connectivity_matrix(neighborList.nl).toarray()
+        return matrix
