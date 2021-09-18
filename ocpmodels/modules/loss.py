ef98b2778ab676e1fad966de005b43653be7b354
Johannes Klicpera <j.klicpera@mailbox.org>
2021-09-18 04:50:35 +0200

    Correctly calculate distributed loss average (#269)
    Co-authored-by: Abhishek Das <das.abhshk@gmail.com>

--- ocpmodels/modules/loss.py
+++ ocpmodels/modules/loss.py
@@ -1,6 +1,8 @@
 import torch
 from torch import nn
 
+from ocpmodels.common import distutils
+
 
 class L2MAELoss(nn.Module):
     def __init__(self, reduction="mean"):
@@ -14,3 +16,25 @@ class L2MAELoss(nn.Module):
             return torch.mean(dists)
         elif self.reduction == "sum":
             return torch.sum(dists)
+
+
+class DDPLoss(nn.Module):
+    def __init__(self, loss_fn, reduction="mean"):
+        super().__init__()
+        self.loss_fn = loss_fn
+        self.loss_fn.reduction = "sum"
+        self.reduction = reduction
+        assert reduction in ["mean", "sum"]
+
+    def forward(self, input: torch.Tensor, target: torch.Tensor):
+        loss = self.loss_fn(input, target)
+        if self.reduction == "mean":
+            num_samples = input.shape[0]
+            num_samples = distutils.all_reduce(
+                num_samples, device=input.device
+            )
+            # Multiply by world size since gradients are averaged
+            # across DDP replicas
+            return loss * distutils.get_world_size() / num_samples
+        else:
+            return loss
