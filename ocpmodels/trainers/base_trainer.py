b9095b33a9bdec0f7ab205553a6ddfdc2894c297
Johannes Klicpera <j.klicpera@mailbox.org>
2021-09-26 02:04:56 +0200

    Fix shared parameter gradient scaling (#283)
    * Fix shared parameter grad scaling
    
    * Fix shared_parameters in GemNet
    
    * Warn once if a shared parameter has no gradient
    
    Co-authored-by: Abhishek Das <das.abhshk@gmail.com>
    Former-commit-id: 60bcc6178ea31c183f8533acc34919866a7511c7

--- ocpmodels/trainers/base_trainer.py
+++ ocpmodels/trainers/base_trainer.py
@@ -593,10 +593,18 @@ class BaseTrainer(ABC):
         self.optimizer.zero_grad()
         loss.backward()
         # Scale down the gradients of shared parameters
-        if hasattr(self.model, "shared_parameters"):
-            for p, factor in self.model.shared_parameters:
-                if p.grad is not None:
+        if hasattr(self.model.module, "shared_parameters"):
+            for p, factor in self.model.module.shared_parameters:
+                if hasattr(p, "grad") and p.grad is not None:
                     p.grad.detach().div_(factor)
+                else:
+                    if not hasattr(self, "warned_shared_param_no_grad"):
+                        self.warned_shared_param_no_grad = True
+                        logging.warning(
+                            "Some shared parameters do not have a gradient. "
+                            "Please check if all shared parameters are used "
+                            "and point to PyTorch parameters."
+                        )
         if self.clip_grad_norm:
             if self.scaler:
                 self.scaler.unscale_(self.optimizer)
