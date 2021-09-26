Fix shared parameter gradient scaling (#283)
* Fix shared parameter grad scaling

* Fix shared_parameters in GemNet

* Warn once if a shared parameter has no gradient

Co-authored-by: Abhishek Das <das.abhshk@gmail.com>

ocpmodels/models/gemnet/gemnet.py
@@ -23,9 +23,7 @@ from .layers.atom_update_block import OutputBlock
 from .layers.base_layers import Dense
 from .layers.efficient import EfficientInteractionDownProjection
 from .layers.embedding_block import AtomEmbedding, EdgeEmbedding
-from .layers.interaction_block import (
-    InteractionBlockTripletsOnly,
-)
+from .layers.interaction_block import InteractionBlockTripletsOnly
 from .layers.radial_basis import RadialBasis
 from .layers.scaling import AutomaticFit
 from .layers.spherical_basis import CircularBasisLayer
@@ -252,10 +250,10 @@ class GemNetT(torch.nn.Module):
         self.int_blocks = torch.nn.ModuleList(int_blocks)
 
         self.shared_parameters = [
-            (self.mlp_rbf3, self.num_blocks),
-            (self.mlp_cbf3, self.num_blocks),
-            (self.mlp_rbf_h, self.num_blocks),
-            (self.mlp_rbf_out, self.num_blocks + 1),
+            (self.mlp_rbf3.linear.weight, self.num_blocks),
+            (self.mlp_cbf3.weight, self.num_blocks),
+            (self.mlp_rbf_h.linear.weight, self.num_blocks),
+            (self.mlp_rbf_out.linear.weight, self.num_blocks + 1),
         ]
 
     def get_triplets(self, edge_index, num_atoms):