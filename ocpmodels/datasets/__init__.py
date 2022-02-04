a9953b737be06420485cbb185439596d87e2019e
Muhammed Shuaibi <mshuaibi@andrew.cmu.edu>
2022-02-04 14:49:00 +0000

    fix imports
    Former-commit-id: a6c792fe04115c2ceb0c011df44f2b1508bba05d

--- ocpmodels/datasets/__init__.py
+++ ocpmodels/datasets/__init__.py
@@ -3,11 +3,4 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-__all__ = [
-    "SinglePointLmdbDataset",
-    "TrajectoryLmdbDataset",
-    "data_list_collater",
-]
-
-from .single_point_lmdb import SinglePointLmdbDataset
-from .trajectory_lmdb import TrajectoryLmdbDataset, data_list_collater
+from .oc20_lmdb import OC20LmdbDataset, data_list_collater
