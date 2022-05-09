e9e90665eb6076033f3583d8ebb0c1708c7a0f10
AlexDuvalinho <afonsalex@free.fr>
2022-05-09 17:00:02 -0400

    adapt unit tests for original GNNs

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=test
 #SBATCH --ntasks=1
-#SBATCH --time=20:00:00
+#SBATCH --time=24:00:00
 #SBATCH --mem=32GB
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:4
 #SBATCH --output=slurm_res/output-job.txt
 #SBATCH --error=slurm_res/error-job.txt
 
