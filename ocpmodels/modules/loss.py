Corrects relative weighting of force loss with no. of atoms per structure

ocpmodels/modules/loss.py
@@ -18,6 +18,24 @@ class L2MAELoss(nn.Module):
             return torch.sum(dists)
 
 
+class AtomwiseMSELoss(nn.Module):
+    def __init__(self, reduction="mean"):
+        super().__init__()
+        self.reduction = reduction
+        assert reduction in ["mean", "sum"]
+
+    def forward(
+        self, input: torch.Tensor, target: torch.Tensor, natoms: torch.Tensor
+    ):
+        assert natoms.shape[0] == input.shape[0] == target.shape[0]
+        assert len(natoms.shape) == 1  # (nAtoms, )
+        loss = natoms * ((input - target) ** 2).mean(-1)  # Avg across x,y,z
+        if self.reduction == "mean":
+            return loss.mean()
+        elif self.reduction == "sum":
+            return loss.sum()
+
+
 class DDPLoss(nn.Module):
     def __init__(self, loss_fn, reduction="mean"):
         super().__init__()
@@ -26,10 +44,21 @@ class DDPLoss(nn.Module):
         self.reduction = reduction
         assert reduction in ["mean", "sum"]
 
-    def forward(self, input: torch.Tensor, target: torch.Tensor):
-        loss = self.loss_fn(input, target)
+    def forward(
+        self,
+        input: torch.Tensor,
+        target: torch.Tensor,
+        natoms: torch.Tensor = None,
+        batch_size: int = None,
+    ):
+        if natoms is None:
+            loss = self.loss_fn(input, target)
+        else:  # atom-wise loss
+            loss = self.loss_fn(input, target, natoms)
         if self.reduction == "mean":
-            num_samples = input.shape[0]
+            num_samples = (
+                batch_size if batch_size is not None else input.shape[0]
+            )
             num_samples = distutils.all_reduce(
                 num_samples, device=input.device
             )

ocpmodels/trainers/base_trainer.py
@@ -41,7 +41,7 @@ from ocpmodels.modules.evaluator import Evaluator
 from ocpmodels.modules.exponential_moving_average import (
     ExponentialMovingAverage,
 )
-from ocpmodels.modules.loss import DDPLoss, L2MAELoss
+from ocpmodels.modules.loss import AtomwiseMSELoss, DDPLoss, L2MAELoss
 from ocpmodels.modules.normalizer import Normalizer
 from ocpmodels.modules.scheduler import LRScheduler
 
@@ -442,6 +442,8 @@ class BaseTrainer(ABC):
                 self.loss_fn[loss] = nn.MSELoss()
             elif loss_name == "l2mae":
                 self.loss_fn[loss] = L2MAELoss()
+            elif loss_name == "atomwisemse":
+                self.loss_fn[loss] = AtomwiseMSELoss()
             else:
                 raise NotImplementedError(
                     f"Unknown loss function name: {loss_name}"

ocpmodels/trainers/forces_trainer.py
@@ -489,24 +489,46 @@ class ForcesTrainer(BaseTrainer):
                 loss.append(train_loss_force_normalized)
 
             else:
-                # Force coefficient = 30 has been working well for us.
-                force_mult = self.config["optim"].get("force_coefficient", 30)
                 if self.config["task"].get("train_on_free_atoms", False):
+                    force_mult = self.config["optim"].get(
+                        "force_coefficient", 1
+                    )
                     fixed = torch.cat(
                         [batch.fixed.to(self.device) for batch in batch_list]
                     )
                     mask = fixed == 0
-                    loss.append(
-                        force_mult
-                        * self.loss_fn["force"](
-                            out["forces"][mask], force_target[mask]
+                    if self.config["optim"]["loss_force"] == "atomwisemse":
+                        natoms = torch.cat(
+                            [
+                                batch.natoms.to(self.device)
+                                for batch in batch_list
+                            ]
                         )
-                    )
+                        natoms = torch.repeat_interleave(natoms, natoms)
+                        force_loss = force_mult * self.loss_fn["force"](
+                            out["forces"][mask],
+                            force_target[mask],
+                            natoms=natoms[mask],
+                            batch_size=self.config["optim"]["batch_size"],
+                        )
+                        loss.append(force_loss)
+                    else:
+                        loss.append(
+                            force_mult
+                            * self.loss_fn["force"](
+                                out["forces"][mask], force_target[mask]
+                            )
+                        )
                 else:
+                    # Force coefficient = 30 has been working well for us.
+                    force_mult = self.config["optim"].get(
+                        "force_coefficient", 30
+                    )
                     loss.append(
                         force_mult
                         * self.loss_fn["force"](out["forces"], force_target)
                     )
+
         # Sanity check to make sure the compute graph is correct.
         for lc in loss:
             assert hasattr(lc, "grad_fn")