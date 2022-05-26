f0526221025d6635f2f10ce97778582dfbff8f1b
Muhammed Shuaibi <mshuaibi@andrew.cmu.edu>
2022-05-26 15:28:46 +0000

    atomwise l2 bugfix for no-ddp

--- ocpmodels/modules/loss.py
+++ ocpmodels/modules/loss.py
@@ -25,7 +25,11 @@ class AtomwiseL2Loss(nn.Module):
         assert reduction in ["mean", "sum"]
 
     def forward(
-        self, input: torch.Tensor, target: torch.Tensor, natoms: torch.Tensor
+        self,
+        input: torch.Tensor,
+        target: torch.Tensor,
+        natoms: torch.Tensor,
+        batch_size: torch.Tensor = None,  # not used
     ):
         assert natoms.shape[0] == input.shape[0] == target.shape[0]
         assert len(natoms.shape) == 1  # (nAtoms, )
