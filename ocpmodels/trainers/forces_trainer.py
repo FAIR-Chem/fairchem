FAIR-Chem/fairchem

commit 041596977f7f0038e77f78ead7dcd7eb1be125ea

Make sure best val metric is loaded when restoring checkpoint (#349)
Former-commit-id: 5fb90e7e68d411299a6a33a50d1f50f103ac7390

ocpmodels/trainers/forces_trainer.py
@@ -291,7 +291,13 @@ class ForcesTrainer(BaseTrainer):
         primary_metric = self.config["task"].get(
             "primary_metric", self.evaluator.task_primary_metric[self.name]
         )
-        self.best_val_metric = 1e9 if "mae" in primary_metric else -1.0
+        if (
+            self.primary_metric is None
+            or self.primary_metric != primary_metric
+        ):
+            self.best_val_metric = 1e9 if "mae" in primary_metric else -1.0
+        else:
+            primary_metric = self.primary_metric
         self.metrics = {}
 
         # Calculate start_epoch from step instead of loading the epoch number

ocpmodels/trainers/base_trainer.py
@@ -394,6 +394,8 @@ class BaseTrainer(ABC):
         checkpoint = torch.load(checkpoint_path, map_location=map_location)
         self.epoch = checkpoint.get("epoch", 0)
         self.step = checkpoint.get("step", 0)
+        self.best_val_metric = checkpoint.get("best_val_metric", None)
+        self.primary_metric = checkpoint.get("primary_metric", None)
 
         # Load model, optimizer, normalizer state dict.
         # if trained with ddp and want to load in non-ddp, modify keys from
@@ -527,6 +529,11 @@ class BaseTrainer(ABC):
                         "amp": self.scaler.state_dict()
                         if self.scaler
                         else None,
+                        "best_val_metric": self.best_val_metric,
+                        "primary_metric": self.config["task"].get(
+                            "primary_metric",
+                            self.evaluator.task_primary_metric[self.name],
+                        ),
                     },
                     checkpoint_dir=self.config["cmd"]["checkpoint_dir"],
                     checkpoint_file=checkpoint_file,

commit 66bacfd5c277f8935b6b60525168629bf6f6d867

Fixes bug introduced in #349

ocpmodels/trainers/forces_trainer.py
@@ -292,7 +292,7 @@ class ForcesTrainer(BaseTrainer):
             "primary_metric", self.evaluator.task_primary_metric[self.name]
         )
         if (
-            self.primary_metric is None
+            not hasattr(self, "primary_metric")
             or self.primary_metric != primary_metric
         ):
             self.best_val_metric = 1e9 if "mae" in primary_metric else -1.0