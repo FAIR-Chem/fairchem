5eef0d16cc60c12e5256d9e961e24652b1cfd522
Muhammed Shuaibi <mshuaibi@andrew.cmu.edu>
2022-07-07 19:36:38 +0000

    add license header
    Former-commit-id: 413027744b8fd4152bc7950994ab907f22f4914f

--- ocpmodels/models/gemnet_oc/layers/spherical_basis.py
+++ ocpmodels/models/gemnet_oc/layers/spherical_basis.py
@@ -1,3 +1,9 @@
+"""
+Copyright (c) Facebook, Inc. and its affiliates.
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+"""
+
 import torch
 
 from .basis_utils import get_sph_harm_basis
