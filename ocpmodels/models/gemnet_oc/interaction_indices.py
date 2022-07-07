8af12d699e7a8810e51af4960f29db6a93c575ec
Muhammed Shuaibi <mshuaibi@andrew.cmu.edu>
2022-07-07 19:36:38 +0000

    add license header

--- ocpmodels/models/gemnet_oc/interaction_indices.py
+++ ocpmodels/models/gemnet_oc/interaction_indices.py
@@ -1,3 +1,9 @@
+"""
+Copyright (c) Facebook, Inc. and its affiliates.
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+"""
+
 import torch
 from torch_scatter import segment_coo
 from torch_sparse import SparseTensor
