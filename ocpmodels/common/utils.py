b892033de817a07ed71a4da688e9ee003b24c134
Muhammed Shuaibi <45150244+mshuaibii@users.noreply.github.com>
2022-07-12 13:01:52 -0400

    Bugfix: Data created with PyG>2.0 (#367)
    Former-commit-id: 8ec6cff61604ae3c8a4a85caa0d8b432df6d7edd

--- ocpmodels/common/utils.py
+++ ocpmodels/common/utils.py
@@ -33,8 +33,11 @@ from torch_scatter import segment_coo, segment_csr
 
 
 def pyg2_data_transform(data: Data):
-    # if we're on the new pyg (2.0 or later), we need to convert the data to the new format
-    if torch_geometric.__version__ >= "2.0":
+    """
+    if we're on the new pyg (2.0 or later) and if the Data stored is in older format
+    we need to convert the data to the new format
+    """
+    if torch_geometric.__version__ >= "2.0" and "_store" not in data.__dict__:
         return Data(
             **{k: v for k, v in data.__dict__.items() if v is not None}
         )
