b8462bb991b91711925af97d08a52b6200125724
Nima Shoghi <nimashoghi@fb.com>
2022-09-07 15:00:26 -0400

    Build and tests on macos and windows (#401)
    * Build and test on macos and windows as well
    
    * Make all model tests use the registry
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Updated `setup_imports` to work with Windows
    
    * Decrease default tolerances so tests pass on Windows
    Remove atol/rtol from serialized approx representation
    
    * Fix excpetion during import error construction when registry is empty
    
    * Fix snapshot parse error
    
    * Upgrade `certifi` on Windows to fix SSL issues
    
    * Install/update `python-certifi-win32` on Windows as well
    
    * Try using `pip-system-certs` package to fix SSL issues
    
    * Trigger CI build without invalidating cache
    
    * Change checkpoint loading to use requests module
    
    * Read checkpoint bytes into io.BytesIO before loading
    
    Co-authored-by: Abhishek Das <das.abhshk@gmail.com>

--- tests/models/test_gemnet.py
+++ tests/models/test_gemnet.py
@@ -14,9 +14,10 @@ import pytest
 import torch
 from ase.io import read
 
+from ocpmodels.common.registry import registry
 from ocpmodels.common.transforms import RandomRotate
+from ocpmodels.common.utils import setup_imports
 from ocpmodels.datasets import data_list_collater
-from ocpmodels.models import GemNetT
 from ocpmodels.preprocessing import AtomsToGraphs
 
 
@@ -41,7 +42,9 @@ def load_data(request):
 @pytest.fixture(scope="class")
 def load_model(request):
     torch.manual_seed(4)
-    model = GemNetT(
+    setup_imports()
+
+    model = registry.get_model_class("gemnet_t")(
         None,
         -1,
         1,
