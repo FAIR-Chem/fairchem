3d6ab604edb10257ac725a9fce869f2771c0e801
Nima Shoghi <nimashoghi@fb.com>
2022-09-13 18:21:41 -0400

    Consistent scaling module across models, stricter consistency checks (#390)
    * Moved all trainer context setup code to `new_trainer_context` function
    
    * Updated `BaseTrainer.save` to return the saved checkpoint path
    
    * Added scaling module for creating scale factors that are stored in the model checkpoint as buffers
    
    * Updated GemNet to use new scaling module
    
    * Updated GemNet-GP to use new scaling module
    
    * Updated GemNet-OC to use new scaling module
    
    * Updated PaiNN to use new scaling module
    
    * Added backwards compatibility with old `.json` and `.pt` scaling modules
    Fixed bug with GemNet and GemNet GP
    
    * Updated backwards compatibility to work with checkpoints with training state
    Updated `fit.py` script to allow for updating old checkpoints to new format
    
    * Support explicit scale factors dicts
    
    * Updated `load_scales_compat` to patch the model's `load_state_dict` to ignore missing scale factors
    
    * Format base_trainer
    
    * Reverted `load_state_dict` patching: This didn't work because `load_state_dict` isn't called on child modules
    Created our own `load_state_dict` wrapper util that ignores scale factor keys
    
    * Added consistency check when scale factors are loaded multiple times (e.g., when scale factors are loaded using both traditional and new methods)
    
    * Added error when `scale_file` doesn't exist
    Added warning for empty scale dict
    
    * Added support for checkpoints with the "scale_dict" key
    
    * Added check for unfitted scale factors before train/val/predict/run-relaxations
    
    Co-authored-by: Abhishek Das <das.abhshk@gmail.com>
    Former-commit-id: 366803fb582f8fbc381c996abab3809904bb7d48

--- ocpmodels/models/gemnet_oc/gemnet_oc.py
+++ ocpmodels/models/gemnet_oc/gemnet_oc.py
@@ -22,6 +22,7 @@ from ocpmodels.common.utils import (
     radius_graph_pbc,
 )
 from ocpmodels.models.base import BaseModel
+from ocpmodels.modules.scaling.compat import load_scales_compat
 
 from .initializers import get_initializer
 from .interaction_indices import (
@@ -36,7 +37,6 @@ from .layers.embedding_block import AtomEmbedding, EdgeEmbedding
 from .layers.force_scaler import ForceScaler
 from .layers.interaction_block import InteractionBlock
 from .layers.radial_basis import RadialBasis
-from .layers.scaling import ScaledModule
 from .layers.spherical_basis import CircularBasisLayer, SphericalBasisLayer
 from .utils import (
     get_angle,
@@ -49,7 +49,7 @@ from .utils import (
 
 
 @registry.register_model("gemnet_oc")
-class GemNetOC(ScaledModule, BaseModel):
+class GemNetOC(BaseModel):
     """
     Arguments
     ---------
@@ -229,7 +229,6 @@ class GemNetOC(ScaledModule, BaseModel):
         forces_coupled: bool = False,
         output_init: str = "HeOrthogonal",
         activation: str = "silu",
-        scale_file: Optional[str] = None,
         quad_interaction: bool = False,
         atom_edge_interaction: bool = False,
         edge_atom_interaction: bool = False,
@@ -238,6 +237,7 @@ class GemNetOC(ScaledModule, BaseModel):
         qint_tags: list = [0, 1, 2],
         num_elements: int = 83,
         otf_graph: bool = False,
+        scale_file: Optional[str] = None,
         **kwargs,  # backwards compatibility with deprecated arguments
     ):
         super().__init__()
@@ -379,29 +379,7 @@ class GemNetOC(ScaledModule, BaseModel):
         if direct_forces:
             self.out_forces.reset_parameters(out_initializer)
 
-        # Load scaling factors
-        if scale_file is not None:
-            if isinstance(scale_file, str) and os.path.isfile(scale_file):
-                scales = torch.load(scale_file, map_location="cpu")
-                self.load_scales(scales)
-            elif isinstance(scale_file, dict):
-                scales = scale_file
-                self.load_scales(scales)
-            else:
-                logging.error(
-                    f"Scale file '{scale_file}' does not exist. "
-                    f"If you're running a pretrained checkpoint, "
-                    f"predictions will be inaccurate without the "
-                    f"scale file used during training. "
-                    f"If you do not want to use a scale file, remove "
-                    f"the `scale_file` line from the config yaml."
-                )
-                raise FileNotFoundError
-        else:
-            logging.warning(
-                "`scale_file` is set to `None`. "
-                "The model will use unit scaling factors. "
-            )
+        load_scales_compat(self, scale_file)
 
     def set_cutoffs(self, cutoff, cutoff_qint, cutoff_aeaint, cutoff_aint):
         self.cutoff = cutoff
