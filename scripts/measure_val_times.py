731f728b1a55c1b7c07ddae8067cd3df9376bcb4
Victor Schmidt <vsch@pm.me>
2022-09-23 18:22:52 -0400

    docstrings

--- scripts/measure_val_times.py
+++ scripts/measure_val_times.py
@@ -1,3 +1,13 @@
+"""
+Python script to time a list of OCP trainer configurations
+|
+use as:
+|
+$ pyhton scripts/measure_val_times.py n_runs=5 output_filename="times_n_runs_5.json"
+|
+Re-running with the same output_filename will resume timing, ignoring already timed
+configs. A config is considered timed after all its runs are completed.
+"""
 import json
 import os
 import sys
@@ -25,13 +35,38 @@ except:  # noqa: E722
     pass
 
 
-def print_time_stats(times_dict, keylen=55):
+def print_time_stats(times_dict: dict, keylen: int = 55) -> None:
+    """
+    Prints a dictionnary of time statistics
+
+    Args:
+        times_dict (dict): the time to print, expects "mean" and "std" sub keys
+        keylen (int, optional): the length of the keys to have uniform spacing.
+            Defaults to 55.
+    """
     for note, s in times_dict.items():
         n = note + "  " + "⸱" * (keylen - len(note))
         print(f"• {n} {s['mean']:8.4f}s +/- {s['std']:.4f}s")
 
 
-def save_and_print(output_json, times, rewiring_times, configs, TRAINER_CONF_OVERRIDES):
+def save_and_print(
+    output_json: Path,
+    times: dict,
+    rewiring_times: dict,
+    configs: dict,
+    TRAINER_CONF_OVERRIDES: dict,
+) -> None:
+    """
+    Checkpoints the timing state and prints a summary of its keys
+
+    Args:
+        output_json (Path): Where to write the checkpoint
+        times (dict): Trainer timings
+        rewiring_times (dict): Graph rewiring timings
+        configs (dict): The trainer configs we're timing
+        TRAINER_CONF_OVERRIDES (dict): The trainer's overriding args (like
+            batch_size etc.)
+    """
 
     val_times = {
         note: [sum(epoch) for epoch in epochs] for note, epochs in times.items()
@@ -555,6 +590,7 @@ ALL_CONFIGS = [
 
 if __name__ == "__main__":
 
+    # parse command-line arguments
     args = resolved_args(
         defaults={
             "n_runs": 1,  # runs for the same trainer
@@ -568,11 +604,14 @@ if __name__ == "__main__":
         }
     ).pretty_print()
 
+    # safe-guard all configs
     configs = deepcopy(ALL_CONFIGS)
 
     if args.max_confs > 0:
+        # crop the number of confs to process, for debugging purposes
         configs = configs[: args.max_confs]
     elif args.conf_idx is not None:
+        # select a single conf
         if args.max_confs > 0:
             print(
                 "\nWARNING: `conf_idx` has precedence over `max_confs`",
@@ -580,54 +619,78 @@ if __name__ == "__main__":
             )
         configs = [configs[args.conf_idx]]
 
+    # initiate the time and rewiring time data stores
     times = {c["overrides"]["note"]: [[] for _ in range(args.n_runs)] for c in configs}
-
     rewiring_times = {}
 
+    # disable gradients for validation inference
     torch.set_grad_enabled(False)
+
+    # create the output folder and file
     output_dir = Path(__file__).resolve().parent.parent / "data" / "times"
     output_dir.mkdir(exist_ok=True, parents=True)
     output_json = output_dir / args.output_filename
 
+    # existing_confs is initially empty
     existing_confs = set()
-
     if output_json.exists() and not args.overwrite and not args.dryrun:
+        # there's an output file: use it to load existing timings
         with output_json.open("r") as f:
             existing_times = json.load(f)
+
+        # overwrite times and rewiring_times
         times = existing_times["times"]
         rewiring_times = existing_times["rewiring_times"]
+        # update existing_confs
         existing_confs = set(k for k in times if any(times[k]))
 
+    # for each trainer configuration
     for c, config in enumerate(configs):
         note = config["overrides"]["note"]
         if note in existing_confs:
-            print("🤠 Data for", note, "already exists. Skipping.")
+            # config has already been timed: ignore
+            print(f"🤠 Data for `{note}` already exists. Skipping.")
             continue
+
+        # create trainer in eval modefor this config
         trainer = make_trainer(**config, verbose=False)
         trainer.model.eval()
+
+        # for each run (to have stats)
         for r in range(args.n_runs):
 
+            # print status
             print(
                 f"\n\n🔄 Timing {note} ({c+1}/{len(configs)})",
                 f"-> Run {r+1}/{args.n_runs}\n\n",
             )
 
+            # for each batch in the val-id dataset
             for b, batch in enumerate(tqdm(trainer.val_loader, desc=note)):
+                # time the forward pass
                 t = time()
                 with torch.cuda.amp.autocast(enabled=trainer.scaler is not None):
                     _ = trainer._forward(batch)
                 forward_duration = time() - t
+
                 if trainer.model.module.graph_rewiring:
+                    # remove the rewiring time which really should be done in
+                    # the data loader
                     forward_duration -= trainer.model.module.rewiring_time
+                    # store rewiring time
                     if note not in rewiring_times:
                         rewiring_times[note] = []
                     if len(rewiring_times[note]) == r:
                         rewiring_times[note].append([])
                     rewiring_times[note][r].append(trainer.model.module.rewiring_time)
+
+                # store forward time
                 times[note][r].append(forward_duration)
                 if args.max_batchs > 0 and b > args.max_batchs - 1:
+                    # break for debugs
                     break
 
+        # all runs have been completed for this config: checkpoint timing state
         if not args.dryrun:
             save_and_print(
                 output_json, times, rewiring_times, configs, TRAINER_CONF_OVERRIDES
