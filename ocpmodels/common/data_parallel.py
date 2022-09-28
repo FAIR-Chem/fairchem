caee912ef79eebdea0d418f9afa01a561fa8a72f
Nima Shoghi <nimashoghi@fb.com>
2022-09-28 10:33:18 -0400

    Balanced batch sampler fix (#406)
    * Updated `BalancedBatchSampler` to ignore incompatible datasets
    
    * Add warning message when loading incompatible dataset
    
    * Better warning message
    
    * If balancing is disabled, we don't have to check `self.balance_batches` every iteration.
    
    * Allow boolean values for optim.load_balancing
    Code cleanup
    
    * Removed code duplication for balanced batch sampler
    Added tests for balanced batch sampler
    
    * Updated tests to pass on Windows
    
    * Fix failing windows tests
    
    
    Former-commit-id: cfe706b0c30d4735cc431aa3d310dc06766f124d

--- ocpmodels/common/data_parallel.py
+++ ocpmodels/common/data_parallel.py
@@ -8,6 +8,8 @@ LICENSE file in the root directory of this source tree.
 import heapq
 import logging
 from itertools import chain
+from pathlib import Path
+from typing import List, Literal, Protocol, Union, runtime_checkable
 
 import numba
 import numpy as np
@@ -130,7 +132,32 @@ def balanced_partition(sizes, num_parts):
     return idx_balanced
 
 
+@runtime_checkable
+class _HasMetadata(Protocol):
+    @property
+    def metadata_path(self) -> Path:
+        ...
+
+
 class BalancedBatchSampler(Sampler):
+    def _load_dataset(self, dataset, mode: Literal["atoms", "neighbors"]):
+        errors: List[str] = []
+        if not isinstance(dataset, _HasMetadata):
+            errors.append(
+                f"Dataset {dataset} does not have a metadata_path attribute."
+            )
+            return None, errors
+        if not dataset.metadata_path.exists():
+            errors.append(
+                f"Metadata file {dataset.metadata_path} does not exist."
+            )
+            return None, errors
+
+        key = {"atoms": "natoms", "neighbors": "neighbors"}[mode]
+        sizes = np.load(dataset.metadata_path)[key]
+
+        return sizes, errors
+
     def __init__(
         self,
         dataset,
@@ -138,56 +165,33 @@ class BalancedBatchSampler(Sampler):
         num_replicas,
         rank,
         device,
-        mode="atoms",
+        mode: Union[str, bool] = "atoms",
         shuffle=True,
         drop_last=False,
         force_balancing=False,
+        throw_on_error=False,
     ):
+        if mode is True:
+            mode = "atoms"
+
+        if isinstance(mode, str):
+            mode = mode.lower()
+            if mode not in ("atoms", "neighbors"):
+                raise ValueError(
+                    f"Invalid mode {mode}. Must be one of 'atoms', 'neighbors', or a boolean."
+                )
+
         self.dataset = dataset
         self.batch_size = batch_size
         self.num_replicas = num_replicas
         self.rank = rank
         self.device = device
-        self.mode = mode.lower()
+        self.mode = mode
         self.shuffle = shuffle
         self.drop_last = drop_last
 
-        self.balance_batches = self.num_replicas > 1
-        if self.balance_batches:
-            if (
-                not hasattr(dataset, "metadata_path")
-                or not dataset.metadata_path.is_file()
-            ):
-                if force_balancing:
-                    logging.warning(
-                        f"No metadata file found at '{dataset.metadata_path}'. "
-                        "BalancedBatchSampler has to load the data to "
-                        "determine batch sizes, which incurs "
-                        "significant overhead!"
-                    )
-                    self.sizes = None
-                else:
-                    logging.warning(
-                        f"No metadata file found at '{dataset.metadata_path}'. "
-                        "Batches will not be balanced, "
-                        "which can incur significant overhead!"
-                    )
-                    self.balance_batches = False
-                    self.sizes = None
-            else:
-                if self.mode == "atoms":
-                    self.sizes = np.load(dataset.metadata_path)["natoms"]
-                elif self.mode == "neighbors":
-                    self.sizes = np.load(dataset.metadata_path)["neighbors"]
-                else:
-                    raise NotImplementedError(
-                        f"Unknown load balancing mode: {self.mode}"
-                    )
-        else:
-            self.sizes = None
-
         self.single_sampler = DistributedSampler(
-            dataset,
+            self.dataset,
             num_replicas=num_replicas,
             rank=rank,
             shuffle=shuffle,
@@ -199,6 +203,43 @@ class BalancedBatchSampler(Sampler):
             drop_last=drop_last,
         )
 
+        self.sizes = None
+        self.balance_batches = False
+
+        if self.num_replicas <= 1:
+            logging.info(
+                "Batch balancing is disabled for single GPU training."
+            )
+            return
+
+        if self.mode is False:
+            logging.info(
+                "Batch balancing is disabled because `optim.load_balancing` is `False`"
+            )
+            return
+
+        self.sizes, errors = self._load_dataset(dataset, self.mode)
+        if self.sizes is None:
+            self.balance_batches = force_balancing
+            if force_balancing:
+                errors.append(
+                    "BalancedBatchSampler has to load the data to  determine batch sizes, which incurs significant overhead! "
+                    "You can disable balancing by setting `optim.load_balancing` to `False`."
+                )
+            else:
+                errors.append(
+                    "Batches will not be balanced, which can incur significant overhead!"
+                )
+        else:
+            self.balance_batches = True
+
+        if errors:
+            msg = "BalancedBatchSampler: " + " ".join(errors)
+            if throw_on_error:
+                raise RuntimeError(msg)
+            else:
+                logging.warning(msg)
+
     def __len__(self):
         return len(self.batch_sampler)
 
@@ -206,42 +247,39 @@ class BalancedBatchSampler(Sampler):
         self.single_sampler.set_epoch(epoch)
 
     def __iter__(self):
+        if not self.balance_batches:
+            yield from self.batch_sampler
+            return
+
         for batch_idx in self.batch_sampler:
-            if self.balance_batches:
-                if self.sizes is None:
-                    # Unfortunately, we need to load the data to know the image sizes
-                    data_list = [self.dataset[idx] for idx in batch_idx]
+            if self.sizes is None:
+                # Unfortunately, we need to load the data to know the image sizes
+                data_list = [self.dataset[idx] for idx in batch_idx]
 
-                    if self.mode == "atoms":
-                        sizes = [data.num_nodes for data in data_list]
-                    elif self.mode == "neighbors":
-                        sizes = [
-                            data.edge_index.shape[1] for data in data_list
-                        ]
-                    else:
-                        raise NotImplementedError(
-                            f"Unknown load balancing mode: {self.mode}"
-                        )
+                if self.mode == "atoms":
+                    sizes = [data.num_nodes for data in data_list]
+                elif self.mode == "neighbors":
+                    sizes = [data.edge_index.shape[1] for data in data_list]
                 else:
-                    sizes = [self.sizes[idx] for idx in batch_idx]
-
-                idx_sizes = torch.stack(
-                    [torch.tensor(batch_idx), torch.tensor(sizes)]
-                )
-                idx_sizes_all = distutils.all_gather(
-                    idx_sizes, device=self.device
-                )
-                idx_sizes_all = torch.cat(idx_sizes_all, dim=-1).cpu()
-                if gp_utils.initialized():
-                    idx_sizes_all = torch.unique(input=idx_sizes_all, dim=1)
-                idx_all = idx_sizes_all[0]
-                sizes_all = idx_sizes_all[1]
-
-                local_idx_balanced = balanced_partition(
-                    sizes_all.numpy(), num_parts=self.num_replicas
-                )
-                # Since DistributedSampler pads the last batch
-                # this should always have an entry for each replica.
-                yield idx_all[local_idx_balanced[self.rank]]
+                    raise NotImplementedError(
+                        f"Unknown load balancing mode: {self.mode}"
+                    )
             else:
-                yield batch_idx
+                sizes = [self.sizes[idx] for idx in batch_idx]
+
+            idx_sizes = torch.stack(
+                [torch.tensor(batch_idx), torch.tensor(sizes)]
+            )
+            idx_sizes_all = distutils.all_gather(idx_sizes, device=self.device)
+            idx_sizes_all = torch.cat(idx_sizes_all, dim=-1).cpu()
+            if gp_utils.initialized():
+                idx_sizes_all = torch.unique(input=idx_sizes_all, dim=1)
+            idx_all = idx_sizes_all[0]
+            sizes_all = idx_sizes_all[1]
+
+            local_idx_balanced = balanced_partition(
+                sizes_all.numpy(), num_parts=self.num_replicas
+            )
+            # Since DistributedSampler pads the last batch
+            # this should always have an entry for each replica.
+            yield idx_all[local_idx_balanced[self.rank]]
