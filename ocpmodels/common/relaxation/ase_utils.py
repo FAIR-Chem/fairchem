3e127762c0d314abbc5942fed5266c1fa36740b1
Muhammed Shuaibi <mshuaibi@andrew.cmu.edu>
2022-09-30 11:00:14 -0700

    fix for cps with relax datasets

--- ocpmodels/common/relaxation/ase_utils.py
+++ ocpmodels/common/relaxation/ase_utils.py
@@ -138,6 +138,11 @@ class OCPCalculator(Calculator):
             config["model_attributes"]["name"] = config.pop("model")
             config["model"] = config["model_attributes"]
 
+        # for checkpoints with relaxation datasets defined, remove to avoid
+        # unnecesarily trying to load that dataset
+        if "relax_dataset" in config["task"]:
+            del config["task"]["relax_dataset"]
+
         # Calculate the edge indices on the fly
         config["model"]["otf_graph"] = True
 
