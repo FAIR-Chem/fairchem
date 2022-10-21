d38bdfe36936936bccd51b45ca831a1c77421901
Brook Wander <brook.l.wander@gmail.com>
2022-10-21 18:42:28 +0000

    updating with reverse method applied too
    Former-commit-id: e29f08cefb0bb639f95f6e2348598263a7fb3716

--- src/fairchem/data/oc/ocdata/test/flag_anomaly.py
+++ src/fairchem/data/oc/ocdata/test/flag_anomaly.py
@@ -11,38 +11,35 @@ class DetectTrajAnomaly:
         final_atoms,
         atoms_tag,
         final_slab_atoms=None,
-        reconstruction_cutoff_multiplier=1.5,
+        surface_change_cutoff_multiplier=1.5,
+        desorption_cutoff_multiplier = 1.3,
     ):
         """
         Flag anomalies based on initial and final stucture of a relaxation.
 
         Args:
-               init_atoms (ase.Atoms): the adslab in its initial state
-               final_atoms (ase.Atoms): the adslab in its final state
-               atoms_tag (list): the atom tags; 0=bulk, 1=surface, 2=adsorbate
-               final_slab_atoms (ase.Atoms, optional): the relaxed slab if unspecified this defaults
-                   to using the initial adslab instead.
-               reconstruction_cutoff_multiplier (float): cushion for small atom movements when assessing
-                    atom connectivity for reconstruction
+            init_atoms (ase.Atoms): the adslab in its initial state
+            final_atoms (ase.Atoms): the adslab in its final state
+            atoms_tag (list): the atom tags; 0=bulk, 1=surface, 2=adsorbate
+            final_slab_atoms (ase.Atoms, optional): the relaxed slab if unspecified this defaults
+            to using the initial adslab instead.
+            surface_change_cutoff_multiplier (float, optional): cushion for small atom movements
+                when assessing atom connectivity for reconstruction
+            desorption_cutoff_multiplier (float, optional): cushion for physisorbed systems to not
+                be discarded. Applied to the covalent radii.
         """
         self.init_atoms = init_atoms
         self.final_atoms = final_atoms
         self.final_slab_atoms = final_slab_atoms
         self.atoms_tag = atoms_tag
-        self.initial_connectivity = self._get_connectivity(self.init_atoms)
-        self.final_connectivity = self._get_connectivity(self.final_atoms)
-        self.final_connectivity_desorption = self._get_connectivity(
-            self.final_atoms, 1.3
-        )
-        if self.final_slab_atoms is not None:
-            self.slab_connectivity = self._get_connectivity(
-                self.final_slab_atoms, reconstruction_cutoff_multiplier
-            )
-        else:
-            self.slab_connectivity = self._get_connectivity(
-                self.final_atoms, reconstruction_cutoff_multiplier
-            )
-
+        self.surface_change_cutoff_multiplier = surface_change_cutoff_multiplier
+        self.desorption_cutoff_multiplier = desorption_cutoff_multiplier
+        
+        if self.final_slab_atoms is None:
+            slab_idxs = [idx for idx, tag in enumerate(self.atoms_tag) if tag != 2] 
+            self.final_slab_atoms = self.init_atoms[slab_idxs]
+            
+            
     def is_adsorbate_dissociated(self):
         """
         Tests if the initial adsorbate connectivity is maintained.
@@ -53,26 +50,32 @@ class DetectTrajAnomaly:
         adsorbate_idx = [idx for idx, tag in enumerate(self.atoms_tag) if tag == 2]
         return (
             np.array_equal(
-                self.initial_connectivity[adsorbate_idx][:, adsorbate_idx],
-                self.final_connectivity[adsorbate_idx][:, adsorbate_idx],
+                self._get_connectivity(self.init_atoms[adsorbate_idx]),
+                self._get_connectivity(self.final_atoms[adsorbate_idx]),
             )
             is False
         )
 
-    def is_surface_reconstructed(self):
+    def has_surface_changed(self):
         """
-        If any slab atoms moved more than X Angstrom, consider possible reconstruction.
-        A larger X means the user is more conversative of what's considered reconstructed.
+        Tests bond breaking / forming events within a tolerance on the surface so
+        that systems with significant adsorbate induces surface changes may be discarded
+        since the reference to the relaxed slab may no longer be valid.
 
         Returns:
             (bool): True if the surface is reconstructed, otherwise False
         """
         surf_idx = [idx for idx, tag in enumerate(self.atoms_tag) if tag != 2]
-        final_slab_connectivity_from_adslab = self.final_connectivity[surf_idx][
-            :, surf_idx
-        ]
+        
+        adslab_connectivity = self._get_connectivity(self.final_atoms[surf_idx])
+        slab_connectivity_w_cushion = self._get_connectivity(self.final_slab_atoms, self.surface_change_cutoff_multiplier)
+        slab_test = 1 in adslab_connectivity - slab_connectivity_w_cushion
+        
+        adslab_connectivity_w_cushion = self._get_connectivity(self.final_atoms[surf_idx], self.surface_change_cutoff_multiplier)
+        slab_connectivity = self._get_connectivity(self.final_slab_atoms)
+        adslab_test = 1 in slab_connectivity - adslab_connectivity_w_cushion
 
-        return 1 in final_slab_connectivity_from_adslab - self.slab_connectivity
+        return any([slab_test, adslab_test])
 
     def is_adsorbate_desorbed(self):
         """
@@ -86,9 +89,10 @@ class DetectTrajAnomaly:
             idx for idx, tag in enumerate(self.atoms_tag) if tag == 2
         ]
         surface_atoms_idx = [idx for idx, tag in enumerate(self.atoms_tag) if tag != 2]
+        final_connectivity = self._get_connectivity(self.final_atoms, self.desorption_cutoff_multiplier)
 
         for idx in adsorbate_atoms_idx:
-            if sum(self.final_connectivity_desorption[idx][surface_atoms_idx]) >= 1:
+            if sum(final_connectivity[idx][surface_atoms_idx]) >= 1:
                 return False
         return True
 
