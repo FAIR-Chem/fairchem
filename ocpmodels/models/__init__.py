6c0971fd952006223d4654d882b27fbcfff534da
Victor Schmidt <vsch@pm.me>
2022-11-02 18:30:46 -0400

    rename model imports

--- ocpmodels/models/__init__.py
+++ ocpmodels/models/__init__.py
@@ -6,13 +6,15 @@
 from .base_model import BaseModel  # noqa: F401
 from .cgcnn import CGCNN  # noqa: F401
 from .dimenet import DimeNetWrap as DimeNet  # noqa: F401
-from .old_dimenet_plus_plus import DimeNetPlusPlusWrap as DimeNetPlusPlus  # noqa: F401
+from .old_dimenet_plus_plus import (  # noqa: F401
+    DimeNetPlusPlusWrap as OldDimeNetPlusPlus,
+)
 from .fanet import FANet  # noqa: F401
-from .old_forcenet import ForceNet  # noqa: F401
+from .old_forcenet import ForceNet as OldForcenet  # noqa: F401
 from .gemnet.gemnet import GemNetT  # noqa: F401
-from .dimenet_plus_plus import NewDimeNetPlusPlus  # noqa: F401
-from .forcenet import NewForceNet  # noqa: F401
-from .schnet import NewSchNet  # noqa: F401
-from .old_schnet import SchNetWrap as SchNet  # noqa: F401
+from .dimenet_plus_plus import DimeNetPlusPlus  # noqa: F401
+from .forcenet import ForceNet  # noqa: F401
+from .schnet import SchNet  # noqa: F401
+from .old_schnet import SchNetWrap as OldSchNet  # noqa: F401
 from .sfarinet import SfariNet  # noqa: F401
 from .spinconv import spinconv  # noqa: F401
