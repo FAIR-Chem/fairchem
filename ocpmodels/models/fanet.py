4003dc7697c179c19be1dec7bfef9be9215ce194
Victor Schmidt <vsch@pm.me>
2022-11-02 19:32:33 -0400

    add force decoder to fanet

--- ocpmodels/models/fanet.py
+++ ocpmodels/models/fanet.py
@@ -14,6 +14,7 @@ from ocpmodels.common.registry import registry
 from ocpmodels.common.utils import get_pbc_distances
 from ocpmodels.models.base_model import BaseModel
 from ocpmodels.models.utils.pos_encodings import PositionalEncoding
+from ocpmodels.models.force_decoder import ForceDecoder
 from ocpmodels.modules.phys_embeddings import PhysEmbedding
 from ocpmodels.modules.pooling import Graclus, Hierarchical_Pooling
 
@@ -395,7 +396,16 @@ class FANet(BaseModel):
             self.w_lin = Linear(self.hidden_channels, 1)
 
         # Force head
-        self.decoder = None
+        self.decoder = (
+            ForceDecoder(
+                kwargs["force_decoder_type"],
+                kwargs["hidden_channels"],
+                kwargs["force_decoder_model_config"],
+                self.act,
+            )
+            if "direct" in self.regress_forces
+            else None
+        )
 
     def forward(self, data):
         # Rewire the graph
