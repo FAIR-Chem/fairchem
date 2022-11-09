d1e10eb8e86a9eb21c41f7178fa96fe6c996b351
Abhishek Das <das.abhshk@gmail.com>
2022-11-09 14:11:45 -0800

    Retry
    Former-commit-id: 4b36148c08314f12dc1ba3e62edae5284ab7a99f

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="ocp-models",
-    version="0.0.3",
+    version="0.1.0",
     description="Machine learning models for use in catalysis as part of the Open Catalyst Project",
     url="https://github.com/Open-Catalyst-Project/ocp",
     packages=find_packages(),
