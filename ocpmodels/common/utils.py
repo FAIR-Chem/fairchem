126ac8b3bf7504999442b8ab357ac10bc9f0358a
Victor Schmidt <vsch@pm.me>
2023-01-03 13:04:57 -0500

    remove breakpoint

--- ocpmodels/common/utils.py
+++ ocpmodels/common/utils.py
@@ -747,7 +747,6 @@ def build_config(args, args_override):
     config = set_qm7x_target_stats(config)
     config = override_narval_paths(config)
     config = auto_note(config)
-    breakpoint()
     config = move_qm7x_data_to_slurm_tmpdir(config)
 
     if not config["no_cpus_to_workers"]:
