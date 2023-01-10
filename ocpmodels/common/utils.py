7abbfc2adbd7f27dcff0498fd9ad595fb6088d87
Abhishek Das <das.abhshk@gmail.com>
2023-01-10 14:34:47 -0800

    Fix device bug

--- ocpmodels/common/utils.py
+++ ocpmodels/common/utils.py
@@ -526,7 +526,9 @@ def get_pbc_distances(
     distances = distance_vectors.norm(dim=-1)
 
     # redundancy: remove zero distances
-    nonzero_idx = torch.arange(len(distances))[distances != 0]
+    nonzero_idx = torch.arange(len(distances), device=distances.device)[
+        distances != 0
+    ]
     edge_index = edge_index[:, nonzero_idx]
     distances = distances[nonzero_idx]
 
