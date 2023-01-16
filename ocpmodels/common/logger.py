96e476c8645bff70581da01edc74a9f6a5f5e5ca
Victor Schmidt <vsch@pm.me>
2023-01-16 15:00:41 -0500

    avoir url file on Beluga

--- ocpmodels/common/logger.py
+++ ocpmodels/common/logger.py
@@ -132,8 +132,9 @@ class WandBLogger(Logger):
             wandb.save(str(sbatch_files[0]))
 
         self.url = wandb.run.get_url()
-        with open(Path(self.trainer_config["run_dir"] / "wandb_url.txt"), "w") as f:
-            f.write(self.url + "\n")
+        if self.url:
+            with open(Path(self.trainer_config["run_dir"] / "wandb_url.txt"), "w") as f:
+                f.write(self.url + "\n")
         if not CLUSTER.drac:
             self.collect_output_files(policy="live")
             self.collect_output_files(policy="end")
