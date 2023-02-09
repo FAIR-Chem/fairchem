e1f97bbcc6e132276454a76b28d1b1735df186ab
Muhammed Shuaibi <45150244+mshuaibii@users.noreply.github.com>
2023-02-09 09:53:47 -0800

    Catch OOM during relaxations (#447)
    * Dynamic batching
    
    * remove debug calls
    
    * move recovery outside except
    
    * revert local changes
    
    * cleanup
    
    * wrap only run in try/except block
    
    * extract batch size from sids
    
    * sid->natoms
    
    ---------
    
    Co-authored-by: anuroopsriram <anuroops@fb.com>
    Former-commit-id: 0b44ba89f40ef3af33b56b978ecd22544204786c

--- ocpmodels/common/relaxation/ml_relaxation.py
+++ ocpmodels/common/relaxation/ml_relaxation.py
@@ -5,11 +5,15 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
 
+import logging
+from collections import deque
 from pathlib import Path
 
 import torch
+from torch_geometric.data import Batch
 
 from ocpmodels.common.registry import registry
+from ocpmodels.datasets.lmdb_dataset import data_list_collater
 
 from .optimizers.lbfgs_torch import LBFGS, TorchCalc
 
@@ -40,25 +44,46 @@ def ml_relax(
         save_full_traj: bool
             Whether to save out the full ASE trajectory. If False, only save out initial and final frames.
     """
-    batch = batch[0]
-    ids = batch.sid
-    calc = TorchCalc(model, transform)
+    batches = deque([batch[0]])
+    relaxed_batches = []
+    while batches:
+        batch = batches.popleft()
+        oom = False
+        ids = batch.sid
+        calc = TorchCalc(model, transform)
 
-    # Run ML-based relaxation
-    traj_dir = relax_opt.get("traj_dir", None)
-    optimizer = LBFGS(
-        batch,
-        calc,
-        maxstep=relax_opt.get("maxstep", 0.04),
-        memory=relax_opt["memory"],
-        damping=relax_opt.get("damping", 1.0),
-        alpha=relax_opt.get("alpha", 70.0),
-        device=device,
-        save_full_traj=save_full_traj,
-        traj_dir=Path(traj_dir) if traj_dir is not None else None,
-        traj_names=ids,
-        early_stop_batch=early_stop_batch,
-    )
-    relaxed_batch = optimizer.run(fmax=fmax, steps=steps)
+        # Run ML-based relaxation
+        traj_dir = relax_opt.get("traj_dir", None)
+        optimizer = LBFGS(
+            batch,
+            calc,
+            maxstep=relax_opt.get("maxstep", 0.04),
+            memory=relax_opt["memory"],
+            damping=relax_opt.get("damping", 1.0),
+            alpha=relax_opt.get("alpha", 70.0),
+            device=device,
+            traj_dir=Path(traj_dir) if traj_dir is not None else None,
+            traj_names=ids,
+            early_stop_batch=early_stop_batch,
+        )
+        try:
+            relaxed_batch = optimizer.run(fmax=fmax, steps=steps)
+            relaxed_batches.append(relaxed_batch)
+        except RuntimeError as e:
+            oom = True
+            torch.cuda.empty_cache()
 
+        if oom:
+            # move OOM recovery code outside of except clause to allow tensors to be freed.
+            data_list = batch.to_data_list()
+            if len(data_list) == 1:
+                raise e
+            logging.info(
+                f"Failed to relax batch with size: {len(data_list)}, splitting into two..."
+            )
+            mid = len(data_list) // 2
+            batches.appendleft(data_list_collater(data_list[:mid]))
+            batches.appendleft(data_list_collater(data_list[mid:]))
+
+    relaxed_batch = Batch.from_data_list(relaxed_batches)
     return relaxed_batch
