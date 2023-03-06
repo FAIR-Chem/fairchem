d7b84ac4d552046819f38d33b4cbfba0404ea213
AlexDuvalinho <afonsalex@free.fr>
2023-03-06 09:16:23 -0500

    remove old models impport

--- ocpmodels/models/__init__.py
+++ ocpmodels/models/__init__.py
@@ -6,15 +6,10 @@
 from .base_model import BaseModel  # noqa: F401
 from .cgcnn import CGCNN  # noqa: F401
 from .dimenet import DimeNet  # noqa: F401
-from .old_dimenet_plus_plus import (  # noqa: F401
-    DimeNetPlusPlusWrap as OldDimeNetPlusPlus,
-)
 from .faenet import FAENet  # noqa: F401
-from .old_forcenet import ForceNet as OldForcenet  # noqa: F401
 from .gemnet.gemnet import GemNetT  # noqa: F401
 from .dimenet_plus_plus import DimeNetPlusPlus  # noqa: F401
 from .forcenet import ForceNet  # noqa: F401
 from .schnet import SchNet  # noqa: F401
-from .old_schnet import SchNetWrap as OldSchNet  # noqa: F401
 from .sfarinet import SfariNet  # noqa: F401
 from .spinconv import spinconv  # noqa: F401
