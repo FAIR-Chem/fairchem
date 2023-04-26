17e096b2defb027bafb29d8ff44bf4ad5427ccef
Zack Ulissi <zulissi@meta.com>
2023-04-25 17:00:44 -0700

    more fixes to file logic path

--- ocpmodels/datasets/lmdb_dataset.py
+++ ocpmodels/datasets/lmdb_dataset.py
@@ -74,11 +74,8 @@ class LmdbDataset(Dataset):
                 if cur_env.begin().get("length".encode("ascii")) is not None:
                     num_entries -= 1
 
-                # Get all of the valid keys [0, len(lmdb)] encoded as ascii
-                cur_keys = [f"{j}".encode("ascii") for j in range(num_entries)]
-
-                # Append the keys as a list
-                self._keys.append(cur_keys)
+                # Append the keys (0->num_entries) as a list
+                self._keys.append(list(range(num_entries)))
 
             keylens = [len(k) for k in self._keys]
             self._keylen_cumulative = np.cumsum(keylens).tolist()
@@ -86,11 +83,16 @@ class LmdbDataset(Dataset):
         else:
             self.metadata_path = self.path.parent / "metadata.npz"
             self.env = self.connect_db(self.path)
-            self._keys = [
-                f"{j}".encode("ascii")
-                for j in range(self.env.stat()["entries"])
-            ]
-            self.num_samples = len(self._keys)
+
+            num_entries = self.env.stat()["entries"]
+
+            # If "length" encoded as ascii is present, we have one fewer
+            # data than the stats suggest
+            if self.env.begin().get("length".encode("ascii")) is not None:
+                num_entries -= 1
+
+            self._keys = list(range(num_entries))
+            self.num_samples = num_entries
 
         # If specified, limit dataset to only a portion of the entire dataset
         # total_shards: defines total chunks to partition dataset
@@ -127,12 +129,16 @@ class LmdbDataset(Dataset):
 
             # Return features.
             datapoint_pickled = (
-                self.envs[db_idx].begin().get(self._keys[db_idx][el_idx])
+                self.envs[db_idx]
+                .begin()
+                .get(f"{self._keys[db_idx][el_idx]}".encode("ascii"))
             )
             data_object = pyg2_data_transform(pickle.loads(datapoint_pickled))
             data_object.id = f"{db_idx}_{el_idx}"
         else:
-            datapoint_pickled = self.env.begin().get(self._keys[idx])
+            datapoint_pickled = self.env.begin().get(
+                f"{self._keys[idx]}".encode("ascii")
+            )
             data_object = pyg2_data_transform(pickle.loads(datapoint_pickled))
 
         if self.transform is not None:
