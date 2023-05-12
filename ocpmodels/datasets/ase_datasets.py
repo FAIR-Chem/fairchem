bd86f230df7e088697a736e284d735ef50895f17
Ethan Sunshine <esunshin@andrew.cmu.edu>
2023-05-12 17:25:28 +0000

    Lint
    Former-commit-id: a77e7f50f9414eb15be9503011cfb456259671a8

--- ocpmodels/datasets/ase_datasets.py
+++ ocpmodels/datasets/ase_datasets.py
@@ -33,9 +33,9 @@ class AseReadDataset(Dataset):
                     If you are using this for a training dataset, set
                     "r_energy"=True and/or "r_forces"=True as appropriate
                     In that case, energy/forces must be in the files you read (ex. OUTCAR)
-                    
+
             apply_tags (bool, optional): Apply a tag of one to each atom, which is
-                    required of some models. A value of None will only tag structures 
+                    required of some models. A value of None will only tag structures
                     that are not already tagged.
 
         transform (callable, optional): Additional preprocessing function
@@ -72,14 +72,13 @@ class AseReadDataset(Dataset):
             atoms = ase.io.read(self.id[idx])
         except Exception as err:
             warnings.warn(f"{err} occured for: {self.id[idx]}")
-            
+
         if self.config.get("apply_tags") == False:
             pass
         elif self.config.get("apply_tags") == True:
             atoms = self.apply_tags(atoms)
         elif sum(atoms.get_tags()) < 1:
             atoms = self.apply_tags(atoms)
-            
 
         data_object = self.a2g.convert(atoms)
 
@@ -92,7 +91,7 @@ class AseReadDataset(Dataset):
     # but there is nothing necessary to do here
     def close_db(self):
         pass
-    
+
     def apply_tags(self, atoms):
         atoms.set_tags(np.ones(len(atoms)))
         return atoms
@@ -122,9 +121,9 @@ class AseDBDataset(Dataset):
                     If you are using this for a training dataset, set
                     "r_energy"=True and/or "r_forces"=True as appropriate
                     In that case, energy/forces must be in the database
-                    
+
             apply_tags (bool, optional): Apply a tag of one to each atom, which is
-                    required of some models. A value of None will only tag structures 
+                    required of some models. A value of None will only tag structures
                     that are not already tagged.
 
         transform (callable, optional): Additional preprocessing function
@@ -158,7 +157,7 @@ class AseDBDataset(Dataset):
 
     def __getitem__(self, idx):
         atoms = self.db.get_atoms(self.id[idx])
-        
+
         if self.config.get("apply_tags") == False:
             pass
         elif self.config.get("apply_tags") == True:
@@ -181,7 +180,7 @@ class AseDBDataset(Dataset):
 
     def close_db(self):
         pass
-    
+
     def apply_tags(self, atoms):
         atoms.set_tags(np.ones(len(atoms)))
         return atoms
