06c5be6b43c38a292431bb6ee304cb60805d4d01
Ethan Sunshine <esunshin@andrew.cmu.edu>
2023-05-12 17:25:28 +0000

    Lint

--- tests/datasets/test_ase_datasets.py
+++ tests/datasets/test_ase_datasets.py
@@ -8,37 +8,62 @@ from ocpmodels.datasets import AseReadDataset, AseDBDataset
 structures = [
     build.molecule("H2O"),
     build.bulk("Cu"),
-    build.fcc111("Pt", size=[2,2,3], vacuum = 8, periodic=True),
+    build.fcc111("Pt", size=[2, 2, 3], vacuum=8, periodic=True),
 ]
+
 
 def test_ase_read_dataset():
     for i, structure in enumerate(structures):
-        write(os.path.join(os.path.dirname(os.path.abspath(__file__)), f"{i}.cif"), structure)
-        
-    dataset = AseReadDataset(config = {
-        "src": os.path.join(os.path.dirname(os.path.abspath(__file__))),
-        "suffix": ".cif"
-    })
-    
+        write(
+            os.path.join(
+                os.path.dirname(os.path.abspath(__file__)), f"{i}.cif"
+            ),
+            structure,
+        )
+
+    dataset = AseReadDataset(
+        config={
+            "src": os.path.join(os.path.dirname(os.path.abspath(__file__))),
+            "suffix": ".cif",
+        }
+    )
+
     assert len(dataset) == len(structures)
-    
+
     for i in range(len(structures)):
-        os.remove(os.path.join(os.path.dirname(os.path.abspath(__file__)), f"{i}.cif"))
-    
+        os.remove(
+            os.path.join(
+                os.path.dirname(os.path.abspath(__file__)), f"{i}.cif"
+            )
+        )
+
+
 def test_ase_db_dataset():
     try:
-        os.remove(os.path.join(os.path.dirname(os.path.abspath(__file__)), "asedb.db"))
+        os.remove(
+            os.path.join(
+                os.path.dirname(os.path.abspath(__file__)), "asedb.db"
+            )
+        )
     except FileNotFoundError:
         pass
-    
-    database = db.connect(os.path.join(os.path.dirname(os.path.abspath(__file__)), "asedb.db"))
+
+    database = db.connect(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "asedb.db")
+    )
     for i, structure in enumerate(structures):
         database.write(structure)
-        
-    dataset = AseDBDataset(config = {
-        "src": os.path.join(os.path.dirname(os.path.abspath(__file__)), "asedb.db"),
-    })
-    
+
+    dataset = AseDBDataset(
+        config={
+            "src": os.path.join(
+                os.path.dirname(os.path.abspath(__file__)), "asedb.db"
+            ),
+        }
+    )
+
     assert len(dataset) == len(structures)
-    
-    os.remove(os.path.join(os.path.dirname(os.path.abspath(__file__)), "asedb.db"))
+
+    os.remove(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "asedb.db")
+    )
