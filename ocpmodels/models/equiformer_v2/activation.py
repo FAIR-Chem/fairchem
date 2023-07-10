ea13343a57fe8480bac7513228c084afbe51e276
Abhishek Das <das.abhshk@gmail.com>
2023-07-10 13:48:45 -0700

    Remove commented import line

--- ocpmodels/models/equiformer_v2/activation.py
+++ ocpmodels/models/equiformer_v2/activation.py
@@ -2,8 +2,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-# from .linear import Linear_gaussian_init
-
 
 class ScaledSiLU(nn.Module):
     def __init__(self, inplace=False):
