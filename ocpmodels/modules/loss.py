5f47f8af3e3178587d1d51bf4428d6928804270b
Janice Lan <janlan@fb.com>
2023-07-21 16:04:31 -0700

    support loss mean over all dimensions

--- ocpmodels/modules/loss.py
+++ ocpmodels/modules/loss.py
@@ -52,9 +52,15 @@ class DDPLoss(nn.Module):
         super().__init__()
         self.loss_fn = loss_fn
         self.loss_name = loss_name
-        self.loss_fn.reduction = "sum"
         self.reduction = reduction
-        assert reduction in ["mean", "sum"]
+        assert reduction in ["mean", "mean_all", "sum"]
+
+        # for forces, we want to sum over xyz errors and average over batches/atoms (mean)
+        # for other metrics, we want to average over all axes (mean_all) or leave as a sum (sum)
+        if reduction == "mean_all":
+            self.loss_fn.reduction = "mean"
+        else:
+            self.loss_fn.reduction = "sum"
 
     def forward(
         self,
@@ -63,6 +69,9 @@ class DDPLoss(nn.Module):
         natoms: Optional[torch.Tensor] = None,
         batch_size: Optional[int] = None,
     ):
+        # ensure torch doesn't do any unwanted broadcasting
+        assert input.shape == target.shape
+
         # zero out nans, if any
         found_nans_or_infs = not torch.all(input.isfinite())
         if found_nans_or_infs is True:
@@ -87,4 +96,5 @@ class DDPLoss(nn.Module):
             # across DDP replicas
             return loss * distutils.get_world_size() / num_samples
         else:
+            # if reduction is sum or mean over all axes, no other operations are needed
             return loss
