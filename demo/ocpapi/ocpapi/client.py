5798247da76e81682e8f4209c9a09d153bfc0cb7
Kyle Michel <kylemichel@meta.com>
2023-08-22 17:41:15 -0700

    Add Client and get_bulks method

---
+++ demo/ocpapi/ocpapi/client.py
@@ -0,0 +1,83 @@
+import asyncio
+
+import requests
+
+from ocpapi.models import BulksResponse
+
+
+class RequestException(Exception):
+    def __init__(self, method: str, url: str, cause: str) -> None:
+        super().__init__(f"Request to {method} {url} failed. {cause}")
+
+
+class Client:
+    """
+    Exposes each route in the OCP API as a method.
+    """
+
+    def __init__(
+        self,
+        base_url: str = "https://open-catalyst-api.metademolab.com/ocp/",
+    ) -> None:
+        """
+        Args:
+            base_url: The base URL for all API requests.
+        """
+        # Normalize the base URL so that all methods below can assume it
+        # does not end in a '/' character
+        self._base_url = base_url.rstrip("/")
+
+    async def get_bulks(self) -> BulksResponse:
+        """
+        Fetch the list of bulk materials that are supported in the API.
+        """
+        response = await self._run_request(
+            url=f"{self._base_url}/bulks",
+            method="GET",
+            expected_response_code=200,
+        )
+        return BulksResponse.from_json(response)
+
+    async def _run_request(
+        self, url: str, method: str, expected_response_code: int, **kwargs
+    ) -> str:
+        """
+        Helper method that runs the input request on a thread so that
+        it doesn't block the event loop on the calling thread.
+        """
+
+        # Make the request
+        try:
+            response = await asyncio.to_thread(
+                requests.request,
+                method=method,
+                url=url,
+                **kwargs,
+            )
+        except Exception as e:
+            raise RequestException(
+                method=method,
+                url=url,
+                cause=f"Exception while making request: {type(e).__name__}: {e}",
+            ) from e
+
+        # Check the response code
+        if response.status_code != expected_response_code:
+            raise RequestException(
+                method=method,
+                url=url,
+                cause=(
+                    f"Expected response code {expected_response_code}; "
+                    f"got {response.status_code}. Body = {response.text}"
+                ),
+            )
+
+        # Try to decode the response
+        try:
+            return response.text
+        except Exception as e:
+            raise RequestException(
+                method=method,
+                url=url,
+                cause=f"Error parsing response body: {response.text}",
+            ) from e
---
+++ demo/ocpapi/ocpapi/models.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass, field
+from typing import List
+
+from dataclasses_json import CatchAll, Undefined, config, dataclass_json
+
+
+@dataclass_json(undefined=Undefined.INCLUDE)
+@dataclass
+class _Model:
+    """
+    Base class for all data models.
+
+    Attributes:
+        other: Fields that may have been added to the API that all not yet
+            supported explicitly in this class.
+    """
+
+    other_fields: CatchAll
+
+
+@dataclass_json(undefined=Undefined.INCLUDE)
+@dataclass
+class Bulk(_Model):
+    """
+    Stores information about a single bulk material.
+
+    Attributes:
+        src_id: The ID of the material.
+        formula: The chemical formula of the material.
+        elements: The list of elements in the material.
+    """
+
+    src_id: str = ""
+    formula: str = ""
+    elements: List[str] = field(
+        metadata=config(field_name="els"),
+        default_factory=list,
+    )
+
+
+@dataclass_json(undefined=Undefined.INCLUDE)
+@dataclass
+class BulksResponse(_Model):
+    """
+    Stores the response from a request to fetch bulks supported in the API.
+
+    Attributes:
+        bulks_supported: List of bulks that can be used in the API.
+    """
+
+    bulks_supported: List[Bulk] = field(default_factory=list)
