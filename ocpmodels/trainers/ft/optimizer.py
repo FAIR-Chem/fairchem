d1d35b9afeaccf0981d9f5fae69e96485d5a918e
Nima Shoghi <nimashoghi@gmail.com>
2023-08-26 19:48:51 +0000

    Update
    Former-commit-id: 6e79e4a3015a15c210a07d4efaae4d6b62ce53e8

--- ocpmodels/trainers/ft/optimizer.py
+++ ocpmodels/trainers/ft/optimizer.py
@@ -3,12 +3,14 @@ from typing import Literal, Tuple, Union
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 from typing_extensions import Annotated, override
 
 from ocpmodels.common.typed_config import Field, TypedConfig
 from ocpmodels.modules.exponential_moving_average import (
     ExponentialMovingAverage,
 )
+from .lr_scheduler import warmup_cosine_decay
 
 
 class AdamWConfig(TypedConfig):
@@ -28,7 +30,6 @@ Duration = Tuple[int, Literal["steps", "epochs"]]
 class ReduceLROnPlateauConfig(TypedConfig):
     lr_scheduler: Literal["ReduceLROnPlateau"]
 
-    monitor: str
     patience: int
     factor: float
     mode: Literal["min", "max"] = "min"
@@ -37,6 +38,31 @@ class ReduceLROnPlateauConfig(TypedConfig):
     cooldown: int = 0
     threshold: float = 1.0e-4
     threshold_mode: str = "rel"
+
+    def _to_linear_warmup_cos_rlp_dict(self):
+        """
+        Params for PerParamGroupLinearWarmupCosineAnnealingRLPLR's RLP
+            mode="min",
+            factor=0.1,
+            patience=10,
+            threshold=1e-4,
+            threshold_mode="rel",
+            cooldown=0,
+            min_lr=0,
+            eps=1e-8,
+            verbose=False,
+        """
+        return {
+            "mode": self.mode,
+            "factor": self.factor,
+            "patience": self.patience,
+            "threshold": self.threshold,
+            "threshold_mode": self.threshold_mode,
+            "cooldown": self.cooldown,
+            "min_lr": self.min_lr,
+            "eps": self.eps,
+            "verbose": False,
+        }
 
 
 class WarmupCosineDecayRLPSchedulerConfig(TypedConfig):
@@ -50,11 +76,11 @@ class WarmupCosineDecayRLPSchedulerConfig(TypedConfig):
     last_step: int = -1
     should_restart: bool = True
 
-    rlp: ReduceLROnPlateauConfig
+    rlp: ReduceLROnPlateauConfig | None = None
 
 
 LRSchedulerConfig = Annotated[
-    WarmupCosineDecayRLPSchedulerConfig,
+    Union[ReduceLROnPlateauConfig, WarmupCosineDecayRLPSchedulerConfig],
     Field(discriminator="lr_scheduler"),
 ]
 
@@ -65,38 +91,87 @@ class ExponentialMovingAverageConfig(TypedConfig):
     decay: float = 0.9999
 
 
-class OptimConfig(TypedConfig):
+class BaseOptimConfig(TypedConfig):
+    exponential_moving_average: ExponentialMovingAverageConfig | None = None
+
+
+class SingleGroupOptimConfig(BaseOptimConfig):
+    optim_mode: Literal["single_group"] = "single_group"
+
     optimizer: OptimizerConfig
     lr_scheduler: LRSchedulerConfig
-    exponential_moving_average: ExponentialMovingAverageConfig | None = None
-    layerwise_learning_rate_decay: LLRDConfig | None = None
-
-    @override
-    def __post_init__(self):
-        super().__post_init__()
-
-        if self.layerwise_learning_rate_decay:
-            # LLRD only works for AdamW + WarmupCosineDecayRLP
-            assert (
-                self.optimizer.optimizer == "AdamW"
-            ), f"LLRD only works for AdamW, not {self.optimizer.optimizer}"
-            assert isinstance(
-                self.lr_scheduler.lr_scheduler,
-                WarmupCosineDecayRLPSchedulerConfig,
-            ), f"LLRD only works for WarmupCosineDecayRLP, not {self.lr_scheduler.lr_scheduler}"
 
 
-def _construct_optimizer(model: nn.Module, config: OptimConfig):
+class MultiGroupOptimConfig(BaseOptimConfig):
+    optim_mode: Literal["multi_group"]
+
+    groups: dict[str, SingleGroupOptimConfig]
+
+
+OptimConfig = Annotated[
+    Union[SingleGroupOptimConfig, MultiGroupOptimConfig],
+    Field(discriminator="optim_mode"),
+]
+
+
+def _construct_single_group(model: nn.Module, config: SingleGroupOptimConfig):
     match config.optimizer:
         case AdamWConfig():
-            return optim.AdamW()
+            optimizer = optim.AdamW(
+                model.parameters(),
+                lr=config.optimizer.lr,
+                amsgrad=config.optimizer.amsgrad,
+                weight_decay=config.optimizer.weight_decay,
+                betas=config.optimizer.betas,
+                eps=config.optimizer.eps,
+            )
         case _:
             raise NotImplementedError(
                 f"Optimizer {config.optimizer} not implemented."
             )
 
+    match config.lr_scheduler:
+        case WarmupCosineDecayRLPSchedulerConfig():
+            lr_scheduler = WarmupCosineDecayRLPScheduler(
+                optimizer,
+                config.lr_scheduler.warmup,
+                config.lr_scheduler.decay,
+                warmup_start_lr_factor=config.lr_scheduler.warmup_start_lr_factor,
+                min_lr_factor=config.lr_scheduler.min_lr_factor,
+                last_step=config.lr_scheduler.last_step,
+                should_restart=config.lr_scheduler.should_restart,
+            )
+        case ReduceLROnPlateauConfig():
+            lr_scheduler = ReduceLROnPlateau(
+                optimizer,
+                patience=config.lr_scheduler.patience,
+                factor=config.lr_scheduler.factor,
+                mode=config.lr_scheduler.mode,
+                min_lr=config.lr_scheduler.min_lr,
+                eps=config.lr_scheduler.eps,
+                cooldown=config.lr_scheduler.cooldown,
+                threshold=config.lr_scheduler.threshold,
+                threshold_mode=config.lr_scheduler.threshold_mode,
+            )
+        case _:
+            raise NotImplementedError(
+                f"LR scheduler {config.lr_scheduler} not implemented."
+            )
+
+    return optimizer, lr_scheduler
+
 
 def load_optimizer(model: nn.Module, config: OptimConfig):
+    match config:
+        case SingleGroupOptimConfig():
+            optimizer, lr_scheduler = _construct_single_group(
+                model, config.optimizer
+            )
+        case MultiGroupOptimConfig():
+            pass
+        case _:
+            raise ValueError(f"Invalid optimizer config: {config}")
+
     # Construct EMA
     ema = None
     if config.exponential_moving_average:
