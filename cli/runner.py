ff8ffa5d5071c07c869db652a669ea143b9dde6b
Kyle Michel <kylemichel@meta.com>
2023-09-07 14:52:14 -0700

    Add support for "train" in cli
    Former-commit-id: 733dbab98057778016f14a19fe5186804221e1f7

--- cli/runner.py
+++ cli/runner.py
@@ -5,20 +5,17 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
 
-import argparse
 import copy
 import logging
-from typing import List
+from pathlib import Path
+from typing import Any, Dict, Optional
 
 import submitit
 
-from ocpmodels.common.flags import flags
 from ocpmodels.common.utils import (
-    build_config,
     create_grid,
     new_trainer_context,
     save_experiment_log,
-    setup_logging,
 )
 
 
@@ -26,8 +23,10 @@ class Runner(submitit.helpers.Checkpointable):
     def __init__(self) -> None:
         self.config = None
 
-    def __call__(self, config) -> None:
-        with new_trainer_context(args=args, config=config) as ctx:
+    def __call__(self, config, distributed) -> None:
+        with new_trainer_context(
+            config=config, distributed=distributed
+        ) as ctx:
             self.config = ctx.config
             self.task = ctx.task
             self.trainer = ctx.trainer
@@ -45,48 +44,55 @@ class Runner(submitit.helpers.Checkpointable):
         return submitit.helpers.DelayedSubmission(new_runner, self.config)
 
 
-if __name__ == "__main__":
-    setup_logging()
-
-    parser: argparse.ArgumentParser = flags.get_parser()
-    args: argparse.Namespace
-    override_args: List[str]
-    args, override_args = parser.parse_known_args()
-    config = build_config(args, override_args)
-
-    if args.submit:  # Run on cluster
+def run_with_config(
+    config: Dict[str, Any],
+    sweep_yml: Optional[Path],
+    submit: bool,
+    logdir: str,
+    identifier: str,
+    slurm_mem: int,
+    slurm_timeout: int,
+    slurm_partition: str,
+    num_gpus: int,
+    num_nodes: int,
+    distributed: bool,
+) -> None:
+    if submit:  # Run on cluster
         slurm_add_params = config.get(
             "slurm", None
         )  # additional slurm arguments
-        if args.sweep_yml:  # Run grid search
-            configs = create_grid(config, args.sweep_yml)
+        if sweep_yml:  # Run grid search
+            configs = create_grid(config, str(sweep_yml))
         else:
             configs = [config]
 
         logging.info(f"Submitting {len(configs)} jobs")
         executor = submitit.AutoExecutor(
-            folder=args.logdir / "%j", slurm_max_num_timeout=3
+            folder=logdir / "%j",
+            slurm_max_num_timeout=3,
         )
         executor.update_parameters(
-            name=args.identifier,
-            mem_gb=args.slurm_mem,
-            timeout_min=args.slurm_timeout * 60,
-            slurm_partition=args.slurm_partition,
-            gpus_per_node=args.num_gpus,
+            name=identifier,
+            mem_gb=slurm_mem,
+            timeout_min=slurm_timeout * 60,
+            slurm_partition=slurm_partition,
+            gpus_per_node=num_gpus,
             cpus_per_task=(config["optim"]["num_workers"] + 1),
-            tasks_per_node=(args.num_gpus if args.distributed else 1),
-            nodes=args.num_nodes,
+            tasks_per_node=(num_gpus if distributed else 1),
+            nodes=num_nodes,
             slurm_additional_parameters=slurm_add_params,
         )
         for config in configs:
             config["slurm"] = copy.deepcopy(executor.parameters)
             config["slurm"]["folder"] = str(executor.folder)
-        jobs = executor.map_array(Runner(), configs)
+        jobs = executor.map_array(
+            Runner(), configs, [distributed] * len(configs)
+        )
         logging.info(
             f"Submitted jobs: {', '.join([job.job_id for job in jobs])}"
         )
-        log_file = save_experiment_log(args, jobs, configs)
+        log_file = save_experiment_log(logdir, jobs, configs)
         logging.info(f"Experiment log saved to: {log_file}")
 
     else:  # Run locally
-        Runner()(config)
+        Runner()(config, distributed)
