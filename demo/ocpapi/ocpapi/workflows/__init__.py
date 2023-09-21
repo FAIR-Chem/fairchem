b99123fa8427e2254d19b388aa33cceb349115e7
Kyle Michel <kylemichel@meta.com>
2023-09-21 15:29:06 -0700

    Add context to log statements

--- demo/ocpapi/ocpapi/workflows/__init__.py
+++ demo/ocpapi/ocpapi/workflows/__init__.py
@@ -1,9 +1,9 @@
-from .retry import NO_LIMIT, retry_api_calls  # noqa
 from .adsorbates import (  # noqa
-    UnsupportedBulkException,
     UnsupportedAdsorbateException,
-    find_adsorbate_binding_sites,
+    UnsupportedBulkException,
     filter_slabs_with_miller_indices,
+    find_adsorbate_binding_sites,
     get_adsorbate_relaxation_results,
     wait_for_adsorbate_relaxations,
 )
+from .retry import NO_LIMIT, retry_api_calls  # noqa
