ccee0c8ceca2ca12e7c5af5396504b37fc0aa0ef
Kyle Michel <kylemichel@meta.com>
2023-09-21 14:36:03 -0700

    Add workflows for adsorbate slab relaxations

--- src/fairchem/demo/ocpapi/ocpapi/workflows/__init__.py
+++ src/fairchem/demo/ocpapi/ocpapi/workflows/__init__.py
@@ -1,1 +1,9 @@
 from .retry import NO_LIMIT, retry_api_calls  # noqa
+from .adsorbates import (  # noqa
+    UnsupportedBulkException,
+    UnsupportedAdsorbateException,
+    find_adsorbate_binding_sites,
+    filter_slabs_with_miller_indices,
+    get_adsorbate_relaxation_results,
+    wait_for_adsorbate_relaxations,
+)
