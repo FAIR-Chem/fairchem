371ad84f585e02992d134599e97121d9ffec47ba
Muhammed Shuaibi <mushuaibi@gmail.com>
2023-11-06 16:21:44 -0800

    oc22 support

--- ocpmodels/common/utils.py
+++ ocpmodels/common/utils.py
@@ -1204,11 +1204,13 @@ def update_config(base_config):
     are now. Update old configs to fit the new expected structure.
     """
     config = copy.deepcopy(base_config)
+    config["dataset"]["format"] = config["task"].get("dataset", "lmdb")
     ### Read task based off config structure, similar to OCPCalculator.
     if config["task"]["dataset"] in [
         "trajectory_lmdb",
         "lmdb",
         "trajectory_lmdb_v2",
+        "oc22_lmdb",
     ]:
         task = "s2ef"
     elif config["task"]["dataset"] == "single_point_lmdb":
