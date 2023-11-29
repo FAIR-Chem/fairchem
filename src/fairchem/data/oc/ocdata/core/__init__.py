abd2b559039da149e5ceca156ccf56004b75fbfe
Muhammed Shuaibi <45150244+mshuaibii@users.noreply.github.com>
2023-11-28 16:11:14 -0800

    Support for placing multiple adsorbates (#74)
    * initial multi-adsorbate placement
    
    * support for multi configurations
    
    * add multi-ads coverage tests
    
    * update docstring
    
    * typo
    
    * update metadata dict
    
    * adsorbate argument to placement
    
    * Remove unused imports
    
    * Remove more unused imports
    
    * update docs
    
    * indent

--- src/fairchem/data/oc/ocdata/core/__init__.py
+++ src/fairchem/data/oc/ocdata/core/__init__.py
@@ -1,5 +1,5 @@
-from .adsorbate import Adsorbate
 from .bulk import Bulk
 from .slab import Slab
-
+from .adsorbate import Adsorbate
 from .adsorbate_slab_config import AdsorbateSlabConfig
+from .multi_adsorbate_slab_config import MultipleAdsorbateSlabConfig
