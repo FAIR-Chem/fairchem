a0027b699a703a52f1a4576d49885bb99d0f5357
Victor Schmidt <vsch@pm.me>
2024-01-10 17:45:37 -0500

    Use convert_path

--- setup.py
+++ setup.py
@@ -5,16 +5,31 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
 
+from distutils.util import convert_path
 from pathlib import Path
 
 from setuptools import setup
+
+
+def make_ocpmodels_package_dict():
+    dirs = [
+        convert_path(str(p))
+        for p in Path("./ocpmodels/").glob("**")
+        if (p / "__init__.py").exists()
+    ]
+    pkgs = [d.replace("/", ".") for d in dirs]
+    return {p: d for p, d in zip(pkgs, dirs)}
+
+
+pkg_dict = make_ocpmodels_package_dict()
+pkg_dict["ocdata"] = convert_path("ocdata")
 
 setup(
     name="ocpmodels",
     version="0.1.0",
     description="Machine learning models for use in catalysis as part of the Open Catalyst Project",
     url="https://github.com/Open-Catalyst-Project/ocp",
-    packages=["ocdata"]
-    + [str(p) for p in Path("./ocpmodels/").glob("**") if (p / "__init__.py").exists()],
+    packages=list(pkg_dict.keys()),
+    package_dir=pkg_dict,
     include_package_data=True,
 )
