c7e0178c0a0f747f6dfb4c3e60b1cddea9e3e4cb
Victor Schmidt <vsch@pm.me>
2024-01-10 17:15:31 -0500

    change setup

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
 setup(
-    name="ocp-models",
-    version="0.0.3",
+    name="ocpmodels",
+    version="mila-0.1.0",
     description="Machine learning models for use in catalysis as part of the Open Catalyst Project",
     url="https://github.com/Open-Catalyst-Project/ocp",
-    packages=find_packages(),
+    packages=["ocdata", "ocpmodels"],
     include_package_data=True,
 )
