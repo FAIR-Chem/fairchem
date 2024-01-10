9f22a1961f1bbb8fcbf39e20dad6ba1bf1dabc3e
Victor Schmidt <vsch@pm.me>
2024-01-10 17:38:17 -0500

    install recursively

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
 
+from pathlib import Path
+
 from setuptools import setup
 
 setup(
@@ -12,6 +14,7 @@ setup(
     version="0.1.0",
     description="Machine learning models for use in catalysis as part of the Open Catalyst Project",
     url="https://github.com/Open-Catalyst-Project/ocp",
-    packages=["ocdata", "ocpmodels"],
+    packages=["ocdata"]
+    + [str(p) for p in Path("./ocpmodels/").glob("**") if (p / "__init__.py").exists()],
     include_package_data=True,
 )
