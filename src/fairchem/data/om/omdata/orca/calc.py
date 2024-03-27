3ab4b584db9e9a373d5c2664dbdbcbcca72ce255
Muhammed Shuaibi <45150244+mshuaibii@users.noreply.github.com>
2024-03-27 09:17:03 -0700

    add orca recipes (#3)
    * add orca recipes
    
    * more docs
    
    * explicit setup, directory
    
    * version fix
    
    * support quacc==0.7.2

---
+++ src/fairchem/data/om/omdata/orca/calc.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+from shutil import which
+
+from ase.calculators.orca import ORCA, OrcaProfile
+from pydantic import Field
+from sella import Sella
+
+ORCA_FUNCTIONAL = "wB97M-V"
+ORCA_BASIS = "def2-TZVPD"
+ORCA_SIMPLE_INPUT = [
+    "EnGrad",
+    "RIJCOSX",
+    "def2/J",
+    "NoUseSym",
+    "DIIS",
+    "NOSOSCF",
+    "NormalConv",
+    "DEFGRID2",
+]
+ORCA_SIMPLE_INPUT_QUACC_IGNORE = []
+ORCA_BLOCKS = ["%scf Convergence Tight maxiter 500 end"]
+ORCA_ASE_SIMPLE_INPUT = " ".join([ORCA_FUNCTIONAL] + [ORCA_BASIS] + ORCA_SIMPLE_INPUT)
+OPT_PARAMETERS = {
+    "optimizer": Sella,
+    "store_intermediate_results": True,
+    "fmax": 0.01,
+    "max_steps": 1000,
+}
+
+
+def write_orca_inputs(
+    atoms,
+    output_directory,
+    charge=0,
+    mult=1,
+    orcasimpleinput=ORCA_ASE_SIMPLE_INPUT,
+    orcablocks=" ".join(ORCA_BLOCKS),
+):
+    """
+    One-off method to be used if you wanted to write inputs for an arbitrary
+    system. Primarily used for debugging.
+    """
+
+    MyOrcaProfile = OrcaProfile([str(Field(Path(which("orca") or "orca")).default)])
+    calc = ORCA(
+        charge=charge,
+        mult=mult,
+        profile=MyOrcaProfile,
+        orcasimpleinput=orcasimpleinput,
+        orcablocks=orcablocks,
+        directory=output_directory,
+    )
+    calc.write_inputfiles(atoms, ["energy", "forces"])
