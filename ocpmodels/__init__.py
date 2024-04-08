7b5f09bbc9ad308f591ed3222644fc134ebac66b
lbluque <lbluque@meta.com>
2024-04-08 09:26:16 -0700

    set package version dynamically from tags/commits

--- ocpmodels/__init__.py
+++ ocpmodels/__init__.py
@@ -1,6 +1,14 @@
 """
-Copyright (c) Facebook, Inc. and its affiliates.
+Copyright (c) Meta, Inc. and its affiliates.
 
 This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("ocpmodels")
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = ""
