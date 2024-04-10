07c7e6766ea24780567f193a6f668388b3bd31b4
lbluque <lbluque@meta.com>
2024-04-10 14:32:24 -0700

    all tests pass with torch 2.2 and pyg 2.5

--- tests/datasets/test_utils.py
+++ tests/datasets/test_utils.py
@@ -13,8 +13,8 @@ def pyg_data():
 
 
 def test_rename_data_object_keys(pyg_data):
-    assert "rand_tensor" in pyg_data.keys
+    assert "rand_tensor" in pyg_data
     key_mapping = {"rand_tensor": "random_tensor"}
     pyg_data = rename_data_object_keys(pyg_data, key_mapping)
-    assert "rand_tensor" not in pyg_data.keys
-    assert "random_tensor" in pyg_data.keys
+    assert "rand_tensor" not in pyg_data
+    assert "random_tensor" in pyg_data
