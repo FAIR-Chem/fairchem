ea39398fba68e418ac93959e9d0599769622afc4
Muhammed Shuaibi <45150244+mshuaibii@users.noreply.github.com>
2024-04-11 10:08:46 -0700

    [BE] Remove hardcoded paths (#82)
    * include pkls in pip setup
    
    * remove hardcoded paths
    
    * remove unused configs
    
    * fix test imports
    
    * be moving too fast

--- data/open-catalyst-dataset/tests/test_multi_adsorbate_slab_config.py
+++ data/open-catalyst-dataset/tests/test_multi_adsorbate_slab_config.py
@@ -7,9 +7,9 @@ from ase.data import covalent_radii
 from pymatgen.analysis.adsorption import AdsorbateSiteFinder
 from pymatgen.io.ase import AseAtomsAdaptor
 
-from ocdata.configs.paths import ADSORBATES_PKL_PATH, BULK_PKL_PATH
 from ocdata.core import Adsorbate, Bulk, MultipleAdsorbateSlabConfig, Slab
 from ocdata.core.adsorbate_slab_config import get_interstitial_distances
+from ocdata.databases.pkls import ADSORBATES_PKL_PATH, BULK_PKL_PATH
 
 
 @pytest.fixture(scope="class")
