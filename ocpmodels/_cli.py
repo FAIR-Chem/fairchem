815a52b994797e432e1fc2b3e05de27ce0c8e17e
lbluque <lbluque@meta.com>
2024-04-11 13:24:24 -0700

    clean up Runner and trainer context

--- ocpmodels/_cli.py
+++ ocpmodels/_cli.py
@@ -11,7 +11,8 @@ import copy
 import logging
 from typing import TYPE_CHECKING
 
-import submitit
+from submitit import AutoExecutor
+from submitit.helpers import Checkpointable, DelayedSubmission
 
 from ocpmodels.common.flags import flags
 from ocpmodels.common.utils import (
@@ -26,27 +27,27 @@ if TYPE_CHECKING:
     import argparse
 
 
-class Runner(submitit.helpers.Checkpointable):
-    def __init__(self) -> None:
+class Runner(Checkpointable):
+    def __init__(self, distributed: bool = False) -> None:
         self.config = None
+        self.distributed = distributed
 
-    def __call__(self, config) -> None:
-        with new_trainer_context(args=args, config=config) as ctx:
+    def __call__(self, config: dict) -> None:
+        with new_trainer_context(config=config, distributed=self.distributed) as ctx:
             self.config = ctx.config
             self.task = ctx.task
             self.trainer = ctx.trainer
-
             self.task.setup(self.trainer)
             self.task.run()
 
     def checkpoint(self, *args, **kwargs):
-        new_runner = Runner()
+        new_runner = Runner(self.distributed)
         self.trainer.save(checkpoint_file="checkpoint.pt", training_state=True)
         self.config["checkpoint"] = self.task.chkpt_path
         self.config["timestamp_id"] = self.trainer.timestamp_id
         if self.trainer.logger is not None:
             self.trainer.logger.mark_preempting()
-        return submitit.helpers.DelayedSubmission(new_runner, self.config)
+        return DelayedSubmission(new_runner, self.config)
 
 
 def main():
@@ -64,9 +65,7 @@ def main():
         configs = create_grid(config, args.sweep_yml) if args.sweep_yml else [config]
 
         logging.info(f"Submitting {len(configs)} jobs")
-        executor = submitit.AutoExecutor(
-            folder=args.logdir / "%j", slurm_max_num_timeout=3
-        )
+        executor = AutoExecutor(folder=args.logdir / "%j", slurm_max_num_timeout=3)
         executor.update_parameters(
             name=args.identifier,
             mem_gb=args.slurm_mem,
@@ -81,7 +80,7 @@ def main():
         for config in configs:
             config["slurm"] = copy.deepcopy(executor.parameters)
             config["slurm"]["folder"] = str(executor.folder)
-        jobs = executor.map_array(Runner(), configs)
+        jobs = executor.map_array(Runner(distributed=args.distributed), configs)
         logging.info(f"Submitted jobs: {', '.join([job.job_id for job in jobs])}")
         log_file = save_experiment_log(args, jobs, configs)
         logging.info(f"Experiment log saved to: {log_file}")
