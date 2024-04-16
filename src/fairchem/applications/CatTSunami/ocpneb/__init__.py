f7d498d426c1a91d05cae489001d9505f43bc281
Brook Wander <brook.l.wander@gmail.com>
2024-04-16 14:58:17 +0000

    initial commit

---
+++ src/fairchem/applications/CatTSunami/ocpneb/__init__.py
@@ -0,0 +1,2 @@
+from ocpneb.core.reaction import Reaction
+from ocpneb.core.OCPdyNEB import OCPdyNEB
---
+++ src/fairchem/applications/CatTSunami/ocpneb/configs/paths.py
@@ -0,0 +1,5 @@
+DISSOCIATION_REACTION_DB_PATH = "/home/jovyan/ocpneb/database/dissociation_reactions_20Oct23.pkl"
+DESORPTION_REACTION_DB_PATH = "../../database/desorptions_9Aug23.pkl"
+TRANSFER_REACTION_DB_PATH = "../../database/transfers_5Sept23.pkl"
+
+CHECKPOINT_PATH = "/home/jovyan/checkpoints/eq2_31M_ec4_allmd.pt"
