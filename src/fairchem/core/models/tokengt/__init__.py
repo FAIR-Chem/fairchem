6145977f409c7be17ed866bc51394c9038db60fe
Ryan Liu <ryanliu@escher.lbl.gov>
2024-05-13 16:37:08 -0700

    drafted tokengt

---
+++ src/fairchem/core/models/tokengt/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+from .models.tokengt import TokenGTModel
+from __future__ import annotations
+
+__all__ = ["TokenGTModel"]
