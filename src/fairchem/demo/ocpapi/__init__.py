d049299d3101c1f6b0a3d6d0c263aa7108fbc620
Luis Barroso-Luque <lbluque@users.noreply.github.com>
2024-05-14 15:09:41 -0700

    PyPi CI workflows (#690)
    * build distributions separately
    
    * draft release workflows
    
    * build V5
    
    * test-pypi to test
    
    * no docs for right now
    
    * check ref_name
    
    * no test for now
    
    * set test environment for testing
    
    * environments
    
    * add more release packages
    
    * fix dist paths
    
    * final release and test-release workflows
    
    * add cattsunami and fix project urls
    
    * new fairchem-core readme
    
    * use test-pypi in environment url
    
    * update ocpapi README.md
    
    * fix only-include
    
    * add version
    
    * add version
    
    * no trigger on release for test-release.yml
    
    * temporary test-release as release
    
    * revert back to real release
    
    * add cattsunami to build.yml
    
    * revert real release.yml

--- src/fairchem/demo/ocpapi/__init__.py
+++ src/fairchem/demo/ocpapi/__init__.py
@@ -1,2 +1,20 @@
+"""
+Copyright (c) Meta, Inc. and its affiliates.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+"""
+
+from __future__ import annotations
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("fairchem.demo.ocpapi")
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = ""
+
+
 from .client import *  # noqa
 from .workflows import *  # noqa
