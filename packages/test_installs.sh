0b0beeffa827c6da01ea40e9edf996c50bf14e47
Misko <misko@meta.com>
2024-05-16 23:56:21 +0000

    update
    Former-commit-id: de8658c9bdbf3ae771a1beb91d2d158da95887a1

--- packages/test_installs.sh
+++ packages/test_installs.sh
@@ -1,7 +1,7 @@
 packages_to_build="fairchem-core fairchem-data-oc fairchem-demo-ocpapi fairchem-applications-cattsunami"
 pys="python3.10" # python3.9"
 
-install_reqs() {
+install_reqs_dev() {
     reqs="https://raw.githubusercontent.com/FAIR-Chem/fairchem/main/packages/requirements.txt https://raw.githubusercontent.com/FAIR-Chem/fairchem/main/packages/requirements-optional.txt"
     for req in $reqs; do
         pip install -r $req
@@ -9,9 +9,17 @@ install_reqs() {
     pip install syrupy
 }
 
+
+install_reqs() {
+    pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+    pip install torch_geometric
+    pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.3.0+cpu.html
+    pip install syrupy
+}
+
 build_install_and_test_from_git_and_test() {( set -e
     py=$1
-    install_reqs
+    install_reqs_dev
 
     git clone https://github.com/FAIR-Chem/fairchem.git
     pushd fairchem/packages
@@ -34,8 +42,9 @@ build_install_and_test_from_pip_clone_git_and_test() {( set -e
     done
     
     git clone https://github.com/FAIR-Chem/fairchem.git
-
+    pushd fairchem
     $py -m pytest fairchem/tests
+    popd
 )}
 
 tmpdir=`mktemp -d` 
