9b87082232dd46ae5f589cb6e748febed815d222
Misko <misko@meta.com>
2024-05-30 22:52:34 +0000

    do pbc per system

--- src/fairchem/core/models/base.py
+++ src/fairchem/core/models/base.py
@@ -69,12 +69,47 @@ class BaseModel(nn.Module):
 
         if use_pbc:
             if otf_graph:
-                edge_index, cell_offsets, neighbors = radius_graph_pbc(
-                    data,
-                    cutoff,
-                    max_neighbors,
-                    enforce_max_neighbors_strictly,
+                edge_index_per_system, cell_offsets_per_system, neighbors_per_system = (
+                    list(
+                        zip(
+                            *[
+                                radius_graph_pbc(
+                                    data[idx],
+                                    cutoff,
+                                    max_neighbors,
+                                    enforce_max_neighbors_strictly,
+                                )
+                                for idx in range(len(data))
+                            ]
+                        )
+                    )
                 )
+
+                # atom indexs in the edge_index need to be offset
+                atom_index_offset = data.natoms.cumsum(dim=0).roll(1)
+                atom_index_offset[0] = 0
+                edge_index = torch.hstack(
+                    [
+                        edge_index_per_system[idx] + atom_index_offset[idx]
+                        for idx in range(len(data))
+                    ]
+                )
+                cell_offsets = torch.vstack(cell_offsets_per_system)
+                neighbors = torch.hstack(neighbors_per_system)
+
+                ## TODO this is the original call, but blows up with memory
+                ## using two different samples
+                ## sid='mp-675045-mp-675045-0-7' (MPTRAJ)
+                ## sid='75396' (OC22)
+                # edge_index, cell_offsets, neighbors = radius_graph_pbc(
+                #     data,
+                #     cutoff,
+                #     max_neighbors,
+                #     enforce_max_neighbors_strictly,
+                # )
+                # assert (_edge_index == edge_index).all().item()
+                # assert (_cell_offsets == cell_offsets).all().item()
+                # assert (_neighbors == neighbors).all().item()
 
             out = get_pbc_distances(
                 data.pos,
