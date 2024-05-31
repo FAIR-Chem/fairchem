70abf0feb5e1a22636027a5858d1b1e82840205b
Misko <misko@meta.com>
2024-05-31 17:35:24 +0000

    add multiple mapping
    Former-commit-id: 0f0dd8c96391cb7a57099a4852826a5a3ee8d3eb

--- src/fairchem/core/datasets/_utils.py
+++ src/fairchem/core/datasets/_utils.py
@@ -10,10 +10,13 @@ from __future__ import annotations
 import typing
 
 if typing.TYPE_CHECKING:
+    from typing import Union, List
     from torch_geometric.data import Data
 
 
-def rename_data_object_keys(data_object: Data, key_mapping: dict[str, str]) -> Data:
+def rename_data_object_keys(
+    data_object: Data, key_mapping: dict[str, Union[str, List[str]]]
+) -> Data:
     """Rename data object keys
 
     Args:
@@ -23,9 +26,14 @@ def rename_data_object_keys(data_object: Data, key_mapping: dict[str, str]) -> D
     for _property in key_mapping:
         # catch for test data not containing labels
         if _property in data_object:
-            new_property = key_mapping[_property]
-            if new_property not in data_object:
+            list_of_new_keys = key_mapping[_property]
+            if type(list_of_new_keys) is str:
+                list_of_new_keys = [list_of_new_keys]
+            for new_property in list_of_new_keys:
+                if new_property == _property:
+                    continue
+                assert new_property not in data_object
                 data_object[new_property] = data_object[_property]
+            if _property not in list_of_new_keys:
                 del data_object[_property]
-
     return data_object
