609d0dc0c431425b06972b2cf887c269ab51c264
lbluque <lbluque@meta.com>
2024-06-04 16:09:54 -0700

    unit tests

--- tests/core/common/test_optimizable.py
+++ tests/core/common/test_optimizable.py
@@ -4,33 +4,30 @@ import numpy as np
 import numpy.testing as npt
 
 from ase import build
-from ase.optimize import LBFGS, FIRE2
+from ase.optimize import LBFGS, FIRE
+from ase.filters import UnitCellFilter
 from fairchem.core.datasets import data_list_collater
 from fairchem.core.preprocessing.atoms_to_graphs import AtomsToGraphs
 from fairchem.core.common.relaxation.ase_utils import OCPCalculator
-from fairchem.core.common.relaxation import OptimizableBatch
+from fairchem.core.common.relaxation import OptimizableBatch, OptimizableUnitCellBatch
 
 
 @pytest.fixture
 def calculator(tmpdir):
-    # TODO load trainer directly - add a from_checkpoint class method to Trainer class
-    # ckpt_path = Path(__file__).parent.resolve() / "s2efs_with_linref.pt"
-    # calc = OCPCalculator(checkpoint_path=str(ckpt_path), trainer="eqV2_trainer_", seed=0, cpu=False)
     calc = OCPCalculator(
-        model_name="EquiformerV2-31M-S2EF-OC20-All+MD", local_cache=tmpdir
+        model_name="EquiformerV2-31M-S2EF-OC20-All+MD", local_cache=tmpdir, seed=0
     )
-    # removing amp gives non-flakey results
-    # calc.scaler = None
+    # TODO debug this
+    # removing amp so that we always get float32 predictions
+    calc.trainer.scaler = None
     return calc
 
 
 @pytest.fixture(scope="function")
 def atoms_list():
     atoms_list = [
-        build.make_supercell(build.bulk("Cu", "fcc", a=4.0, cubic=True), 2 * np.eye(3)),
+        build.bulk("Cu", "fcc", a=4.0, cubic=True),
         build.bulk("NaCl", crystalstructure="rocksalt", a=3.5),
-        # TODO check why relaxing a surface does not match
-        # build.fcc111("Pt", size=[2, 2, 3], vacuum=8, periodic=True)
     ]
     return atoms_list
 
@@ -41,7 +38,7 @@ def batch(atoms_list):
     return data_list_collater([a2g.convert(atoms) for atoms in atoms_list])
 
 
-@pytest.fixture(params=[LBFGS, FIRE2])
+@pytest.fixture(params=[LBFGS, FIRE])
 def optimizer_cls(request):
     return request.param
 
@@ -60,8 +57,48 @@ def test_ase_relaxation(atoms_list, batch, calculator, optimizer_cls):
         opt = optimizer_cls(atoms)
         opt.run(0.05, 50)
 
+    # compare energy and atom positions
     for a1, a2 in zip(atoms_list, obatch.get_atoms_list()):
-        assert np.round(a1.get_potential_energy(), 1) == np.round(
-            a2.get_potential_energy(), 1
+        assert a1.get_potential_energy() / len(a1) == pytest.approx(
+            a2.get_potential_energy() / len(a2), abs=0.05
         )
-        npt.assert_allclose(a1.positions, a2.positions)
+        pnorm1 = np.linalg.norm(a1.positions, axis=1)
+        pnorm2 = np.linalg.norm(a2.positions, axis=1)
+        npt.assert_allclose(pnorm1, pnorm2, atol=0.05)
+
+
+@pytest.mark.skip("Skip until we have a test model that can predict stress")
+def test_ase_cell_relaxation(atoms_list, batch, calculator, optimizer_cls):
+    """Tests batch relaxation using ASE optimizers."""
+
+    cell_factor = batch.natoms.cpu().numpy().mean()
+    obatch = OptimizableUnitCellBatch(
+        batch, trainer=calculator.trainer, numpy=True, cell_factor=cell_factor
+    )
+
+    # optimize atoms in batch using ASE
+    batch_optimizer = optimizer_cls(obatch)
+    batch_optimizer.run(0.05, 50)
+
+    # optimize atoms one-by-one
+    for atoms in atoms_list:
+        atoms.calc = calculator
+        opt = optimizer_cls(UnitCellFilter(atoms, cell_factor=cell_factor))
+        opt.run(0.05, 50)
+
+    # compare energy, atom positions and cell
+    for a1, a2 in zip(atoms_list, obatch.get_atoms_list()):
+        assert a1.get_potential_energy() / len(a1) == pytest.approx(
+            a2.get_potential_energy() / len(a2), abs=0.05
+        )
+
+        pnorm1 = np.linalg.norm(a1.positions, axis=1)
+        pnorm2 = np.linalg.norm(a2.positions, axis=1)
+        npt.assert_allclose(pnorm1, pnorm2, atol=0.05)
+
+        cnorm1 = np.linalg.norm(a1.cell.array, axis=1)
+        cnorm2 = np.linalg.norm(a2.cell.array, axis=1)
+        npt.assert_allclose(cnorm1, cnorm2, atol=0.05)
+        npt.assert_allclose(
+            a1.cell.array.T / cnorm1, a2.cell.array.T / cnorm2, atol=0.01
+        )
