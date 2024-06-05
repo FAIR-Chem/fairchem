d09c041e2178950f1ba925e40727f498821d1ad3
lbluque <lbluque@meta.com>
2024-06-04 18:12:34 -0700

    move update mask to optimizable

--- src/fairchem/core/common/relaxation/optimizers/lbfgs_torch.py
+++ src/fairchem/core/common/relaxation/optimizers/lbfgs_torch.py
@@ -34,7 +34,6 @@ class LBFGS:
         save_full_traj: bool = True,
         traj_dir: Path | None = None,
         traj_names: list[str] | None = None,
-        mask_converged: bool = True,
     ) -> None:
         """
         Args:
@@ -47,7 +46,6 @@ class LBFGS:
             save_full_traj: wether to save full trajectory
             traj_dir: path to save trajectories in
             traj_names: list of trajectory files names
-            mask_converged: whether to mask batches where all atoms are below convergence threshold
         """
         self.optimizable = optimizable_batch
         self.maxstep = maxstep
@@ -59,41 +57,11 @@ class LBFGS:
         self.save_full = save_full_traj
         self.traj_dir = traj_dir
         self.traj_names = traj_names
-        self.mask_converged = mask_converged
-        self.otf_graph = optimizable_batch.trainer._unwrapped_model.otf_graph
 
         assert not self.traj_dir or (
             traj_dir and len(traj_names)
         ), "Trajectory names should be specified to save trajectories"
         logging.info("Step   Fmax(eV/A)")
-
-        if not self.otf_graph and "edge_index" not in self.optimizable.batch:
-            self.optimizable.update_graph()
-
-    def set_positions(self, positions, update_mask):
-        if self.mask_converged:
-            positions = torch.where(update_mask.unsqueeze(1), positions, 0.0)
-
-        self.optimizable.set_positions(positions.to(dtype=torch.float32))
-
-        if not self.otf_graph:
-            self.optimizable.update_graph()
-
-    def check_convergence(self, iteration):
-        energy = self.optimizable.get_potential_energies()
-
-        # TODO check why forces are cast to float64
-        forces = self.optimizable.get_forces(apply_constraint=True)
-        forces = forces.to(dtype=torch.float64)
-
-        max_forces = self.optimizable.get_max_forces()
-        logging.info(
-            f"{iteration} " + " ".join(f"{x:0.3f}" for x in max_forces.tolist())
-        )
-
-        # (batch_size) -> (nAtoms)
-        max_forces = max_forces[self.optimizable.batch_indices]
-        return max_forces.lt(self.fmax), energy, forces
 
     def run(self, fmax, steps):
         self.fmax = fmax
@@ -114,34 +82,24 @@ class LBFGS:
 
         iteration = 0
         converged = False
-        converged_mask = torch.zeros(
-            self.optimizable.batch_indices.shape[0],
-            dtype=torch.bool,
-            device=self.device,
-        )
         while iteration < steps and not converged:
-            _converged_mask, energy, forces = self.check_convergence(iteration)
-            # Models like GemNet-OC can have random noise in their predictions.
-            # Here we ensure atom positions are not being updated after already
-            # hitting the desired convergence criteria.
-            converged_mask = torch.logical_or(converged_mask, _converged_mask)
-            converged = torch.all(converged_mask)
-            update_mask = torch.logical_not(converged_mask)
+            logging.info(
+                f"{iteration} "
+                + " ".join(
+                    f"{x:0.3f}" for x in self.optimizable.get_max_forces().tolist()
+                )
+            )
 
             if self.trajectories is not None and (
                 self.save_full or converged or iteration == steps - 1 or iteration == 0
             ):
                 # forces and mask can be augmented
-                self.write(
-                    energy,
-                    forces[: self.optimizable.batch.pos.shape[0]],
-                    update_mask[: self.optimizable.batch.pos.shape[0]],
-                )
+                self.write()
 
-            if not converged and iteration < steps - 1:
-                self.step(iteration, forces, update_mask)
-
+            self.step(iteration)
             iteration += 1
+            # this is calling get max forces twice since it is called for logging above, not super bueno
+            converged = self.optimizable.converged(self.fmax)
 
         # GPU memory usage as per nvidia-smi seems to gradually build up as
         # batches are processed. This releases unoccupied cached memory.
@@ -154,9 +112,9 @@ class LBFGS:
                 traj_fl = Path(self.traj_dir / f"{name}.traj_tmp", mode="w")
                 traj_fl.rename(traj_fl.with_suffix(".traj"))
 
-        self.optimizable.batch.energy = energy
-        # forces are augmented
-        self.optimizable.batch.forces = forces[: self.optimizable.batch.pos.shape[0]]
+        # set predicted values to batch
+        for name, value in self.optimizable.results.items():
+            setattr(self.optimizable.batch, name, value)
 
         return self.optimizable.batch
 
@@ -176,15 +134,8 @@ class LBFGS:
             (x * y).sum(dim=-1), self.optimizable.batch_indices, reduce="sum"
         )
 
-    def step(
-        self,
-        iteration: int,
-        forces: torch.Tensor | None,
-        update_mask: torch.Tensor,
-    ) -> None:
-        if forces is None:
-            forces = self.optimizable.get_forces(apply_constraint=True)
-
+    def step(self, iteration: int) -> None:
+        forces = self.optimizable.get_forces(apply_constraint=True)
         r = self.optimizable.get_positions().to(dtype=torch.float64)
 
         # Update s, y, rho
@@ -220,15 +171,14 @@ class LBFGS:
             # Same configuration again (maybe a restart):
             return
 
-        self.set_positions(r + dr, update_mask)
-
+        self.optimizable.set_positions(r + dr)
         self.r0 = r
         self.f0 = forces
 
-    def write(self, energy, forces, update_mask) -> None:
-        self.optimizable.batch.energy, self.optimizable.batch.forces = energy, forces
+    def write(self) -> None:
         atoms_objects = self.optimizable.get_atoms_list()
-        update_mask_ = torch.split(update_mask, self.optimizable.batch.natoms.tolist())
-        for atm, traj, mask in zip(atoms_objects, self.trajectories, update_mask_):
-            if mask[0] or not self.save_full:
+        for atm, traj, mask in zip(
+            atoms_objects, self.trajectories, self.optimizable.update_mask
+        ):
+            if mask or not self.save_full:
                 traj.write(atm)
