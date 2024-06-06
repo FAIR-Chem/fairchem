bcaa4ad87099d7317d5eb45acf5628edb2cd9a98
lbluque <lbluque@meta.com>
2024-06-05 17:12:55 -0700

    more (unfinished) tests

--- tests/core/common/test_optimizable.py
+++ tests/core/common/test_optimizable.py
@@ -26,9 +26,11 @@ def calculator(tmpdir):
 @pytest.fixture(scope="function")
 def atoms_list():
     atoms_list = [
-        build.bulk("Cu", "fcc", a=4.0, cubic=True),
-        build.bulk("NaCl", crystalstructure="rocksalt", a=3.5),
+        build.bulk("Cu", "fcc", a=3.8, cubic=True),
+        build.bulk("NaCl", crystalstructure="rocksalt", a=5.8),
     ]
+    for atoms in atoms_list:
+        atoms.rattle(stdev=0.05, seed=0)
     return atoms_list
 
 
@@ -38,7 +40,7 @@ def batch(atoms_list):
     return data_list_collater([a2g.convert(atoms) for atoms in atoms_list])
 
 
-@pytest.fixture(params=[LBFGS, FIRE])
+@pytest.fixture(params=[FIRE, LBFGS])
 def optimizer_cls(request):
     return request.param
 
@@ -47,15 +49,17 @@ def test_ase_relaxation(atoms_list, batch, calculator, optimizer_cls):
     """Tests batch relaxation using ASE optimizers."""
     obatch = OptimizableBatch(batch, trainer=calculator.trainer, numpy=True)
 
-    # optimize atoms in batch using ASE
-    batch_optimizer = optimizer_cls(obatch)
-    batch_optimizer.run(0.05, 50)
-
     # optimize atoms one-by-one
     for atoms in atoms_list:
         atoms.calc = calculator
         opt = optimizer_cls(atoms)
-        opt.run(0.05, 50)
+        opt.run(0.01, 20)
+
+    # TODO relaxations with FIRE give nans for positions which leads to a criptic
+    #  eror in edge_rot_mat (since edges are empty)
+    # optimize atoms in batch using ASE
+    batch_optimizer = optimizer_cls(obatch)
+    batch_optimizer.run(0.01, 20)
 
     # compare energy and atom positions
     for a1, a2 in zip(atoms_list, obatch.get_atoms_list()):
@@ -64,13 +68,45 @@ def test_ase_relaxation(atoms_list, batch, calculator, optimizer_cls):
         )
         pnorm1 = np.linalg.norm(a1.positions, axis=1)
         pnorm2 = np.linalg.norm(a2.positions, axis=1)
-        npt.assert_allclose(pnorm1, pnorm2, atol=0.05)
+        npt.assert_allclose(pnorm1, pnorm2, atol=0.01)
+        npt.assert_allclose(a1.positions, a2.positions, rtol=0.01, atol=0.05)
+
+
+@pytest.mark.parametrize("mask_converged", [False, True])
+def test_batch_relaxation_mask(atoms_list, calculator, mask_converged):
+    """Test that masking is working as intended!"""
+    # relax only the first atom in list
+    atoms = atoms_list[0]
+    atoms.calc = calculator
+    opt = LBFGS(atoms)
+    opt.run(0.01, 20)
+
+    # now create a batch
+    batch = data_list_collater([calculator.a2g.convert(atoms) for atoms in atoms_list])
+    obatch = OptimizableBatch(
+        batch, trainer=calculator.trainer, numpy=True, mask_converged=mask_converged
+    )
+
+    npt.assert_allclose(batch.pos[batch.batch == 0].cpu().numpy(), atoms.positions)
+    batch_opt = LBFGS(obatch)
+    batch_opt.run(0.01, 20)
+
+    if mask_converged:
+        # assert preconverged structure was not changed at all
+        assert np.allclose(batch.pos[batch.batch == 0].cpu().numpy(), atoms.positions)
+        assert not np.allclose(
+            batch.pos[batch.batch == 1].cpu().numpy(), atoms_list[1].positions
+        )
+    else:
+        # assert that it was changed
+        assert not np.allclose(
+            batch.pos[batch.batch == 0].cpu().numpy(), atoms.positions
+        )
 
 
 @pytest.mark.skip("Skip until we have a test model that can predict stress")
 def test_ase_cell_relaxation(atoms_list, batch, calculator, optimizer_cls):
     """Tests batch relaxation using ASE optimizers."""
-
     cell_factor = batch.natoms.cpu().numpy().mean()
     obatch = OptimizableUnitCellBatch(
         batch, trainer=calculator.trainer, numpy=True, cell_factor=cell_factor
@@ -78,13 +114,13 @@ def test_ase_cell_relaxation(atoms_list, batch, calculator, optimizer_cls):
 
     # optimize atoms in batch using ASE
     batch_optimizer = optimizer_cls(obatch)
-    batch_optimizer.run(0.05, 50)
+    batch_optimizer.run(0.01, 20)
 
     # optimize atoms one-by-one
     for atoms in atoms_list:
         atoms.calc = calculator
         opt = optimizer_cls(UnitCellFilter(atoms, cell_factor=cell_factor))
-        opt.run(0.05, 50)
+        opt.run(0.01, 20)
 
     # compare energy, atom positions and cell
     for a1, a2 in zip(atoms_list, obatch.get_atoms_list()):
@@ -94,11 +130,12 @@ def test_ase_cell_relaxation(atoms_list, batch, calculator, optimizer_cls):
 
         pnorm1 = np.linalg.norm(a1.positions, axis=1)
         pnorm2 = np.linalg.norm(a2.positions, axis=1)
-        npt.assert_allclose(pnorm1, pnorm2, atol=0.05)
+        npt.assert_allclose(pnorm1, pnorm2, atol=0.01)
+        npt.assert_allclose(a1.positions, a2.positions, rtol=0.01, atol=0.05)
 
         cnorm1 = np.linalg.norm(a1.cell.array, axis=1)
         cnorm2 = np.linalg.norm(a2.cell.array, axis=1)
-        npt.assert_allclose(cnorm1, cnorm2, atol=0.05)
+        npt.assert_allclose(cnorm1, cnorm2, atol=0.01)
         npt.assert_allclose(
-            a1.cell.array.T / cnorm1, a2.cell.array.T / cnorm2, atol=0.01
+            a1.cell.array.T / cnorm1, a2.cell.array.T / cnorm2, rtol=0.01
         )
