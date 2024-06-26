143d7a611a029a3dd9cdd2dd973ed9bc82247de5
lbluque <lbluque@meta.com>
2024-06-26 14:31:41 -0700

    test applying element refs

--- tests/core/modules/test_element_references.py
+++ tests/core/modules/test_element_references.py
@@ -1,65 +1,42 @@
-from itertools import product
-from random import choice
 import pytest
 import numpy.testing as npt
 import numpy as np
 import torch
 
-from pymatgen.core.periodic_table import Element
-from pymatgen.core import Structure
-
-from fairchem.core.datasets import LMDBDatabase, AseDBDataset
+from fairchem.core.datasets import data_list_collater
 from fairchem.core.modules.element_references import (
     fit_linear_references,
     create_element_references,
     LinearReference,
 )
 
-# create some dummy elemental energies from ionic radii (ignore deuterium and tritium included in pmg)
-DUMMY_EREFS = np.concatenate(
-    [[0], [e.average_ionic_radius for e in Element if e.name not in ("D", "T")]]
-)
-MAX_NUM_ELEMENTS = len(DUMMY_EREFS) - 1
 
-
-@pytest.fixture
-def dataset(tmpdir):
-    # a dummy dataset with binaries with energy that depends on composition only plus noise
-    all_binaries = list(product(list(Element), repeat=2))
-    rng = np.random.default_rng(seed=0)
-
-    with LMDBDatabase(tmpdir / "dummy.aselmdb") as db:
-        for _ in range(1000):
-            elements = choice(all_binaries)
-            structure = Structure.from_prototype("cscl", species=elements, a=2.0)
-            energy = (
-                sum(e.average_ionic_radius for e in elements)
-                + 0.05 * rng.random() * DUMMY_EREFS.mean()
-            )
-            atoms = structure.to_ase_atoms()
-            db.write(atoms, data={"energy": energy})
-
-    dataset = AseDBDataset(
-        config={
-            "src": str(tmpdir / "dummy.aselmdb"),
-            "a2g_args": {"r_data_keys": ["energy"]},
-        }
-    )
-    return dataset
-
-
-@pytest.fixture
-def element_refs(dataset):
+@pytest.fixture(scope="session")
+def element_refs(dataset, max_num_elements):
     return fit_linear_references(
         ["energy"],
         dataset,
         batch_size=16,
         shuffle=False,
-        max_num_elements=MAX_NUM_ELEMENTS,
+        max_num_elements=max_num_elements,
     )
 
 
-def test_create_element_references(element_refs, tmpdir):
+def test_apply_linear_references(element_refs, dataset, dummy_element_refs):
+    max_noise = 0.05 * dummy_element_refs.mean()
+
+    # check that removing element refs keeps only values within max noise
+    batch = data_list_collater([d for d in dataset], otf_graph=True)
+    energy = batch.energy.clone().view(len(batch), -1)
+    deref_energy = element_refs["energy"].dereference(energy, batch)
+    assert all(deref_energy <= max_noise)
+
+    # and check that we recover the total energy from applying references
+    ref_energy = element_refs["energy"](deref_energy, batch)
+    assert torch.allclose(ref_energy, energy)
+
+
+def test_create_element_references(element_refs, tmp_path):
     # test from state dict
     sdict = element_refs["energy"].state_dict()
 
@@ -70,8 +47,8 @@ def test_create_element_references(element_refs, tmpdir):
     )
 
     # test from saved stated dict
-    torch.save(sdict, tmpdir / "linref.pt")
-    refs = create_element_references(file=tmpdir / "linref.pt")
+    torch.save(sdict, tmp_path / "linref.pt")
+    refs = create_element_references(file=tmp_path / "linref.pt")
     assert isinstance(refs, LinearReference)
     assert torch.allclose(
         element_refs["energy"].element_references, refs.element_references
@@ -79,10 +56,9 @@ def test_create_element_references(element_refs, tmpdir):
 
     # from a legacy numpy npz file
     np.savez(
-        tmpdir / "linref.npz", coeff=element_refs["energy"].element_references.numpy()
+        tmp_path / "linref.npz", coeff=element_refs["energy"].element_references.numpy()
     )
-    # breakpoint()
-    refs = create_element_references(file=tmpdir / "linref.npz")
+    refs = create_element_references(file=tmp_path / "linref.npz")
     assert isinstance(refs, LinearReference)
     assert torch.allclose(
         element_refs["energy"].element_references, refs.element_references
@@ -90,34 +66,36 @@ def test_create_element_references(element_refs, tmpdir):
 
     # from a numpy npz file
     np.savez(
-        tmpdir / "linref.npz",
+        tmp_path / "linref.npz",
         element_references=element_refs["energy"].element_references.numpy(),
     )
-    # breakpoint()
-    refs = create_element_references(file=tmpdir / "linref.npz")
+
+    refs = create_element_references(file=tmp_path / "linref.npz")
     assert isinstance(refs, LinearReference)
     assert torch.allclose(
         element_refs["energy"].element_references, refs.element_references
     )
 
 
-def test_fit_linear_references(element_refs, dataset):
+def test_fit_linear_references(
+    element_refs, dataset, max_num_elements, dummy_element_refs
+):
     # create the composition matrix
     energy = np.array([d.energy for d in dataset])
     cmatrix = np.vstack(
         [
-            np.bincount(d.atomic_numbers.int().numpy(), minlength=MAX_NUM_ELEMENTS + 1)
+            np.bincount(d.atomic_numbers.int().numpy(), minlength=max_num_elements + 1)
             for d in dataset
         ]
     )
     mask = cmatrix.sum(axis=0) != 0.0
 
     # fit using numpy
-    element_refs_np = np.zeros(MAX_NUM_ELEMENTS + 1)
+    element_refs_np = np.zeros(max_num_elements + 1)
     element_refs_np[mask] = np.linalg.lstsq(cmatrix[:, mask], energy, rcond=None)[0]
 
     # length is max_num_elements + 1, since H starts at 1
-    assert len(element_refs["energy"].element_references) == MAX_NUM_ELEMENTS + 1
+    assert len(element_refs["energy"].element_references) == max_num_elements + 1
     # first element is dummy, should always be zero
     assert element_refs["energy"].element_references[0] == 0.0
     # elements not present should be zero
@@ -128,7 +106,7 @@ def test_fit_linear_references(element_refs, dataset):
     )
     # close enough to ground truth w/out noise
     npt.assert_allclose(
-        DUMMY_EREFS[mask],
+        dummy_element_refs[mask],
         element_refs["energy"].element_references.numpy()[mask],
         atol=5e-2,
     )
