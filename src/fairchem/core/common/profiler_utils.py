3afd45448a9cb1740f6b8904ba7b09b315e0a83c
Ray Gao <rgao@meta.com>
2024-07-10 21:19:03 +0000

    add comment

--- src/fairchem/core/common/profiler_utils.py
+++ src/fairchem/core/common/profiler_utils.py
@@ -24,7 +24,26 @@ def get_default_profiler_handler(run_id: str, output_dir: str, logger: Logger):
     return trace_handler
 
 def get_profile_schedule(wait: int = 5, warmup: int = 5, active: int = 2):
-    """Get a profile schedule"""
+    """Get a profile schedule and total number of steps to run
+    check pytorch docs on the meaning of these paramters:
+    https://pytorch.org/docs/stable/profiler.html#torch.profiler.schedule
+    Example usage:
+    ```
+    trace_handler = get_default_profiler_handler(run_id = self.config["cmd"]["timestamp_id"],
+                                                    output_dir = self.config["cmd"]["results_dir"],
+                                                    logger = self.logger)
+    profile_schedule, total_profile_steps = get_profile_schedule()
+
+    with profile(
+        activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA],
+        schedule=profile_schedule,
+        on_trace_ready=trace_handler
+    ) as p:
+        for i in steps:
+            <code block to profile>
+            if i < total_profile_steps:
+                p.step()
+    """
     total_profile_steps = wait + warmup + active
     profile_schedule = torch.profiler.schedule(wait=wait, warmup=warmup, active=active)
 
