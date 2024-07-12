372b6ec869be5401e955d558e381677f74fab1df
Zack Ulissi <zulissi@meta.com>
2024-07-12 23:03:54 +0000

    convert all md to ipynb on launch

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -6,3 +6,6 @@ pip install -e packages/fairchem-data-oc[dev]
 pip install -e packages/fairchem-demo-ocpapi[dev]
 pip install -e packages/fairchem-applications-cattsunami
 pip install jupytext
+
+# Convert all .md docs to ipynb for easy viewing in vscode later!
+jupytext --to ipynb docs/**/*.md
