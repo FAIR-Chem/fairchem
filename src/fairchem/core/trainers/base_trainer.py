00014188bf8958befe7e556e8426f3c60d6abcff
lbluque <lbluque@meta.com>
2024-07-13 21:22:01 -0700

    default to batch_size 1 if not set in config
    Former-commit-id: d7957a30a7a119f9dce2e8495a14df3defef2b31

--- src/fairchem/core/trainers/base_trainer.py
+++ src/fairchem/core/trainers/base_trainer.py
@@ -288,7 +288,7 @@ class BaseTrainer(ABC):
             )(self.config["dataset"])
             self.train_sampler = self.get_sampler(
                 self.train_dataset,
-                self.config["optim"]["batch_size"],
+                self.config["optim"].get("batch_size", 1),
                 shuffle=True,
             )
             self.train_loader = self.get_dataloader(
@@ -309,7 +309,7 @@ class BaseTrainer(ABC):
             self.val_sampler = self.get_sampler(
                 self.val_dataset,
                 self.config["optim"].get(
-                    "eval_batch_size", self.config["optim"]["batch_size"]
+                    "eval_batch_size", self.config["optim"].get("batch_size", 1)
                 ),
                 shuffle=False,
             )
@@ -331,7 +331,7 @@ class BaseTrainer(ABC):
             self.test_sampler = self.get_sampler(
                 self.test_dataset,
                 self.config["optim"].get(
-                    "eval_batch_size", self.config["optim"]["batch_size"]
+                    "eval_batch_size", self.config["optim"].get("batch_size", 1)
                 ),
                 shuffle=False,
             )
@@ -340,27 +340,27 @@ class BaseTrainer(ABC):
                 self.test_sampler,
             )
 
-            if self.config.get("relax_dataset", None):
-                if self.config["relax_dataset"].get("use_train_settings", True):
-                    relax_config = self.config["dataset"].copy()
-                    relax_config.update(self.config["relax_dataset"])
-                else:
-                    relax_config = self.config["relax_dataset"]
+        if self.config.get("relax_dataset", None):
+            if self.config["relax_dataset"].get("use_train_settings", True):
+                relax_config = self.config["dataset"].copy()
+                relax_config.update(self.config["relax_dataset"])
+            else:
+                relax_config = self.config["relax_dataset"]
 
-                self.relax_dataset = registry.get_dataset_class(
-                    relax_config.get("format", "lmdb")
-                )(relax_config)
-                self.relax_sampler = self.get_sampler(
-                    self.relax_dataset,
-                    self.config["optim"].get(
-                        "eval_batch_size", self.config["optim"]["batch_size"]
-                    ),
-                    shuffle=False,
-                )
-                self.relax_loader = self.get_dataloader(
-                    self.relax_dataset,
-                    self.relax_sampler,
-                )
+            self.relax_dataset = registry.get_dataset_class(
+                relax_config.get("format", "lmdb")
+            )(relax_config)
+            self.relax_sampler = self.get_sampler(
+                self.relax_dataset,
+                self.config["optim"].get(
+                    "eval_batch_size", self.config["optim"].get("batch_size", 1)
+                ),
+                shuffle=False,
+            )
+            self.relax_loader = self.get_dataloader(
+                self.relax_dataset,
+                self.relax_sampler,
+            )
 
     def load_task(self):
         # Normalizer for the dataset.
