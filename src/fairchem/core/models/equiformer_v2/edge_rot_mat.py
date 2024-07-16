840e75ac96faa50af3024734baadee500988a8fd
Xiang Fu <xiangfu@meta.com>
2024-07-16 21:53:06 +0000

    fix white space warnings.

--- src/fairchem/core/models/equiformer_v2/edge_rot_mat.py
+++ src/fairchem/core/models/equiformer_v2/edge_rot_mat.py
@@ -46,7 +46,7 @@ def init_edge_rot_mat(edge_distance_vec):
 
     zprod = (norm_x @ norm_x.new_tensor([0,1,0])).abs()
     z_aligned = (zprod > 0.9999)
-    
+
     # Construct the 3D rotation matrix
     norm_x = norm_x.view(-1, 3, 1)
     norm_y = -norm_y.view(-1, 3, 1)
@@ -54,11 +54,11 @@ def init_edge_rot_mat(edge_distance_vec):
 
     edge_rot_mat_inv = torch.cat([norm_z, norm_x, norm_y], dim=2)
     edge_rot_mat = torch.transpose(edge_rot_mat_inv, 1, 2)
-    
+
     # use identity rotation matrices for edges that are already aligned with the z-axis
     output = torch.zeros_like(edge_rot_mat)
     output[~z_aligned] = edge_rot_mat[~z_aligned]
     output[z_aligned] = torch.eye(3).repeat(z_aligned.sum(), 1, 1).to(
         edge_rot_mat.device).to(edge_rot_mat.dtype).detach()
-    
+
     return output, z_aligned
