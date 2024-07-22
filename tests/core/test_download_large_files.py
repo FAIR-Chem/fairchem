9bd6c7479caab5ae4b9b36bde9933abca3983331
Daniel Levine <levineds@meta.com>
2024-07-22 21:42:07 +0000

    add unittest

---
+++ tests/core/test_download_large_files.py
@@ -0,0 +1,11 @@
+from unittest.mock import patch
+from fairchem.core.scripts import download_large_files as dl_large
+
+@patch.object(dl_large, "urlretreive")
+def test_download_large_files(url_mock):
+    def urlretrieve_mock(x, y):
+        if not os.path.exists(os.path.dirname(y)):
+            raise ValueError(f'The path to {y} does not exist. fairchem directory structure has changed,')
+    url_mock.side_effect = urlretrieve_mock
+    dl_large.main('ALL')
+
