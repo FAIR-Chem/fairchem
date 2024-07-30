39f5e2e0d71c1cdb19292880245b892ecc557cd3
Misko <misko@meta.com>
2024-07-30 16:32:30 +0000

    typo

--- tests/core/e2e/test_s2ef.py
+++ tests/core/e2e/test_s2ef.py
@@ -322,7 +322,7 @@ class TestSmallDatasetOptim:
     @pytest.mark.parametrize(
         ("model_name", "expected_energy_mae", "expected_force_mae"),
         [
-            pytest.param("gemnet", 0.4, 0.06, id="gemnet"),
+            pytest.param("gemnet_oc", 0.4, 0.06, id="gemnet_oc"),
             pytest.param("escn", 0.4, 0.06, id="escn"),
             pytest.param("equiformer_v2", 0.4, 0.06, id="equiformer_v2"),
         ],
