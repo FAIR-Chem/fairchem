029d4d3c3c246d29d46407128807d24a1b56cfa6
Luis Barroso-Luque <lbluque@users.noreply.github.com>
2024-08-04 21:14:35 -0600

    (OTF) Normalization and element references (#715)
    * denorm targets in _forward only
    
    * linear reference class
    
    * atomref in normalizer
    
    * raise input error
    
    * clean up normalizer interface
    
    * add element refs
    
    * add element refs correctly
    
    * ruff
    
    * fix save_checkpoint
    
    * reference and dereference
    
    * 2xnorm linref trainer add
    
    * clean-up
    
    * otf linear reference fit
    
    * fix tensor device
    
    * otf element references and normalizers
    
    * use only present elements when fitting
    
    * lint
    
    * _forward norm and derefd values
    
    * fix list of paths in src
    
    * total mean and std
    
    * fitted flag to avoid refitting normalizers/references on rerun
    
    * allow passing lstsq driver
    
    * element ref unit tests
    
    * remove superfluous type
    
    * lint fix
    
    * allow setting batch_size explicitly
    
    * test applying element refs
    
    * normalizer tests
    
    * increase distributed timeout
    
    * save normalizers and linear refs in otf_fit
    
    * remove debug code
    
    * fix removing refs
    
    * swap otf_fit for fit, and save all normalizers in one file
    
    * log loading and saving normalizers
    
    * fit references and normalizer scripts
    
    * lint fixes
    
    * allow absent optim key in config
    
    * lin-ref description
    
    * read files based on extension
    
    * pass seed
    
    * rename dataset fixture
    
    * check if file is none
    
    * pass generator correctly
    
    * separate method for norms and refs
    
    * add normalizer code back
    
    * fix Generator construction
    
    * import order
    
    * log warnings if multiple inputs are passed
    
    * raise Error if duplicate references or norms are set
    
    * use len batch
    
    * assert element reference targets are scalar
    
    * fix name and rename method
    
    * load and save norms and refs using same logic
    
    * fix creating normalizer
    
    * remove print statements
    
    * adding new notebook for using fairchem models with NEBs without CatTSunami enumeration (#764)
    
    * adding new notebook for using fairchem models with NEBs
    
    * adding md tutorials
    
    * blocking code cells that arent needed or take too long
    
    * warn instead of error when duplicate norm/ref target names
    
    * allow timeout to be read from config
    
    * test seed noseed ref fits
    
    * lotsa refactoring
    
    * lotsa fixing
    
    * more fixing...
    
    * num_workers zero to prevent mp issues
    
    * add otf norms smoke test and fixes
    
    * allow overriding normalization fit values
    
    * update tests
    
    * fix normalizer loading
    
    * use rmsd instead of only stdev
    
    * fix tests
    
    * correct rmsd calc and fix loading
    
    * clean up norm loading and log values
    
    * logg linear reference metrics
    
    * load element references state dict
    
    * fix loading and tests
    
    * fix imports in scripts
    
    * fix test?
    
    * fix test
    
    * use numpy as default to fit references
    
    * minor fixes
    
    * rm torch_tempdir fixture
    
    ---------
    
    Co-authored-by: Brook Wander <73855115+brookwander@users.noreply.github.com>
    Co-authored-by: Muhammed Shuaibi <45150244+mshuaibii@users.noreply.github.com>

--- src/fairchem/core/common/distutils.py
+++ src/fairchem/core/common/distutils.py
@@ -10,7 +10,8 @@ from __future__ import annotations
 import logging
 import os
 import subprocess
-from typing import TypeVar
+from datetime import timedelta
+from typing import Any, TypeVar
 
 import torch
 import torch.distributed as dist
@@ -27,6 +28,7 @@ def os_environ_get_or_throw(x: str) -> str:
 
 
 def setup(config) -> None:
+    timeout = timedelta(minutes=config.get("timeout", 30))
     if config["submit"]:
         node_list = os.environ.get("SLURM_STEP_NODELIST")
         if node_list is None:
@@ -72,6 +74,7 @@ def setup(config) -> None:
                     init_method=config["init_method"],
                     world_size=config["world_size"],
                     rank=config["rank"],
+                    timeout=timeout,
                 )
             except subprocess.CalledProcessError as e:  # scontrol failed
                 raise e
@@ -95,10 +98,11 @@ def setup(config) -> None:
             rank=world_rank,
             world_size=world_size,
             init_method="env://",
+            timeout=timeout,
         )
     else:
         config["local_rank"] = int(os.environ.get("LOCAL_RANK", config["local_rank"]))
-        dist.init_process_group(backend=config.get("backend", "nccl"))
+        dist.init_process_group(backend=config.get("backend", "nccl"), timeout=timeout)
 
 
 def cleanup() -> None:
@@ -133,6 +137,14 @@ def broadcast(
     if get_world_size() == 1:
         return
     dist.broadcast(tensor, src, group, async_op)
+
+
+def broadcast_object_list(
+    object_list: list[Any], src: int, group=dist.group.WORLD, device: str | None = None
+) -> None:
+    if get_world_size() == 1:
+        return
+    dist.broadcast_object_list(object_list, src, group, device)
 
 
 def all_reduce(
