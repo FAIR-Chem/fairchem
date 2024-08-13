6372fe308da45ba6990f0ef77830bad735b6570a
Ray Gao <rgao@meta.com>
2024-08-13 16:50:34 +0000

    str paths

--- src/fairchem/core/common/slurm.py
+++ src/fairchem/core/common/slurm.py
@@ -14,11 +14,11 @@ def add_timestamp_id_to_submission_pickle(slurm_folder: str, slurm_job_id: str, 
     # instead a-priori before the submission starts (ie: if we had a db to store a global job unique job)
     submission_pickle_path = JobPaths(folder=slurm_folder, job_id=slurm_job_id)
     try:
-        with open(submission_pickle_path, "rb") as f:
+        with open(str(submission_pickle_path), "rb") as f:
             pkl = pickle.load(f)
             # args passed to the runner function (0 is the input_dict): https://github.com/FAIR-Chem/fairchem/blob/main/src/fairchem/core/_cli.py#L36
             pkl.args[0]["timestamp_id"] = timestamp_id
-        with open(submission_pickle_path, "wb") as f:
+        with open(str(submission_pickle_path), "wb") as f:
             pickle.dump(pkl, f)
     except Exception as e:
         # Since this only affects the ability to resume jobs, if the pickle doesn't exist
