9972c12461f6c614224c22f502f3c11ed484d7d6
Misko <misko@meta.com>
2024-08-15 21:07:46 +0000

    remove backbone and heads model

--- src/fairchem/core/models/equiformer_v2/__init__.py
+++ src/fairchem/core/models/equiformer_v2/__init__.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from .equiformer_v2 import EquiformerV2BackboneAndHeads
 from .equiformer_v2_deprecated import EquiformerV2
 
-__all__ = ["EquiformerV2", "EquiformerV2BackboneAndHeads"]
+__all__ = ["EquiformerV2"]
