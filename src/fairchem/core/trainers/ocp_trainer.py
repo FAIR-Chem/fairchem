f08de5fbb32cdea68c1d2d73331d88a721463708
lbluque <lbluque@meta.com>
2024-09-09 15:51:39 -0700

    do not shadow built-in property

--- src/fairchem/core/trainers/ocp_trainer.py
+++ src/fairchem/core/trainers/ocp_trainer.py
@@ -256,10 +256,11 @@ class OCPTrainer(BaseTrainer):
                 elif isinstance(out[target_key], dict):
                     # if output is a nested dictionary (in the case of hydra models), we attempt to retrieve it using the property name
                     # ie: "output_head_name.property"
-                    assert "property" in self.output_targets[target_key], \
-                        f"we need to know which property to match the target to, please specify the property field in the task config, current config: {self.output_targets[target_key]}"
-                    property = self.output_targets[target_key]["property"]
-                    pred = out[target_key][property]
+                    assert (
+                        "property" in self.output_targets[target_key]
+                    ), f"we need to know which property to match the target to, please specify the property field in the task config, current config: {self.output_targets[target_key]}"
+                    prop = self.output_targets[target_key]["property"]
+                    pred = out[target_key][prop]
 
             ## TODO: deprecate the following logic?
             ## Otherwise, assume target property is a derived output of the model. Construct the parent property
@@ -664,9 +665,7 @@ class OCPTrainer(BaseTrainer):
                 )
                 gather_results["chunk_idx"] = np.cumsum(
                     [gather_results["chunk_idx"][i] for i in idx]
-                )[
-                    :-1
-                ]  # np.split does not need last idx, assumes n-1:end
+                )[:-1]  # np.split does not need last idx, assumes n-1:end
 
                 full_path = os.path.join(
                     self.config["cmd"]["results_dir"], "relaxed_positions.npz"
