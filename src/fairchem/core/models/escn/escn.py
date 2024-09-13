a7e6d1d87ed47aa07ce0ea1e4e883dcdff262aba
Misko <misko@meta.com>
2024-09-13 17:08:58 +0000

    add graph model mixin

--- src/fairchem/core/models/escn/escn.py
+++ src/fairchem/core/models/escn/escn.py
@@ -19,7 +19,7 @@ if typing.TYPE_CHECKING:
 
 from fairchem.core.common.registry import registry
 from fairchem.core.common.utils import conditional_grad
-from fairchem.core.models.base import BackboneInterface, HeadInterface
+from fairchem.core.models.base import BackboneInterface, GraphModelMixin, HeadInterface
 from fairchem.core.models.escn.so3 import (
     CoefficientMapping,
     SO3_Embedding,
@@ -39,7 +39,7 @@ with contextlib.suppress(ImportError):
 
 
 @registry.register_model("escn_backbone")
-class eSCNBackbone(nn.Module, BackboneInterface):
+class eSCNBackbone(nn.Module, BackboneInterface, GraphModelMixin):
     """Equivariant Spherical Channel Network
     Paper: Reducing SO(3) Convolutions to SO(2) for Efficient Equivariant GNNs
 
