efcb0af2eac41dfb42e081e79aa5e3785dd63ebb
lbluque <lbluque@meta.com>
2024-09-20 11:03:18 -0700

    expose ml_relax in relaxation

--- src/fairchem/core/common/relaxation/__init__.py
+++ src/fairchem/core/common/relaxation/__init__.py
@@ -7,6 +7,7 @@ LICENSE file in the root directory of this source tree.
 
 from __future__ import annotations
 
+from .ml_relaxation import ml_relax
 from .optimizers.optimizable import OptimizableBatch, OptimizableUnitCellBatch
 
-__all__ = ["OptimizableBatch", "OptimizableUnitCellBatch"]
+__all__ = ["ml_relax", "OptimizableBatch", "OptimizableUnitCellBatch"]
