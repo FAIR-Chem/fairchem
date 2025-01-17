2354f35e914cce6135b9930ce86eff9b2b7f37ff
Ray Gao <rgao@meta.com>
2025-01-17 20:49:06 +0000

    wip refactor hydra cli
    Former-commit-id: e0efe32af1cf098a08f22f77c3923bbf65f95622

--- src/fairchem/core/_cli_hydra.py
+++ src/fairchem/core/_cli_hydra.py
@@ -7,16 +7,18 @@ LICENSE file in the root directory of this source tree.
 
 from __future__ import annotations
 
+import argparse
 import logging
 import os
+import tempfile
+import uuid
+from enum import Enum
 from typing import TYPE_CHECKING
 
 import hydra
 from omegaconf import OmegaConf
 
 if TYPE_CHECKING:
-    import argparse
-
     from omegaconf import DictConfig
 
     from fairchem.core.components.runner import Runner
@@ -27,11 +29,59 @@ from submitit.helpers import Checkpointable, DelayedSubmission
 from torch.distributed.launcher.api import LaunchConfig, elastic_launch
 
 from fairchem.core.common import distutils
-from fairchem.core.common.flags import flags
 from fairchem.core.common.utils import get_timestamp_uid, setup_env_vars, setup_imports
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+# this effects the cli only since the actual job will be run in subprocesses or remoe
+logging.basicConfig(level=logging.INFO)
+
+
+class SchedulerType(str, Enum):
+    LOCAL = "local"
+    SLURM = "slurm"
+
+
+class DeviceType(str, Enum):
+    CPU = "cpu"
+    CUDA = "cuda"
+
+
+class SchedulerConfig:
+    scheduler_type: SchedulerType = SchedulerType.LOCAL
+    ranks_per_node: int = 1
+    num_nodes: int = 1
+    slurm: dict = None
+
+    def __post_init__(self):
+        defaults = {
+            "slurm_mem": 80,  # slurm mem in GB
+            "timeout_min": 4320,  # slurm timeout in mins, default to 7 days
+            "slurm_partition": None,
+            "cpus_per_task": 8,
+            "slurm_qos": None,
+            "slurm_account": None,
+        }
+        # fuse defaults with user inputs
+        self.slurm = defaults.update(self.slurm)
+
+
+class FairchemJobConfig:
+    def __init__(
+        self,
+        run_name: str | None = None,
+        timestamp_id: str | None = None,
+        run_dir: str | None = None,
+        log_dir: str | None = None,
+        device_type: DeviceType = DeviceType.CUDA,
+    ):
+        self.run_name = run_name or uuid.uuid4().hex.upper()[0:8]
+        self.timestamp_id = timestamp_id or get_timestamp_uid()
+        self.device_type = device_type
+        if not run_dir:
+            self.run_dir = tempfile.TemporaryDirectory().name
+        if not log_dir:
+            self.log_dir = os.path.join(self.run_dir, self.timestamp_id, "logs")
+        os.makedirs(self.run_dir, exist_ok=True)
+        os.makedirs(self.log_dir, exist_ok=True)
 
 
 class Submitit(Checkpointable):
@@ -94,7 +144,7 @@ def get_hydra_config_from_yaml(
     os.environ["HYDRA_FULL_ERROR"] = "1"
     config_directory = os.path.dirname(os.path.abspath(config_yml))
     config_name = os.path.basename(config_yml)
-    hydra.initialize_config_dir(config_directory)
+    hydra.initialize_config_dir(config_directory, version_base="1.1")
     return hydra.compose(config_name=config_name, overrides=overrides_args)
 
 
@@ -102,62 +152,49 @@ def runner_wrapper(config: DictConfig):
     Submitit()(config)
 
 
-# this is meant as a future replacement for the main entrypoint
 def main(
     args: argparse.Namespace | None = None, override_args: list[str] | None = None
 ):
     if args is None:
-        parser: argparse.ArgumentParser = flags.get_parser()
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--config-yml", type=str, required=True)
         args, override_args = parser.parse_known_args()
 
     cfg = get_hydra_config_from_yaml(args.config_yml, override_args)
-    timestamp_id = get_timestamp_uid()
-    log_dir = os.path.join(args.run_dir, timestamp_id, "logs")
-    # override timestamp id and logdir
-    args.timestamp_id = timestamp_id
-    args.logdir = log_dir
-    os.makedirs(log_dir)
-    OmegaConf.update(cfg, "cli_args", vars(args), force_add=True)
-    if args.submit:  # Run on cluster
-        executor = AutoExecutor(folder=log_dir, slurm_max_num_timeout=3)
+    job_cfg: FairchemJobConfig = hydra.utils.instantiate(cfg.job)
+    scheduler: SchedulerConfig = hydra.utils.instantiate(cfg.scheduler)
+    logging.info(f"Running fairchemv2 cli with {job_cfg}, {scheduler}")
+
+    if job_cfg.scheduler_type == SchedulerType.SLURM:  # Run on cluster
+        executor = AutoExecutor(folder=job_cfg.log_dir, slurm_max_num_timeout=3)
         executor.update_parameters(
-            name=args.identifier,
-            mem_gb=args.slurm_mem,
-            timeout_min=args.slurm_timeout * 60,
-            slurm_partition=args.slurm_partition,
-            gpus_per_node=args.num_gpus,
-            cpus_per_task=8,
-            tasks_per_node=args.num_gpus,
-            nodes=args.num_nodes,
-            slurm_qos=args.slurm_qos,
-            slurm_account=args.slurm_account,
+            name=job_cfg.run_name,
+            mem_gb=scheduler.slurm.slurm_mem,
+            timeout_min=scheduler.slurm.slurm_timeout * 60,
+            slurm_partition=scheduler.slurm.slurm_partition,
+            gpus_per_node=scheduler.num_gpus,
+            cpus_per_task=scheduler.slurm.cpus_per_task,
+            tasks_per_node=scheduler.num_gpus,
+            nodes=scheduler.num_nodes,
+            slurm_qos=scheduler.slurm.slurm_qos,
+            slurm_account=scheduler.slurm.slurm_account,
         )
         job = executor.submit(runner_wrapper, cfg)
-        logger.info(
-            f"Submitted job id: {timestamp_id}, slurm id: {job.job_id}, logs: {log_dir}"
+        logging.info(
+            f"Submitted job id: {job_cfg.timestamp_id}, slurm id: {job.job_id}, logs: {job_cfg.log_dir}"
         )
     else:
-        if args.num_gpus > 1:
-            logging.info(f"Running in local mode with {args.num_gpus} ranks")
-            # HACK to disable multiprocess dataloading in local mode
-            # there is an open issue where LMDB's environment cannot be pickled and used
-            # during torch multiprocessing https://github.com/pytorch/examples/issues/526
-            # this HACK only works for a training submission where the config is passed in here
-            if "optim" in cfg and "num_workers" in cfg["optim"]:
-                cfg["optim"]["num_workers"] = 0
-                logging.info(
-                    "WARNING: running in local mode, setting dataloading num_workers to 0, see https://github.com/pytorch/examples/issues/526"
-                )
-
+        if scheduler.num_gpus > 1:
+            logging.info(f"Running in local mode with {job_cfg.num_gpus} ranks")
             launch_config = LaunchConfig(
                 min_nodes=1,
                 max_nodes=1,
-                nproc_per_node=args.num_gpus,
+                nproc_per_node=scheduler.num_gpus,
                 rdzv_backend="c10d",
                 max_restarts=0,
             )
             elastic_launch(launch_config, runner_wrapper)(cfg)
         else:
-            logger.info("Running in local mode without elastic launch")
+            logging.info("Running in local mode without elastic launch")
             distutils.setup_env_local()
             runner_wrapper(cfg)
