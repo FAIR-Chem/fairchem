f844a2f012a5082913b0e5542b903bc1b0be333e
Misko <misko@meta.com>
2025-02-07 00:38:24 +0000

    fix

--- src/fairchem/core/models/base.py
+++ src/fairchem/core/models/base.py
@@ -253,7 +253,10 @@ class HydraModel(nn.Module, GraphModelMixin):
         if finetune_config is not None:
             # Make it hard to sneak more fields into finetuneconfig
             assert (
-                set(finetune_config.keys() - set(["starting_checkpoint", "override"]))
+                len(
+                    set(finetune_config.keys())
+                    - set(["starting_checkpoint", "override"])
+                )
                 == 0
             )
             starting_model: HydraModel = load_model_and_weights_from_checkpoint(
