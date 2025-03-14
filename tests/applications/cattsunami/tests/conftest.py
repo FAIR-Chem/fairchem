7c22acfd804acacacbfb53382d0ff9595520ab44
Daniel Levine <levineds@meta.com>
2025-03-14 19:07:26 +0000

    indicate where tests have been run from

--- tests/applications/cattsunami/tests/conftest.py
+++ tests/applications/cattsunami/tests/conftest.py
@@ -21,8 +21,11 @@ def desorption_inputs(request):
 @pytest.fixture(scope="class")
 def dissociation_inputs(request):
     pkl_path = Path(__file__).parent / "autoframe_inputs_dissociation.pkl"
+    # We need to know where the test repo has been downloaded to,
+    # it may be different from the src location
+    test_dir = Path(__file__).parent[5]
     if not pkl_path.exists():
-        download_large_files.download_file_group("cattsunami")
+        download_large_files.download_file_group("cattsunami", test_dir)
     with open(pkl_path, "rb") as fp:
         request.cls.inputs = pickle.load(fp)
 
@@ -30,7 +33,10 @@ def dissociation_inputs(request):
 @pytest.fixture(scope="class")
 def transfer_inputs(request):
     pkl_path = Path(__file__).parent / "autoframe_inputs_transfer.pkl"
+    # We need to know where the test repo has been downloaded to,
+    # it may be different from the src location
+    test_dir = Path(__file__).parent[5]
     if not pkl_path.exists():
-        download_large_files.download_file_group("cattsunami")
+        download_large_files.download_file_group("cattsunami", test_dir)
     with open(pkl_path, "rb") as fp:
         request.cls.inputs = pickle.load(fp)
